2a62e1fb32823d6a89b4cdb8f02a47a490598b0c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-02 20:36:06 +0800

    return ToHexString(static_cast<uint64_t>(number));

--- include/base/string/ToHexString.cpp
+++ include/base/string/ToHexString.cpp
@@ -2,39 +2,37 @@
 
 std::string base::ToHexString(int8_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(uint8_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(int16_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(uint16_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(int32_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(uint32_t number)
 {
-	return ToHexString(static_cast<int64_t>(number));
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(int64_t number)
 {
-	std::stringstream string_stream;
-	string_stream << "0x" << std::hex << number;
-	return string_stream.str();
+	return ToHexString(static_cast<uint64_t>(number));
 }
 
 std::string base::ToHexString(uint64_t number)
