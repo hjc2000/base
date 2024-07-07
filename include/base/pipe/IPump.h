897ba8c014ecd54087153fb306eba7485481503f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-07 19:44:57 +0800

    virtual void PumpDataToConsumersAsync(std::shared_ptr<base::CancellationToken> cancellation_token)

--- include/base/pipe/IPump.h
+++ include/base/pipe/IPump.h
@@ -1,6 +1,15 @@
 #pragma once
+#ifndef HAS_THREAD
+#define HAS_THREAD 0
+#endif
+
 #include <base/pipe/IPipeSource.h>
 #include <base/task/CancellationToken.h>
+
+#if HAS_THREAD
+#include <iostream>
+#include <thread>
+#endif
 
 namespace base
 {
@@ -16,5 +25,28 @@ namespace base
 		/// @brief 将数据从源中取出，泵送给所有消费者
 		/// @param cancellation_token
 		virtual void PumpDataToConsumers(std::shared_ptr<base::CancellationToken> cancellation_token) = 0;
+
+#if HAS_THREAD
+		virtual void PumpDataToConsumersAsync(std::shared_ptr<base::CancellationToken> cancellation_token)
+		{
+			auto thread_func = [this, cancellation_token]()
+			{
+				try
+				{
+					PumpDataToConsumers(cancellation_token);
+				}
+				catch (const std::exception &e)
+				{
+					std::cerr << e.what() << '\n';
+					std::cerr << "PumpDataToConsumersAsync 启动执行的线程函数发生异常，线程退出" << std::endl;
+				}
+				catch (...)
+				{
+					std::cerr << "PumpDataToConsumersAsync 启动执行的线程函数发生未知异常，线程退出" << std::endl;
+				}
+			};
+			std::thread{thread_func}.detach();
+		}
+#endif
 	};
 } // namespace base
