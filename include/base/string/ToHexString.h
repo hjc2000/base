d2ca5b4464dfeaced009c0e0bc022059f808ca6f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-09 00:25:57 +0800

    注释 	std::string ToHexString(uint8_t *buffer, int32_t size);

--- include/base/string/ToHexString.h
+++ include/base/string/ToHexString.h
@@ -20,5 +20,11 @@ namespace base
 	std::string ToHexString(int64_t number);
 	std::string ToHexString(uint64_t number);
 
+	/// @brief 将缓冲区中的每个字节变成 16 进制字符串，每个字节的字符串用逗号分隔，每
+	/// 20 个字节一行。
+	///
+	/// @param buffer
+	/// @param size
+	/// @return
 	std::string ToHexString(uint8_t *buffer, int32_t size);
 }
