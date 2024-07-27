4981f6962c6813a7f9c489fa98b06245b43d1d7e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-27 23:06:59 +0800

    class ArrayCollection

--- include/base/container/ICollection.h
+++ include/base/container/ICollection.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <array>
 
 namespace base
 {
@@ -26,5 +27,34 @@ namespace base
 		virtual int Count() const = 0;
 		virtual ItemType Get(KeyType key) const = 0;
 		virtual void Put(KeyType key, ItemType const &item) = 0;
+	};
+
+	template <int ItemCount, typename ItemType>
+	class ArrayCollection
+		: public base::ICollection<int, ItemType>
+	{
+	private:
+		std::array<ItemType, ItemCount> &_array;
+
+	public:
+		ArrayCollection(std::array<ItemType, ItemCount> &array)
+			: _array(array)
+		{
+		}
+
+		int Count() const override
+		{
+			return static_cast<int>(_array.size());
+		}
+
+		ItemType Get(int key) const override
+		{
+			return _array[key];
+		}
+
+		void Put(int key, ItemType const &item) override
+		{
+			_array[key] = item;
+		}
 	};
 }
