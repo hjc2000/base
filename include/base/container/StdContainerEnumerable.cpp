8ddab3c50e01c4a2eb506a3755db5db21216db3a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-07-30 00:48:15 +0800

    template <typename T> 	concept IsIterator = requires(T &c) { 		c++; 		c == c; 		*c; 	};

---
+++ include/base/container/StdContainerEnumerable.cpp
@@ -0,0 +1,22 @@
+#if HAS_THREAD
+#include "StdContainerEnumerable.h"
+#include <iostream>
+#include <vector>
+
+void base::TestStdContainerEnumerable()
+{
+	std::shared_ptr<std::vector<int>> vector{new std::vector<int>{1, 2, 3, 4, 5}};
+	base::StdContainerEnumerable<int, std::vector<int>> ve{vector};
+	for (int &i : ve)
+	{
+		std::cout << i << std::endl;
+	}
+
+	base::StdContainerEnumerable<int, std::vector<int>> const &const_ve = ve;
+	for (int const &i : const_ve)
+	{
+		std::cout << i << std::endl;
+	}
+}
+
+#endif // HAS_THREAD
---
+++ include/base/container/StdContainerEnumerable.h
@@ -0,0 +1,118 @@
+#pragma once
+#ifndef HAS_THREAD
+#define HAS_THREAD 0
+#endif
+
+#include <base/container/IEnumerable.h>
+#include <memory>
+
+namespace base
+{
+	template <typename T>
+	concept IsIterator = requires(T &c) {
+		c++;
+		c == c;
+		*c;
+	};
+
+	/// @brief std 容器的迭代器。
+	/// @tparam ItemType 被迭代元素的类型。
+	/// @tparam IteratorType 迭代器类型。
+	template <typename ItemType, IsIterator IteratorType>
+	class StdContainerEnumerator
+		: public base::IEnumerator<ItemType>
+	{
+	private:
+		IteratorType _current_iterator;
+		IteratorType _begin_iterator;
+		IteratorType _end_iterator;
+		bool _is_first_move = true;
+
+	public:
+		StdContainerEnumerator(IteratorType begin_iterator,
+							   IteratorType end_iterator)
+		{
+			_current_iterator = begin_iterator;
+			_begin_iterator = begin_iterator;
+			_end_iterator = end_iterator;
+		}
+
+		ItemType &CurrentValue() override
+		{
+			return *_current_iterator;
+		}
+
+		bool MoveNext() override
+		{
+			if (_is_first_move)
+			{
+				_is_first_move = false;
+			}
+			else
+			{
+				_current_iterator++;
+			}
+
+			if (_current_iterator == _end_iterator)
+			{
+				// 移动到下一个元素失败
+				return false;
+			}
+
+			// 移动到下一个元素成功
+			return true;
+		}
+
+		void Reset() override
+		{
+			_current_iterator = _begin_iterator;
+		}
+	};
+
+	template <typename T>
+	concept IsIterable = requires(T &c) {
+		c.begin();
+		c.end();
+	};
+
+	/// @brief 将 std 容器包装为 IEnumerable
+	/// @tparam ItemType 被迭代元素的类型
+	/// @tparam ContainerType 容器类型
+	template <typename ItemType, IsIterable ContainerType>
+	class StdContainerEnumerable
+		: public IEnumerable<ItemType>
+	{
+	private:
+		std::shared_ptr<ContainerType> _container;
+
+	public:
+		StdContainerEnumerable(std::shared_ptr<ContainerType> container)
+		{
+			_container = container;
+		}
+
+		std::shared_ptr<IEnumerator<ItemType>> GetEnumerator() override
+		{
+			return std::shared_ptr<IEnumerator<ItemType>>{
+				new StdContainerEnumerator<ItemType, decltype(_container->begin())>{
+					_container->begin(),
+					_container->end(),
+				},
+			};
+		}
+
+		std::shared_ptr<IEnumerator<ItemType const>> GetEnumerator() const override
+		{
+			return std::shared_ptr<IEnumerator<ItemType const>>{
+				new StdContainerEnumerator<ItemType const, decltype(_container->begin())>{
+					_container->begin(),
+					_container->end(),
+				},
+			};
+		}
+	};
+
+#if HAS_THREAD
+	void TestStdContainerEnumerable();
+#endif
+}
