afa170b8082fbf5f9c40cad913b8d702c29f47c1
hjc2000 <hjc.666@foxmail.com>
2024-07-30 10:56:11 +0800

    class IEnumeratorForwardIterator

--- include/base/container/IEnumeratorForwardIterator.h
+++ include/base/container/IEnumeratorForwardIterator.h
@@ -22,20 +22,20 @@ namespace base
 
 		/// @brief 解引用
 		/// @return
-		ItemType &operator*()
+		ItemType &operator*() override
 		{
 			return _enumertor->CurrentValue();
 		}
 
 		/// @brief 前缀递增
 		/// @return
-		IEnumeratorForwardIterator<ItemType> &operator++()
+		IEnumeratorForwardIterator<ItemType> &operator++() override
 		{
 			// 不做任何操作，因为 == 运算符里面已经 MoveNext 了。
 			return *this;
 		}
 
-		bool operator==(IEnumeratorForwardIterator<ItemType> const &o) const
+		bool operator==(IEnumeratorForwardIterator<ItemType> const &o) const override
 		{
 			/* C++ 范围 for 的机制是先判断不等于 end() 然后解引用。然后递增，进入下一循环。
 			 * IEnumerator 的操作顺序是先 MoveNext 然后判断是否成功，成功再利用 CurrentValue
