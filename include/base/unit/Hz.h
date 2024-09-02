e12d3fd5c328db54c78a2b253bb330f5a38833a9
hjc2000 <hjc.666@foxmail.com>
2024-09-02 13:12:27 +0800

    class Hz

--- include/base/unit/Hz.h
+++ include/base/unit/Hz.h
@@ -1,9 +1,27 @@
 #pragma once
+#include <base/math/Fraction.h>
 
 namespace base
 {
     class Hz
     {
+    private:
+        base::Fraction _value{1};
+
     public:
+        Hz(base::Fraction value)
+        {
+            _value = value;
+        }
+
+        base::Fraction Value() const
+        {
+            return _value;
+        }
+
+        operator base::Fraction() const
+        {
+            return _value;
+        }
     };
 } // namespace base
