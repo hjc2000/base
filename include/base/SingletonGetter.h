2e98a427003da5f9a3e5b6ad7a91f3aab818ab83
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-06 23:41:55 +0800

    SingletonType &GetSingletonInstance()

--- include/base/SingletonGetter.h
+++ include/base/SingletonGetter.h
@@ -5,6 +5,8 @@
 
 namespace base
 {
+    /// @brief 单例获取器。
+    /// @tparam SingletonType
     template <typename SingletonType>
     class SingletonGetter :
         protected base::ILock
@@ -18,6 +20,8 @@ namespace base
         virtual void Unlock() = 0;
 
     public:
+        /// @brief 获取单例的引用
+        /// @return
         SingletonType &Instance()
         {
             if (_p != nullptr)
@@ -40,14 +44,30 @@ namespace base
             return *_p;
         }
 
+        /// @brief 重载解引用运算符，用来获取单例的引用。
+        /// @return
         SingletonType &operator*()
         {
             return Instance();
         }
 
+        /// @brief 重载指针式的成员访问运算符，用来访问单例对象的成员。
+        /// @return
         SingletonType *operator->()
         {
             return &Instance();
         }
     };
+
+    /// @brief 借助 SingletonGetter 获取单例。
+    /// @note 本函数简化了单例的获取过程，不用再手动构造一个 SingletonGetter 了。
+    /// 但是，单例的提供者仍然需要派生 SingletonGetter 来提供单例。
+    /// @tparam SingletonType
+    /// @return
+    template <typename SingletonType>
+    SingletonType &GetSingletonInstance()
+    {
+        SingletonGetter<SingletonType> getter;
+        return getter.Instance();
+    }
 } // namespace base
