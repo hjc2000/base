a456df8e61a48225a23381227e3b70cf39029c6f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-09-07 00:25:35 +0800

    /// @note 不要求是单例还是多例。依赖提供者自由决定。

---
+++ include/base/di/GetInstance.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <memory>
+#include <stdexcept>
+
+namespace base
+{
+    /// @brief 获取实例。
+    /// @note 不要求是单例还是多例。依赖提供者自由决定。
+    /// @tparam T
+    /// @return
+    template <typename T>
+    std::shared_ptr<T> GetInstance()
+    {
+        throw std::runtime_error{"未实现的特化版本"};
+    }
+
+} // namespace base
