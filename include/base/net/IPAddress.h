330504095e152fb8c59d31ae89cf4226d9a7f57f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-12 20:54:03 +0800

    class IPAddress

---
+++ include/base/net/IPAddress.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <base/container/Array.h>
+#include <base/stream/Span.h>
+
+namespace base
+{
+    /// @brief IP 地址。兼容 IPV4 和 IPV6.
+    class IPAddress
+    {
+    public:
+    };
+} // namespace base
