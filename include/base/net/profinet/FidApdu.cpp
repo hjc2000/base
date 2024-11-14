f71f76e876e595a484a1faac771ed43daf957b47
hjc2000 <hjc.666@foxmail.com>
2024-11-14 10:13:32 +0800

    class FidApdu

---
+++ include/base/net/profinet/FidApdu.cpp
@@ -0,0 +1,1 @@
+#include "FidApdu.h"
---
+++ include/base/net/profinet/FidApdu.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <base/stream/Span.h>
+
+namespace base
+{
+    namespace profinet
+    {
+        class FidApdu
+        {
+        public:
+        };
+    } // namespace profinet
+} // namespace base
