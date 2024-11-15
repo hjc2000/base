80ee2b081132cd9a586aec677712b19f021a79b4
hjc2000 <hjc.666@foxmail.com>
2024-11-15 13:56:07 +0800

    class ApduStatus

--- include/base/net/profinet/ApduStatus.h
+++ include/base/net/profinet/ApduStatus.h
@@ -17,6 +17,23 @@ namespace base
 
             base::Span &Span();
             base::Span const &Span() const;
+
+            /// @brief 对循环周期的计数。
+            /// @note 对于 RTC-PDU，循环周期是 31.25μs，本属性对循环周期计数。接收方使用本属性来检验数据的时效性，
+            /// 以及判断是否重复或丢失。
+            /// @return
+            uint16_t CycleCounter() const;
+            void SetCycleCounter(uint16_t value);
+
+            /// @brief 数据状态。分成 8 个位使用，每个位都有含义。
+            /// @return
+            uint8_t DataStatus() const;
+            void SetDataStatus(uint8_t value);
+
+            /// @brief 传输状态。用来指示一些错误。
+            /// @return
+            uint8_t TransferStatus() const;
+            void SetTransferStatus(uint8_t value);
         };
     } // namespace profinet
 } // namespace base
