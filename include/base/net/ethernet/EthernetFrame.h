1d7e1bb5cc4627d16aa2c5eb2991b9ab0e413a73
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-17 23:39:40 +0800

    注释

--- include/base/net/ethernet/EthernetFrame.h
+++ include/base/net/ethernet/EthernetFrame.h
@@ -12,10 +12,10 @@ namespace base
         class EthernetFrame
         {
         public:
-            /// @brief 构造一个没有引用任何内存的以太网帧
+            /// @brief 构造一个没有引用任何内存的以太网帧。
             EthernetFrame() = default;
 
-            /// @brief 构造函数
+            /// @brief 构造函数。
             /// @param span 引用的内存。
             EthernetFrame(base::Span const &span);
 
@@ -43,7 +43,8 @@ namespace base
             /// @brief 类型或长度。
             /// @note 整型值小于等于 1500，则表示长度，大于 1500 则表示帧类型。当含义是帧类型时，
             /// 这个值是用来给上层的处理程序看的，让上层的处理程序决定将以太网帧送往哪个协议处理程序。
-            /// 例如下面这几个值都大于 1500：
+            /// @note 1500 的 16 进制：0x05dc.
+            /// @note 下面这几个值都大于 1500：
             /// 	@li 0x0800：表示IPv4数据包
             /// 	@li 0x0806：表示ARP请求
             /// 	@li 0x86DD：表示IPv6数据包
@@ -53,8 +54,10 @@ namespace base
             void SetTypeOrLength(LengthTypeEnum value);
 
             /// @brief 载荷数据。
-            /// @note 最大大小 1500 字节。
+            /// @note 字节数的取值范围：[46, 1500].
+            /// @note 巨型帧可以超过 1500 字节。但是需要网络设备支持，否则会导致无法传输。
             /// @note 如果不满 46 字节，需要后面填充 0，使其达到 46 字节。
+            /// @note 因为可能会有填充，所以需要靠 TypeOrLength 属性来识别出有效字节数。
             /// @return
             base::Span Payload() const;
             void SetPayload(base::Span const &value);
