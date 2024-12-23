8851aaf06f602445219d144d461e3f47ef642aa5
hjc2000 <hjc.666@foxmail.com>
2024-12-23 13:40:19 +0800

    String

--- include/base/string/String.cpp
+++ include/base/string/String.cpp
@@ -5,6 +5,11 @@ base::String::String(std::string const &o)
 	_string = o;
 }
 
+base::String::String(char o)
+{
+	_string = o;
+}
+
 base::String::String(String const &o)
 {
 	*this = o;
@@ -24,4 +29,48 @@ std::string &base::String::StdString()
 std::string const &base::String::StdString() const
 {
 	return _string;
+}
+
+char &base::String::operator[](size_t index)
+{
+	if (index > _string.size())
+	{
+		throw std::out_of_range{"索引超出范围。"};
+	}
+
+	return _string[index];
+}
+
+char const &base::String::operator[](size_t index) const
+{
+	if (index > _string.size())
+	{
+		throw std::out_of_range{"索引超出范围。"};
+	}
+
+	return _string[index];
+}
+
+base::String &base::String::operator+=(base::String const &o)
+{
+	_string += o.StdString();
+	return *this;
+}
+
+base::String base::String::operator+(base::String const &o) const
+{
+	std::string ret;
+	ret.reserve(_string.size() + o.StdString().size());
+	ret = _string + o.StdString();
+	return base::String{ret};
+}
+
+size_t base::String::Length() const
+{
+	return _string.size();
+}
+
+base::String operator+(std::string const &left, base::String const &right)
+{
+	return base::String{right + left};
 }
--- include/base/string/String.h
+++ include/base/string/String.h
@@ -1,4 +1,6 @@
 #pragma once
+#include <stdexcept>
+#include <stdint.h>
 #include <string>
 
 namespace base
@@ -9,12 +11,17 @@ namespace base
 		std::string _string;
 
 	public:
+#pragma region 生命周期
 		/// @brief 无参构造函数。
 		String() = default;
 
 		/// @brief 从 std::string 构造。
 		/// @param o
 		String(std::string const &o);
+
+		/// @brief 从单个字符构造。
+		/// @param o
+		String(char o);
 
 		/// @brief 拷贝构造函数。
 		/// @param o
@@ -24,13 +31,48 @@ namespace base
 		/// @param o
 		/// @return
 		String &operator=(String const &o);
+#pragma endregion
 
 		std::string &StdString();
 		std::string const &StdString() const;
 
-		explicit operator std::string() const
+		char &operator[](size_t index);
+		char const &operator[](size_t index) const;
+
+		base::String &operator+=(base::String const &o);
+		base::String operator+(base::String const &o) const;
+
+		/// @brief 字符串长度。不包括结尾的空字符。
+		/// @return
+		size_t Length() const;
+
+#pragma region 迭代
+		using Iterator = std::string::iterator;
+
+		Iterator begin()
 		{
-			return _string;
+			return _string.begin();
 		}
+
+		Iterator end()
+		{
+			return _string.end();
+		}
+
+		using ConstIterator = std::string::const_iterator;
+
+		ConstIterator begin() const
+		{
+			return _string.begin();
+		}
+
+		ConstIterator end() const
+		{
+			return _string.end();
+		}
+
+#pragma endregion
 	};
 } // namespace base
+
+base::String operator+(std::string const &left, base::String const &right);
