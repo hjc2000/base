29bd87864d30b887b4b282397e16b13726d1fcc3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-23 21:08:37 +0800

    String

--- main.cpp
+++ main.cpp
@@ -8,17 +8,16 @@
 #include <base/sfinae/Equal.h>
 #include <base/stream/MemoryStream.h>
 #include <base/string/String.h>
-#include <base/string/StringHelper.h>
 #include <base/string/ToHexString.h>
 #include <format>
 #include <iostream>
 
 int main()
 {
-	base::String str{"  \r\n\t 123,456,789"};
-	std::cout << str << std::endl;
-	str = str.TrimStart();
-	std::cout << str << std::endl;
+	base::String str{"123,456,789   \r\n\t "};
+	std::cout << str.Length() << std::endl;
+	str = str.TrimEnd();
+	std::cout << str.Length() << std::endl;
 
 	base::List<std::string> string_list = str.Split(',');
 	for (auto &str : string_list)
