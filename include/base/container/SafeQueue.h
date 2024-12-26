5f5b24295ad4f69fd17096ac59676413134b2341
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-12-27 01:07:24 +0800

    SafeQueue

--- include/base/container/SafeQueue.h
+++ include/base/container/SafeQueue.h
@@ -1,94 +1,88 @@
 #pragma once
-#ifndef HAS_THREAD
-#define HAS_THREAD 0
-#endif
-
-#if HAS_THREAD
 #include <atomic>
 #include <base/container/IQueue.h>
-#include <mutex>
+#include <base/task/IMutex.h>
 #include <queue>
 #include <stdexcept>
 
 namespace base
 {
-    /// @brief 线程安全的队列，加了互斥锁。
-    /// @tparam T
-    template <typename T>
-    class SafeQueue final :
-        public base::IQueue<T>
-    {
-    private:
-        std::queue<T> _queue;
-        mutable std::mutex _lock;
+	/// @brief 线程安全的队列，加了互斥锁。
+	/// @tparam T
+	template <typename T>
+	class SafeQueue final :
+		public base::IQueue<T>
+	{
+	private:
+		std::queue<T> _queue;
+		mutable std::shared_ptr<base::IMutex> _lock = base::di::CreateMutex();
 
-    public:
-        /// @brief 构造一个空的队列。
-        /// @note 因为拷贝构造函数和移动构造函数无法对被拷贝和被移动的对象加锁，
-        /// 所以会引发线程安全问题，所以本类不支持拷贝和移动。
-        SafeQueue() = default;
+	public:
+		/// @brief 构造一个空的队列。
+		/// @note 因为拷贝构造函数和移动构造函数无法对被拷贝和被移动的对象加锁，
+		/// 所以会引发线程安全问题，所以本类不支持拷贝和移动。
+		SafeQueue() = default;
 
-        /// @brief 队列中元素的数量。
-        /// @return
-        int32_t Count() const override
-        {
-            std::lock_guard l{_lock};
-            return _queue.size();
-        }
+		/// @brief 队列中元素的数量。
+		/// @return
+		int32_t Count() const override
+		{
+			base::LockGuard g{*_lock};
+			return _queue.size();
+		}
 
-        /// @brief 退队。
-        /// @return
-        T Dequeue() override
-        {
-            std::lock_guard l{_lock};
-            if (_queue.empty())
-            {
-                throw std::runtime_error("队列当前为空，无法退队");
-            }
+		/// @brief 退队。
+		/// @return
+		T Dequeue() override
+		{
+			base::LockGuard g{*_lock};
+			if (_queue.empty())
+			{
+				throw std::runtime_error("队列当前为空，无法退队");
+			}
 
-            T ret = _queue.front();
-            _queue.pop();
-            return ret;
-        }
+			T ret = _queue.front();
+			_queue.pop();
+			return ret;
+		}
 
-        /// @brief 尝试退队
-        /// @param out 从队列里拿出来的元素会被赋值给 out，这要求 out 的类要实现赋值运算符。
-        /// @return 退队成功返回 true，失败返回 false
-        bool TryDequeue(T &out) override
-        {
-            std::lock_guard l{_lock};
-            if (_queue.empty())
-            {
-                return false;
-            }
+		/// @brief 尝试退队
+		/// @param out 从队列里拿出来的元素会被赋值给 out，这要求 out 的类要实现赋值运算符。
+		/// @return 退队成功返回 true，失败返回 false
+		bool TryDequeue(T &out) override
+		{
+			base::LockGuard g{*_lock};
+			if (_queue.empty())
+			{
+				return false;
+			}
 
-            out = _queue.front();
-            _queue.pop();
-            return true;
-        }
+			out = _queue.front();
+			_queue.pop();
+			return true;
+		}
 
-        /// @brief 入队
-        /// @param obj
-        void Enqueue(T obj) override
-        {
-            std::lock_guard l{_lock};
-            _queue.push(obj);
-        }
+		/// @brief 入队
+		/// @param obj
+		void Enqueue(T obj) override
+		{
+			base::LockGuard g{*_lock};
+			_queue.push(obj);
+		}
 
-        /// @brief 清空队列。
-        void Clear() override
-        {
-            std::lock_guard l{_lock};
+		/// @brief 清空队列。
+		void Clear() override
+		{
+			base::LockGuard g{*_lock};
 
-            /*
-             * 构造一个空的队列，然后让 _queue 与空队列交换。退出作用域后，empty_queue
-             * 被销毁，所有元素都丢失。
-             *
-             * 这要求 T 类型必须不是在堆上创建的，否则会内存泄漏，因为本函数并不会 delete 每一个元素。
-             */
-            std::queue<T> empty_queue;
-            _queue.swap(empty_queue);
-        }
-    };
+			/*
+			 * 构造一个空的队列，然后让 _queue 与空队列交换。退出作用域后，empty_queue
+			 * 被销毁，所有元素都丢失。
+			 *
+			 * 这要求 T 类型必须不是在堆上创建的，否则会内存泄漏，因为本函数并不会 delete 每一个元素。
+			 */
+			std::queue<T> empty_queue;
+			_queue.swap(empty_queue);
+		}
+	};
 } // namespace base
-#endif // HAS_THREAD
