d9f37716a0cc7f907bd0ae429f647e491382aaba
hjc2000 <hjc.666@foxmail.com>
2024-12-26 14:38:01 +0800

    return "未知 " + base::ToHexString(static_cast<uint16_t>(value));

--- include/base/net/ethernet/enum/LengthTypeEnum.cpp
+++ include/base/net/ethernet/enum/LengthTypeEnum.cpp
@@ -3,41 +3,41 @@
 
 std::string base::ToString(base::ethernet::LengthTypeEnum value)
 {
-    switch (value)
-    {
-    case base::ethernet::LengthTypeEnum::IP:
-        {
-            return "IP " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    case base::ethernet::LengthTypeEnum::ARP:
-        {
-            return "ARP " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    case base::ethernet::LengthTypeEnum::VlanTag:
-        {
-            return "VlanTag " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    case base::ethernet::LengthTypeEnum::Profinet:
-        {
-            return "Profinet " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    case base::ethernet::LengthTypeEnum::MRP:
-        {
-            return "MRP " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    case base::ethernet::LengthTypeEnum::LLDP:
-        {
-            return "LLDP " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    default:
-        {
-            return "暂不支持将此枚举值转为字符串 " + base::ToHexString(static_cast<uint16_t>(value));
-        }
-    }
+	switch (value)
+	{
+	case base::ethernet::LengthTypeEnum::IP:
+		{
+			return "IP " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	case base::ethernet::LengthTypeEnum::ARP:
+		{
+			return "ARP " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	case base::ethernet::LengthTypeEnum::VlanTag:
+		{
+			return "VlanTag " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	case base::ethernet::LengthTypeEnum::Profinet:
+		{
+			return "Profinet " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	case base::ethernet::LengthTypeEnum::MRP:
+		{
+			return "MRP " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	case base::ethernet::LengthTypeEnum::LLDP:
+		{
+			return "LLDP " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	default:
+		{
+			return "未知 " + base::ToHexString(static_cast<uint16_t>(value));
+		}
+	}
 }
 
 std::ostream &operator<<(std::ostream &os, base::ethernet::LengthTypeEnum value)
 {
-    os << base::ToString(value);
-    return os;
+	os << base::ToString(value);
+	return os;
 }
--- include/base/net/ethernet/enum/LengthTypeEnum.h
+++ include/base/net/ethernet/enum/LengthTypeEnum.h
@@ -1,47 +1,38 @@
 #pragma once
-#ifndef HAS_THREAD
-#define HAS_THREAD 0
-#endif
-
 #include <base/define.h>
 #include <cstdint>
-#include <string>
-
-#if HAS_THREAD
 #include <iostream>
-#endif
+#include <string>
 
 namespace base
 {
-    namespace ethernet
-    {
-        /// @brief 提供以太网中一些上层协议的类型长度字段。
-        enum class LengthTypeEnum : uint16_t
-        {
-            /// @brief IP 协议的类型长度字段。
-            IP = 0x0800,
+	namespace ethernet
+	{
+		/// @brief 提供以太网中一些上层协议的类型长度字段。
+		enum class LengthTypeEnum : uint16_t
+		{
+			/// @brief IP 协议的类型长度字段。
+			IP = 0x0800,
 
-            /// @brief ARP 协议的类型长度字段。
-            ARP = 0x0806,
+			/// @brief ARP 协议的类型长度字段。
+			ARP = 0x0806,
 
-            /// @brief VLAN 标签的类型长度字段。
-            /// @note VLAN 标签也叫 802.1Q 标签。
-            VlanTag = 0x8100,
+			/// @brief VLAN 标签的类型长度字段。
+			/// @note VLAN 标签也叫 802.1Q 标签。
+			VlanTag = 0x8100,
 
-            Profinet = 0x8892,
-            MRP = 0x88E3,
-            LLDP = 0x88CC,
-        };
+			Profinet = 0x8892,
+			MRP = 0x88E3,
+			LLDP = 0x88CC,
+		};
 
-    } // namespace ethernet
+	} // namespace ethernet
 
-    std::string ToString(base::ethernet::LengthTypeEnum value);
+	std::string ToString(base::ethernet::LengthTypeEnum value);
 } // namespace base
 
-#if HAS_THREAD
 /// @brief 将 base::ethernet::LengthTypeEnum 输出到输出流中。
 /// @param os
 /// @param value
 /// @return
 std::ostream &operator<<(std::ostream &os, base::ethernet::LengthTypeEnum value);
-#endif
