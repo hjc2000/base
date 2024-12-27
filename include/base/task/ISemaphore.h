ae587d639976dcfb86327d43f6fc41893ed11dfd
hjc2000 <hjc.666@foxmail.com>
2024-12-27 11:27:31 +0800

    std::shared_ptr<base::ISemaphore> CreateSemaphore(int32_t initial_count);

--- include/base/task/ISemaphore.h
+++ include/base/task/ISemaphore.h
@@ -33,7 +33,7 @@ namespace base
 	namespace di
 	{
 		/// @brief 创建一个信号量。
-		/// @param initial_count
+		/// @param initial_count 信号量的初始计数。
 		/// @return
 		std::shared_ptr<base::ISemaphore> CreateSemaphore(int32_t initial_count);
 	} // namespace di
