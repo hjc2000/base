90cf5f0e7e4354ec75370261b6d470fe78509e1c
hjc2000 <hjc.666@foxmail.com>
2025-01-16 15:34:01 +0800

    Mbps

--- include/base/unit/Mbps.cpp
+++ include/base/unit/Mbps.cpp
@@ -1,8 +1,6 @@
 #include "Mbps.h"
 #include <base/unit/Bps.h>
 
-#pragma region 生命周期
-
 base::Mbps::Mbps(int64_t value)
 {
 	_value = value;
@@ -27,46 +25,4 @@ base::Mbps &base::Mbps::operator=(Mbps const &o)
 {
 	_value = o._value;
 	return *this;
-}
-
-#pragma endregion
-
-#pragma region 重载全局运算符
-
-std::ostream &operator<<(std::ostream &ostream, base::Mbps const &right)
-{
-	ostream << static_cast<base::Fraction>(right);
-	return ostream;
-}
-
-base::Mbps operator+(int64_t left, base::Mbps const &right)
-{
-	return left + static_cast<base::Fraction>(right);
-}
-
-base::Mbps operator-(int64_t left, base::Mbps const &right)
-{
-	return left - static_cast<base::Fraction>(right);
-}
-
-base::Mbps operator*(int64_t left, base::Mbps const &right)
-{
-	return left * static_cast<base::Fraction>(right);
-}
-
-base::Mbps operator/(int64_t left, base::Mbps const &right)
-{
-	return left / static_cast<base::Fraction>(right);
-}
-
-#pragma endregion
-
-int64_t std::floor(base::Mbps const &value)
-{
-	return value.Floor();
-}
-
-int64_t std::ceil(base::Mbps const &value)
-{
-	return value.Ceil();
 }
--- include/base/unit/Mbps.h
+++ include/base/unit/Mbps.h
@@ -1,6 +1,5 @@
 #pragma once
-#include <base/math/Fraction.h>
-#include <base/string/ICanToString.h>
+#include <base/unit/IUnit.h>
 
 namespace base
 {
@@ -9,229 +8,22 @@ namespace base
 	/// @brief 比特率。每秒的比特数。这里是 Mbit / s.
 	/// @note 在通信领域，1 Mbit = 1000 * 1000 bit.
 	class Mbps :
-		public base::ICanToString
+		public base::IUnit<Mbps>
 	{
 	private:
-		base::Fraction _value = 0;
+		base::Fraction _value{0};
 
 	public:
-#pragma region 生命周期
 		Mbps() = default;
 		Mbps(int64_t value);
 		Mbps(base::Fraction const &o);
 		Mbps(Mbps const &o);
 		Mbps(base::Bps const &o);
 		base::Mbps &operator=(Mbps const &o);
-#pragma endregion
 
-#pragma region 强制转换运算符
-
-		explicit operator base::Fraction() const
+		virtual base::Fraction &Value() override
 		{
 			return _value;
 		}
-
-		explicit operator int64_t() const
-		{
-			return static_cast<int64_t>(_value);
-		}
-
-		explicit operator double() const
-		{
-			return static_cast<double>(_value);
-		}
-
-#pragma endregion
-
-		/// @brief 向下取整
-		/// @return
-		int64_t Floor() const
-		{
-			return _value.Floor();
-		}
-
-		/// @brief 向上取整
-		/// @return
-		int64_t Ceil() const
-		{
-			return _value.Ceil();
-		}
-
-#pragma region 四则运算符
-
-		base::Mbps operator-() const
-		{
-			return -_value;
-		}
-
-		base::Mbps operator+(Mbps const &value) const
-		{
-			return _value + static_cast<base::Fraction>(value);
-		}
-
-		base::Mbps operator-(Mbps const &value) const
-		{
-			return _value - static_cast<base::Fraction>(value);
-		}
-
-		base::Mbps operator*(Mbps const &value) const
-		{
-			return _value * static_cast<base::Fraction>(value);
-		}
-
-		base::Mbps operator/(Mbps const &value) const
-		{
-			return _value / static_cast<base::Fraction>(value);
-		}
-
-		base::Mbps &operator+=(Mbps const &value)
-		{
-			_value += static_cast<base::Fraction>(value);
-			return *this;
-		}
-
-		base::Mbps &operator-=(Mbps const &value)
-		{
-			_value -= static_cast<base::Fraction>(value);
-			return *this;
-		}
-
-		base::Mbps &operator*=(Mbps const &value)
-		{
-			_value *= static_cast<base::Fraction>(value);
-			return *this;
-		}
-
-		base::Mbps &operator/=(Mbps const &value)
-		{
-			_value /= static_cast<base::Fraction>(value);
-			return *this;
-		}
-
-		base::Mbps operator+(base::Bps const &value) const
-		{
-			return *this + base::Mbps{value};
-		}
-
-		base::Mbps operator-(base::Bps const &value) const
-		{
-			return *this - base::Mbps{value};
-		}
-
-		base::Mbps operator*(base::Bps const &value) const
-		{
-			return *this * base::Mbps{value};
-		}
-
-		base::Mbps operator/(base::Bps const &value) const
-		{
-			return *this / base::Mbps{value};
-		}
-
-		base::Mbps &operator+=(base::Bps const &value)
-		{
-			*this += base::Mbps{value};
-			return *this;
-		}
-
-		base::Mbps &operator-=(base::Bps const &value)
-		{
-			*this -= base::Mbps{value};
-			return *this;
-		}
-
-		base::Mbps &operator*=(base::Bps const &value)
-		{
-			*this *= base::Mbps{value};
-			return *this;
-		}
-
-		base::Mbps &operator/=(base::Bps const &value)
-		{
-			*this /= base::Mbps{value};
-			return *this;
-		}
-
-#pragma endregion
-
-#pragma region 比较运算符
-
-		bool operator==(Mbps const &value) const
-		{
-			return _value == value._value;
-		}
-
-		bool operator<(Mbps const &value) const
-		{
-			return _value < value._value;
-		}
-
-		bool operator>(Mbps const &value) const
-		{
-			return _value > value._value;
-		}
-
-		bool operator<=(Mbps const &value) const
-		{
-			return _value <= value._value;
-		}
-
-		bool operator>=(Mbps const &value) const
-		{
-			return _value >= value._value;
-		}
-
-		bool operator==(base::Bps const &value) const
-		{
-			return *this == base::Mbps{value};
-		}
-
-		bool operator<(base::Bps const &value) const
-		{
-			return *this < base::Mbps{value};
-		}
-
-		bool operator>(base::Bps const &value) const
-		{
-			return *this > base::Mbps{value};
-		}
-
-		bool operator<=(base::Bps const &value) const
-		{
-			return *this <= base::Mbps{value};
-		}
-
-		bool operator>=(base::Bps const &value) const
-		{
-			return *this >= base::Mbps{value};
-		}
-
-#pragma endregion
-
-		/// @brief 转化为字符串
-		/// @return
-		std::string ToString() const override
-		{
-			return _value.ToString();
-		}
 	};
 } // namespace base
-
-std::ostream &operator<<(std::ostream &ostream, base::Mbps const &right);
-base::Mbps operator+(int64_t left, base::Mbps const &right);
-base::Mbps operator-(int64_t left, base::Mbps const &right);
-base::Mbps operator*(int64_t left, base::Mbps const &right);
-base::Mbps operator/(int64_t left, base::Mbps const &right);
-
-namespace std
-{
-	/// @brief 向下取整
-	/// @param value
-	/// @return
-	int64_t floor(base::Mbps const &value);
-
-	/// @brief 向上取整
-	/// @param value
-	/// @return
-	int64_t ceil(base::Mbps const &value);
-} // namespace std
