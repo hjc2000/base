17d969c148aa12059b9941117d1b3732fc803784
hjc2000 <hjc.666@foxmail.com>
2025-01-16 17:25:16 +0800

    #pragma region 比较运算符

--- include/base/unit/IUnit.h
+++ include/base/unit/IUnit.h
@@ -111,40 +111,6 @@ namespace base
 
 #pragma endregion
 
-#pragma region 比较运算符
-
-		template <typename T>
-		bool operator==(T const &value) const
-		{
-			return Value() == TSelf{value}.Value();
-		}
-
-		template <typename T>
-		bool operator<(T const &value) const
-		{
-			return Value() < TSelf{value}.Value();
-		}
-
-		template <typename T>
-		bool operator>(T const &value) const
-		{
-			return Value() > TSelf{value}.Value();
-		}
-
-		template <typename T>
-		bool operator<=(T const &value) const
-		{
-			return Value() <= TSelf{value}.Value();
-		}
-
-		template <typename T>
-		bool operator>=(T const &value) const
-		{
-			return Value() >= TSelf{value}.Value();
-		}
-
-#pragma endregion
-
 		/// @brief 转化为字符串
 		/// @return
 		std::string ToString() const override
@@ -153,6 +119,40 @@ namespace base
 		}
 	};
 } // namespace base
+
+#pragma region 比较运算符
+
+template <typename TLeft, typename TRight>
+bool operator==(base::IUnit<TLeft> const &left, base::IUnit<TRight> const &right)
+{
+	return static_cast<base::Fraction>(left) == static_cast<base::Fraction>(TLeft{right});
+}
+
+template <typename TLeft, typename TRight>
+bool operator<(base::IUnit<TLeft> const &left, base::IUnit<TRight> const &right)
+{
+	return static_cast<base::Fraction>(left) < static_cast<base::Fraction>(TLeft{right});
+}
+
+template <typename TLeft, typename TRight>
+bool operator>(base::IUnit<TLeft> const &left, base::IUnit<TRight> const &right)
+{
+	return static_cast<base::Fraction>(left) > static_cast<base::Fraction>(TLeft{right});
+}
+
+template <typename TLeft, typename TRight>
+bool operator<=(base::IUnit<TLeft> const &left, base::IUnit<TRight> const &right)
+{
+	return static_cast<base::Fraction>(left) <= static_cast<base::Fraction>(TLeft{right});
+}
+
+template <typename TLeft, typename TRight>
+bool operator>=(base::IUnit<TLeft> const &left, base::IUnit<TRight> const &right)
+{
+	return static_cast<base::Fraction>(left) >= static_cast<base::Fraction>(TLeft{right});
+}
+
+#pragma endregion
 
 /// @brief 将 IUnit 输出到输出流。
 /// @tparam T
