805cd5bcee352e3a7f0c9879bb592d0759ea0161
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-11 21:23:14 +0800

    base::

--- include/base/net/modbus/ModbusCrc16.cpp
+++ include/base/net/modbus/ModbusCrc16.cpp
@@ -1,13 +1,11 @@
 #include "ModbusCrc16.h"
 
-using namespace base;
-
-ModbusCrc16::ModbusCrc16(uint16_t polynomial)
+base::ModbusCrc16::ModbusCrc16(uint16_t polynomial)
 	: _polynomial(polynomial)
 {
 }
 
-void ModbusCrc16::Add(uint8_t data)
+void base::ModbusCrc16::Add(uint8_t data)
 {
 	_crc16_register ^= static_cast<uint16_t>(data);
 	for (int i = 0; i < 8; i++)
@@ -42,17 +40,17 @@ void base::ModbusCrc16::ResetRegister()
 	_crc16_register = UINT16_MAX;
 }
 
-uint16_t ModbusCrc16::RegisterValue()
+uint16_t base::ModbusCrc16::RegisterValue()
 {
 	return _crc16_register;
 }
 
-uint8_t ModbusCrc16::RegisterValueHighByte()
+uint8_t base::ModbusCrc16::RegisterValueHighByte()
 {
 	return static_cast<uint8_t>(_crc16_register >> 8);
 }
 
-uint8_t ModbusCrc16::RegisterValueLowByte()
+uint8_t base::ModbusCrc16::RegisterValueLowByte()
 {
 	return static_cast<uint8_t>(_crc16_register);
 }
