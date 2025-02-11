f0d4e723aa6568378fc263e98c0a164486ecdeab
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-02-12 00:12:30 +0800

    IEnumerable

--- include/base/container/ArraySpan.h
+++ include/base/container/ArraySpan.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <algorithm>
+#include <base/container/iterator/IEnumerable.h>
 #include <base/container/Range.h>
 #include <stdexcept>
 #include <stdint.h>
@@ -20,11 +21,70 @@ namespace base
 	/// @note 引用一段内存，不会持有这段内存，不管理这段内存的生命周期。
 	/// @tparam ItemType
 	template <typename ItemType>
-	class ReadOnlyArraySpan
+	class ReadOnlyArraySpan :
+		public base::IEnumerable<ItemType const>
 	{
 	private:
 		ItemType const *_buffer = nullptr;
 		int32_t _count = 0;
+
+#pragma region 迭代器
+
+		class Enumerator :
+			public base::IEnumerator<ItemType const>
+		{
+		private:
+			int _index = 0;
+			bool _first_move = true;
+			ItemType const *_buffer{};
+			int32_t _count = 0;
+
+		public:
+			Enumerator(ItemType const *buffer, int32_t count)
+			{
+				_buffer = buffer;
+				_count = count;
+				Reset();
+			}
+
+			/// @brief 获取当前值的引用
+			/// @return
+			ItemType const &CurrentValue() override
+			{
+				return _buffer[_index];
+			}
+
+			/// @brief 迭代器前进到下一个值
+			/// @return
+			bool MoveNext() override
+			{
+				if (_first_move)
+				{
+					_first_move = false;
+				}
+				else
+				{
+					_index++;
+				}
+
+				if (_index < 0 || _index >= _count)
+				{
+					return false;
+				}
+
+				return true;
+			}
+
+			/// @brief 将迭代器重置到容器开始的位置。
+			/// @note 开始位置是第一个元素前。也就是说重置后，要调用一次 MoveNext 才能获取到第一个值。
+			void Reset() override
+			{
+				_first_move = true;
+				_index = 0;
+			}
+		};
+
+#pragma endregion
 
 	public:
 		ReadOnlyArraySpan() = default;
@@ -67,6 +127,15 @@ namespace base
 
 			return base::ReadOnlyArraySpan<ItemType>{Buffer() + range.Begin(), range.Size()};
 		}
+
+		/// @brief 获取迭代器
+		/// @return
+		std::shared_ptr<IEnumerator<ItemType const>> GetEnumerator() override
+		{
+			return std::shared_ptr<IEnumerator<ItemType const>>{
+				new Enumerator{Buffer(), Count()},
+			};
+		}
 	};
 
 #pragma endregion
@@ -77,11 +146,70 @@ namespace base
 	/// @note 引用一段内存，不会持有这段内存，不管理这段内存的生命周期。
 	/// @tparam ItemType
 	template <typename ItemType>
-	class ArraySpan
+	class ArraySpan :
+		public base::IEnumerable<ItemType>
 	{
 	private:
 		ItemType *_buffer = nullptr;
 		int32_t _count = 0;
+
+#pragma region 迭代器
+
+		class Enumerator :
+			public base::IEnumerator<ItemType>
+		{
+		private:
+			int _index = 0;
+			bool _first_move = true;
+			ItemType *_buffer{};
+			int32_t _count = 0;
+
+		public:
+			Enumerator(ItemType *buffer, int32_t count)
+			{
+				_buffer = buffer;
+				_count = count;
+				Reset();
+			}
+
+			/// @brief 获取当前值的引用
+			/// @return
+			ItemType &CurrentValue() override
+			{
+				return _buffer[_index];
+			}
+
+			/// @brief 迭代器前进到下一个值
+			/// @return
+			bool MoveNext() override
+			{
+				if (_first_move)
+				{
+					_first_move = false;
+				}
+				else
+				{
+					_index++;
+				}
+
+				if (_index < 0 || _index >= _count)
+				{
+					return false;
+				}
+
+				return true;
+			}
+
+			/// @brief 将迭代器重置到容器开始的位置。
+			/// @note 开始位置是第一个元素前。也就是说重置后，要调用一次 MoveNext 才能获取到第一个值。
+			void Reset() override
+			{
+				_first_move = true;
+				_index = 0;
+			}
+		};
+
+#pragma endregion
 
 	public:
 		ArraySpan() = default;
@@ -141,6 +269,15 @@ namespace base
 			}
 
 			return base::ArraySpan<ItemType>{Buffer() + range.Begin(), range.Size()};
+		}
+
+		/// @brief 获取迭代器
+		/// @return
+		std::shared_ptr<IEnumerator<ItemType>> GetEnumerator() override
+		{
+			return std::shared_ptr<IEnumerator<ItemType>>{
+				new Enumerator{Buffer(), Count()},
+			};
 		}
 	};
 
