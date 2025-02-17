8b992040bebcae1d2d05e8fc90e6dc898ebb9403
hjc2000 <hjc.666@foxmail.com>
2025-02-17 11:19:17 +0800

    SetRealTime

--- include/base/time/time.h
+++ include/base/time/time.h
@@ -12,5 +12,11 @@ namespace base
 		/// @brief 获取稳定时钟的当前时刻。
 		/// @return
 		base::TimePointSinceEpoch SteadyClockNow();
+
+		/// @brief 设置系统的时间。
+		/// @note 需要管理员权限。
+		/// @note 操作系统自带时区功能，所以设置时间时不要手动处理时区带来的时间偏移。
+		/// @param value
+		void SetRealTime(base::TimePointSinceEpoch const &value);
 	} // namespace time
 } // namespace base
