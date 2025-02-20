ed057996232554558f85559cd7206cf6e0c00984
hjc2000 <hjc.666@foxmail.com>
2025-02-20 14:53:54 +0800

    TimePointSinceEpoch

--- include/base/time/TimePointSinceEpoch.h
+++ include/base/time/TimePointSinceEpoch.h
@@ -3,18 +3,25 @@
 
 namespace base
 {
-	/// @brief 以 epoch 时刻作为零点的时刻。
-	/// @note epoch 时刻是指 1970 年 1 月 1 日 00:00:00 UTC。
-	/// @note 时刻就像电势那样没有绝对零点，只能选定一个参考的零点。电路中通常选择电源负极作为电势零点，
-	/// 本类选择 epoch 时刻作为时刻的零点。
+	/**
+	 * @brief 以 epoch 时刻作为零点的时刻。
+	 *
+	 * @note epoch 时刻是指 1970 年 1 月 1 日 00:00:00 UTC。
+	 *
+	 * @note 时刻就像电势那样没有绝对零点，只能选定一个参考的零点。电路中通常选择电源负极作为电势零点，
+	 * 本类选择 epoch 时刻作为时刻的零点。
+	 */
 	class TimePointSinceEpoch
 	{
 	private:
 		std::chrono::nanoseconds _time_since_epoch{};
 
 	public: // 构造函数
-		/// @brief 无参构造。构造出来的时间刚好就是 epoch 时刻。
-		/// @note epoch 时刻是指 1970 年 1 月 1 日 00:00:00 UTC。
+		/**
+		 * @brief 参构造。构造出来的时间刚好就是 epoch 时刻。
+		 *
+		 * @note epoch 时刻是指 1970 年 1 月 1 日 00:00:00 UTC。
+		 */
 		TimePointSinceEpoch();
 
 		explicit TimePointSinceEpoch(std::chrono::nanoseconds const &value);
