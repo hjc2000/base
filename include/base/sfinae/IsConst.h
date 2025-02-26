5ff764f5fe7015a2eaf9b3f5924a2c6cf46268ae
hjc2000 <hjc.666@foxmail.com>
2025-02-26 13:51:55 +0800

    注释

--- include/base/sfinae/IsConst.h
+++ include/base/sfinae/IsConst.h
@@ -3,13 +3,17 @@
 
 namespace base
 {
-	/// @brief 检查一个类型是否是 const
-	/// @note 通过模板全特化实现。这里是没有特化的版本，Value 属性是 false，
-	/// 再定义一个特化版本，匹配 Type const，特化版本的 Value 属性是 true。
-	/// 这样就能通过匹配不同的特化版本来检查一个类型是否是 const。
-	/// @tparam Type
+	/**
+	 * @brief 检查一个类型是否是 const
+	 *
+	 * @note 通过模板全特化实现。这里是没有特化的版本，Value 属性是 false，
+	 * 再定义一个特化版本，匹配 Type const，特化版本的 Value 属性是 true。
+	 * 这样就能通过匹配不同的特化版本来检查一个类型是否是 const。
+	 *
+	 * @tparam Type
+	 */
 	template <typename Type>
-	struct IsConst
+	struct is_const
 	{
 		static_function consteval bool Value()
 		{
@@ -17,12 +21,16 @@ namespace base
 		}
 	};
 
-	/// @brief 检查一个类型是否是 const。
-	/// @note 这里是 Type const 的特化版本。使用时，当尖括号中的类型是 const 的时，
-	/// 就会匹配到本特化版本，而不是上方的非特化版本。
-	/// @tparam Type
+	/**
+	 * @brief 检查一个类型是否是 const。
+	 *
+	 * @note 这里是 Type const 的特化版本。使用时，当尖括号中的类型是 const 的时，
+	 * 就会匹配到本特化版本，而不是上方的非特化版本。
+	 *
+	 * @tparam Type
+	 */
 	template <typename Type>
-	struct IsConst<Type const>
+	struct is_const<Type const>
 	{
 		static_function consteval bool Value()
 		{
@@ -30,12 +38,15 @@ namespace base
 		}
 	};
 
-	/// @brief 封装 IsConst 模板
-	/// @tparam T
-	/// @return
+	/**
+	 * @brief 封装 is_const 模板
+	 *
+	 * @tparam T
+	 * @return consteval
+	 */
 	template <typename T>
 	consteval bool IsConstType()
 	{
-		return IsConst<T>::Value();
+		return is_const<T>::Value();
 	}
 } // namespace base
