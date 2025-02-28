ca852b3774fa91292b6305d88543a61366da351b
hjc2000 <hjc.666@foxmail.com>
2025-02-28 11:36:22 +0800

    IJsonDserializable

--- include/base/string/Json.cpp
+++ include/base/string/Json.cpp
@@ -4,3 +4,8 @@ std::string base::IJsonSerializable::ToString() const
 {
 	return ToJson().dump(4);
 }
+
+void base::IJsonDserializable::FromJsonString(std::string const &json_string)
+{
+	FromJson(Json::parse(json_string));
+}
