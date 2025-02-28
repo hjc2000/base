1b1069673b307b70725792e05066df35011adbf9
hjc2000 <hjc.666@foxmail.com>
2025-02-28 11:41:40 +0800

    ICanToString

--- include/base/string/ICanToString.h
+++ include/base/string/ICanToString.h
@@ -4,15 +4,25 @@
 
 namespace base
 {
-	/// @brief 继承此接口表示能够转化为字符串。
+	/**
+	 * @brief 继承此接口表示能够转化为字符串。
+	 *
+	 */
 	class ICanToString
 	{
 	public:
-		/// @brief 转化为字符串
-		/// @return
+		/**
+		 * @brief 转化为字符串
+		 *
+		 * @return std::string
+		 */
 		virtual std::string ToString() const = 0;
 
-		/// @brief 重载强制转换为字符串的运算符。
+		/**
+		 * @brief 重载强制转换为字符串的运算符。
+		 *
+		 * @return std::string
+		 */
 		explicit operator std::string() const
 		{
 			return ToString();
@@ -20,9 +30,22 @@ namespace base
 	};
 } // namespace base
 
+/**
+ * @brief 将 base::ICanToString 对象转为字符串后输出到输出流。
+ *
+ * @param stream
+ * @param o
+ * @return std::ostream&
+ */
 std::ostream &operator<<(std::ostream &stream, base::ICanToString const &o);
 
 namespace std
 {
+	/**
+	 * @brief 将 base::ICanToString 对象转为字符串。
+	 *
+	 * @param o
+	 * @return std::string
+	 */
 	std::string to_string(base::ICanToString const &o);
-}
+} // namespace std
