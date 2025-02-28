87ec5184aa4d3daa01a84ca69fc1f88520f2862e
hjc2000 <hjc.666@foxmail.com>
2025-02-28 15:00:42 +0800

    注释

--- include/base/task/CancellationTokenSource.h
+++ include/base/task/CancellationTokenSource.h
@@ -3,19 +3,35 @@
 
 namespace base
 {
-	/// @brief 取消令牌源
+	/**
+	 * @brief 取消令牌源
+	 *
+	 */
 	class CancellationTokenSource final
 	{
 	private:
 		std::shared_ptr<CancellationToken> _token{new CancellationToken{}};
 
 	public:
+		/**
+		 * @brief 检查任务是否已经取消了。
+		 *
+		 * @return true 已经取消。
+		 * @return false 还没取消。
+		 */
 		bool IsCancellationRequested() const;
+
+		/**
+		 * @brief 取消令牌
+		 *
+		 * @return std::shared_ptr<CancellationToken>
+		 */
 		std::shared_ptr<CancellationToken> Token() const;
 
-		/// @brief 取消
-		/// @warning 没有将宏变量 HAS_THREAD 定义为真的话，本方法禁止在多个线程中调用，
-		/// 因为此时缺少互斥锁保护。
+		/**
+		 * @brief 取消任务。
+		 *
+		 */
 		void Cancel();
 	};
 } // namespace base
