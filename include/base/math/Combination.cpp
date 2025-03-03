b8cba1b7f24fa6b754f0e045d22f85b531957a41
hjc2000 <hjc.666@foxmail.com>
2025-03-03 11:57:47 +0800

    Combination

---
+++ include/base/math/Combination.cpp
@@ -0,0 +1,7 @@
+#include "Combination.h"
+
+base::Combination::Combination(int n, int m)
+	: _n(n),
+	  _m(m)
+{
+}
---
+++ include/base/math/Combination.h
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace base
+{
+	/**
+	 * @brief 组合。
+	 *
+	 */
+	class Combination
+	{
+	private:
+		int _n = 0;
+		int _m = 0;
+
+	public:
+		Combination(int n, int m);
+	};
+} // namespace base
