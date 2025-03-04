c42471e5c931bef7be32a576d8167d9df2048754
hjc2000 <hjc.666@foxmail.com>
2025-03-04 15:10:40 +0800

    LineSegmentPath

--- include/base/math/LineSegmentPath.cpp
+++ include/base/math/LineSegmentPath.cpp
@@ -2,21 +2,44 @@
 #include "base/string/define.h"
 #include <stdexcept>
 
-base::LineSegmentPath::LineSegmentPath(base::Set<std::string> const &points)
-	: _points(points)
+base::LineSegmentPath::LineSegmentPath(base::List<std::string> const &points)
 {
-	if (_points.Count() < 2)
+	if (points.Count() < 2)
 	{
 		throw std::invalid_argument{CODE_POS_STR + "必须至少有 2 个点，并且第一个点和最后一个点必须是端点。"};
 	}
+
+	for (std::string const &point : points)
+	{
+		AddPoint(point);
+	}
 }
 
-base::Set<std::string> &base::LineSegmentPath::Points()
+base::LineSegmentPath::LineSegmentPath(std::initializer_list<std::string> const &points)
+{
+	if (points.size() < 2)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "必须至少有 2 个点，并且第一个点和最后一个点必须是端点。"};
+	}
+
+	for (std::string const &point : points)
+	{
+		AddPoint(point);
+	}
+}
+
+base::List<std::string> const &base::LineSegmentPath::Points() const
 {
 	return _points;
 }
 
-base::Set<std::string> const &base::LineSegmentPath::Points() const
+bool base::LineSegmentPath::AddPoint(std::string const &point)
 {
-	return _points;
+	if (_points.Contains(point))
+	{
+		return false;
+	}
+
+	_points.Add(point);
+	return true;
 }
--- include/base/math/LineSegmentPath.h
+++ include/base/math/LineSegmentPath.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <base/container/Set.h>
+#include <base/container/List.h>
+#include <initializer_list>
 #include <string>
 
 namespace base
@@ -11,7 +12,7 @@ namespace base
 	class LineSegmentPath
 	{
 	private:
-		base::Set<std::string> _points;
+		base::List<std::string> _points;
 
 	public:
 		/**
@@ -20,21 +21,31 @@ namespace base
 		 * @param points 这条线段上有哪些点。
 		 * @note 必须至少有 2 个点。
 		 */
-		LineSegmentPath(base::Set<std::string> const &points);
+		LineSegmentPath(base::List<std::string> const &points);
+
+		/**
+		 * @brief 构造一条线段。
+		 *
+		 * @param points 这条线段上有哪些点。
+		 * @note 必须至少有 2 个点。
+		 */
+		LineSegmentPath(std::initializer_list<std::string> const &points);
 
 	public:
 		/**
 		 * @brief 获取本路径中的点。
 		 *
-		 * @return base::Set<std::string>&
-		 */
-		base::Set<std::string> &Points();
-
-		/**
-		 * @brief 获取本路径中的点。
-		 *
 		 * @return base::List<std::string> const&
 		 */
-		base::Set<std::string> const &Points() const;
+		base::List<std::string> const &Points() const;
+
+		/**
+		 * @brief 添加一个点到路径末尾。
+		 *
+		 * @param point
+		 * @return true 路径中没有该点，添加成功
+		 * @return false 路径中已经有该点了，添加失败。
+		 */
+		bool AddPoint(std::string const &point);
 	};
 } // namespace base
