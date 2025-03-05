680ed10d7fb2831ce57e0af5b6e25d49ee31ffdc
hjc2000 <hjc.666@foxmail.com>
2025-03-05 13:25:11 +0800

    Malloc

---
+++ include/base/Malloc.cpp
@@ -0,0 +1,1 @@
+#include "Malloc.h"
---
+++ include/base/Malloc.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstddef>
+#include <cstdlib>
+
+/**
+ * @brief 分配特定类型的大小的内存。
+ *
+ * @tparam T
+ * @param size
+ * @return T*
+ */
+template <typename T>
+T *Malloc(size_t size)
+{
+	return reinterpret_cast<T *>(malloc(size));
+}
