807fcc4d7e980dd1fb75ee5e40e233efe56c55d7
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-05 18:51:57 +0800

    IEnumerable

--- include/base/container/iterator/IEnumerable.h
+++ include/base/container/iterator/IEnumerable.h
@@ -1,6 +1,5 @@
 #pragma once
 #include <base/container/iterator/IEnumerator.h>
-#include <base/container/iterator/IEnumeratorForwardIterator.h>
 #include <base/container/iterator/IForwardIterator.h>
 #include <memory>
 
@@ -14,56 +13,6 @@ namespace base
 	template <typename ItemType>
 	class IEnumerable
 	{
-	private:
-		/// @brief 将 IEnumerator 包装为 C++ 迭代器
-		/// @tparam item_type
-		template <typename item_type>
-		class Enumerator :
-			public base::IForwardIterator<Enumerator<item_type>, item_type>
-		{
-		private:
-			std::shared_ptr<IEnumerator<item_type>> _enumertor;
-
-		public:
-			/// @brief 将 IEnumerator 包装为 C++ 迭代器
-			/// @param enumertor
-			Enumerator(std::shared_ptr<IEnumerator<item_type>> enumertor)
-			{
-				_enumertor = enumertor;
-			}
-
-			/// @brief 解引用
-			/// @return
-			item_type &operator*() override
-			{
-				return _enumertor->CurrentValue();
-			}
-
-			/// @brief 前缀递增
-			/// @return
-			Enumerator<item_type> &operator++() override
-			{
-				// 不做任何操作，因为 == 运算符里面已经 MoveNext 了。
-				return *this;
-			}
-
-			/// @brief 相等运算符
-			/// @param o
-			/// @return
-			bool operator==(Enumerator<item_type> const &o) const override
-			{
-				/* C++ 范围 for 的机制是先判断不等于 end() 然后解引用。然后递增，进入下一循环。
-				 * IEnumerator 的操作顺序是先 MoveNext 然后判断是否成功，成功再利用 CurrentValue
-				 * 获取引用。
-				 *
-				 * 让 == 运算符返回 !_enumertor->MoveNext()，这样，范围 for 的判断不等于 end() 的
-				 * 步骤就会触发移动，并得到是否移动成功的结果，移动失败，就返回 true ，让范围 for 认为
-				 * 当前等于 end() ，于是结束迭代。
-				 */
-				return !_enumertor->MoveNext();
-			}
-		};
-
 	private:
 		/// @brief const 迭代器
 		/// @tparam item_type
@@ -97,6 +46,56 @@ namespace base
 		};
 
 	public:
+		/// @brief 将 IEnumerator 包装为 C++ 迭代器
+		/// @tparam item_type
+		template <typename item_type>
+		class ForwardIterator :
+			public base::IForwardIterator<ForwardIterator<item_type>, item_type>
+		{
+		private:
+			std::shared_ptr<IEnumerator<item_type>> _enumertor;
+
+		public:
+			/// @brief 将 IEnumerator 包装为 C++ 迭代器
+			/// @param enumertor
+			ForwardIterator(std::shared_ptr<IEnumerator<item_type>> enumertor)
+			{
+				_enumertor = enumertor;
+			}
+
+			/// @brief 解引用
+			/// @return
+			item_type &operator*() override
+			{
+				return _enumertor->CurrentValue();
+			}
+
+			/// @brief 前缀递增
+			/// @return
+			ForwardIterator<item_type> &operator++() override
+			{
+				// 不做任何操作，因为 == 运算符里面已经 MoveNext 了。
+				return *this;
+			}
+
+			/// @brief 相等运算符
+			/// @param o
+			/// @return
+			bool operator==(ForwardIterator<item_type> const &o) const override
+			{
+				/* C++ 范围 for 的机制是先判断不等于 end() 然后解引用。然后递增，进入下一循环。
+				 * IEnumerator 的操作顺序是先 MoveNext 然后判断是否成功，成功再利用 CurrentValue
+				 * 获取引用。
+				 *
+				 * 让 == 运算符返回 !_enumertor->MoveNext()，这样，范围 for 的判断不等于 end() 的
+				 * 步骤就会触发移动，并得到是否移动成功的结果，移动失败，就返回 true ，让范围 for 认为
+				 * 当前等于 end() ，于是结束迭代。
+				 */
+				return !_enumertor->MoveNext();
+			}
+		};
+
+	public:
 		/**
 		 * @brief 获取非 const 迭代器
 		 *
@@ -119,24 +118,24 @@ namespace base
 			};
 		}
 
-		Enumerator<ItemType> begin()
+		ForwardIterator<ItemType> begin()
 		{
-			return Enumerator<ItemType>{GetEnumerator()};
+			return ForwardIterator<ItemType>{GetEnumerator()};
 		}
 
-		Enumerator<ItemType> end()
+		ForwardIterator<ItemType> end()
 		{
-			return Enumerator<ItemType>{GetEnumerator()};
+			return ForwardIterator<ItemType>{GetEnumerator()};
 		}
 
-		Enumerator<ItemType const> begin() const
+		ForwardIterator<ItemType const> begin() const
 		{
-			return Enumerator<ItemType const>{GetEnumerator()};
+			return ForwardIterator<ItemType const>{GetEnumerator()};
 		}
 
-		Enumerator<ItemType const> end() const
+		ForwardIterator<ItemType const> end() const
 		{
-			return Enumerator<ItemType const>{GetEnumerator()};
+			return ForwardIterator<ItemType const>{GetEnumerator()};
 		}
 	};
 } // namespace base
--- include/base/container/iterator/IEnumeratorForwardIterator.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <base/container/iterator/IEnumerator.h>
-#include <base/container/iterator/IForwardIterator.h>
-#include <memory>
-
-namespace base
-{
-} // namespace base
