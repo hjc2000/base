c4bdce1f878728a2d3e647d932ed892c88e530ad
hjc2000 <hjc.666@foxmail.com>
2025-03-06 16:50:41 +0800

    E2

--- include/base/net/profidrive/data-type/E2.cpp
+++ include/base/net/profidrive/data-type/E2.cpp
@@ -5,40 +5,35 @@ int32_t base::profidrive::E2::Factor() const
 	return static_cast<int64_t>(1 << 7);
 }
 
-base::profidrive::E2::E2(base::ReadOnlySpan const &value)
-{
-	Span().CopyFrom(value);
-}
-
-base::profidrive::E2::E2(base::Fraction const &value)
-{
-	/* 行规特定数据类型用一个整型来储存它的值，这个整型值可以认为是将分数的实际值乘上 Factor
-	 * 放大后截断为整型。
-	 */
-	int16_t result = static_cast<int16_t>(value * Factor());
-
-	// 得到整型后要转换为大端序。
-	_converter.GetBytes(result, Span());
-}
-
-base::Span base::profidrive::E2::Span()
-{
-	return _buffer.Span();
-}
-
-base::ReadOnlySpan base::profidrive::E2::Span() const
-{
-	return const_cast<E2 *>(this)->Span();
-}
-
-base::profidrive::E2::operator base::Fraction() const
+base::profidrive::E2::E2(base::ReadOnlySpan const &span)
 {
 	/* 行规特定数据类型用一个整型来储存它的值，这个整型值可以认为是将分数的实际值乘上 Factor
 	 * 放大后截断为整型。
 	 *
 	 * 想要获得分数的实际值，就将这个整型除以 Factor.
 	 */
+	int32_t e2 = _converter.ToInt16(span);
+	_value = base::Fraction{e2, Factor()};
+}
 
-	int16_t value = _converter.ToInt16(Span());
-	return value * base::Fraction{value, Factor()};
+base::profidrive::E2::E2(base::Fraction const &value)
+{
+	_value = value;
+}
+
+base::profidrive::E2::operator base::Fraction() const
+{
+	return _value;
+}
+
+base::Array<uint8_t, 2> base::profidrive::E2::BufferForSending() const
+{
+	/* 行规特定数据类型用一个整型来储存它的值，这个整型值可以认为是将分数的实际值乘上 Factor
+	 * 放大后截断为整型。
+	 */
+	int16_t e2 = static_cast<int16_t>(_value * Factor());
+
+	base::Array<uint8_t, 2> buffer;
+	_converter.GetBytes(e2, buffer.Span());
+	return buffer;
 }
--- include/base/net/profidrive/data-type/E2.h
+++ include/base/net/profidrive/data-type/E2.h
@@ -14,11 +14,7 @@ namespace base
 		class E2
 		{
 		private:
-			/**
-			 * @brief E2 字节序列。
-			 * @note 储存到这里的必须是是大端序，可以直接发送。
-			 */
-			base::Array<uint8_t, 2> _buffer{};
+			base::Fraction _value;
 
 			/**
 			 * @brief 自适应转换器。
@@ -43,9 +39,9 @@ namespace base
 			/**
 			 * @brief 从 profinet 收到大端序的数据后原封不动地将字节序列传进来。
 			 *
-			 * @param value
+			 * @param span
 			 */
-			E2(base::ReadOnlySpan const &value);
+			E2(base::ReadOnlySpan const &span);
 
 			/**
 			 * @brief 通过实际的分数值构造行规特定数据类型。
@@ -56,25 +52,58 @@ namespace base
 
 		public:
 			/**
-			 * @brief 获取可以直接被用来发送的字节序列。已经是大端序了。
-			 *
-			 * @return base::Span
-			 */
-			base::Span Span();
-
-			/**
-			 * @brief 获取可以直接被用来发送的字节序列。已经是大端序了。
-			 *
-			 * @return base::ReadOnlySpan
-			 */
-			base::ReadOnlySpan Span() const;
-
-			/**
 			 * @brief 强制转换为分数类型。
 			 *
 			 * @return base::Fraction
 			 */
 			explicit operator base::Fraction() const;
+
+			base::Array<uint8_t, 2> BufferForSending() const;
+
+		public:
+			E2 operator+(E2 const &right_value) const
+			{
+				return base::profidrive::E2{_value + right_value._value};
+			}
+
+			E2 operator-(E2 const &right_value) const
+			{
+				return base::profidrive::E2{_value - right_value._value};
+			}
+
+			E2 operator*(E2 const &right_value) const
+			{
+				return base::profidrive::E2{_value * right_value._value};
+			}
+
+			E2 operator/(E2 const &right_value) const
+			{
+				return base::profidrive::E2{_value / right_value._value};
+			}
+
+			E2 &operator+=(E2 const &right_value)
+			{
+				_value += right_value._value;
+				return *this;
+			}
+
+			E2 &operator-=(E2 const &right_value)
+			{
+				_value -= right_value._value;
+				return *this;
+			}
+
+			E2 &operator*=(E2 const &right_value)
+			{
+				_value *= right_value._value;
+				return *this;
+			}
+
+			E2 &operator/=(E2 const &right_value)
+			{
+				_value /= right_value._value;
+				return *this;
+			}
 		};
 	} // namespace profidrive
 } // namespace base
