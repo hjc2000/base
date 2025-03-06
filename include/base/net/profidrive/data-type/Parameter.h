a87d5a50acfb8e7451cc2feed9277e5ab0bff50a
hjc2000 <hjc.666@foxmail.com>
2025-03-06 14:39:11 +0800

    Parameter

--- include/base/net/profidrive/data-type/Parameter.h
+++ include/base/net/profidrive/data-type/Parameter.h
@@ -1,5 +1,6 @@
 #pragma once
-#include <base/net/profidrive/data-type/ParameterValue.h>
+#include <cstdint>
+#include <vector>
 
 namespace base
 {
@@ -7,7 +8,21 @@ namespace base
 	{
 		class Parameter
 		{
+		private:
+			std::vector<uint8_t> _parameter_values_buffer;
+
 		public:
+			/**
+			 * @brief 参数值缓冲区。
+			 *
+			 * @note 因为无法确定参数类型，所以使用缓冲区，让应用层自行转换为具体类型。
+			 *
+			 * @return std::vector<uint8_t>&
+			 */
+			std::vector<uint8_t> &ParameterValuesBuffer()
+			{
+				return _parameter_values_buffer;
+			}
 		};
 	} // namespace profidrive
 } // namespace base
