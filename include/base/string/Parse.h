f82404688823dc1448acc9b26ba612cd58fca6e2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-08 00:58:09 +0800

    注释

--- include/base/string/Parse.h
+++ include/base/string/Parse.h
@@ -4,54 +4,57 @@
 
 namespace base
 {
-	/**
-	 * @brief 从字符串中解析 int32_t.
-	 *
-	 * @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
-	 * @return int32_t
-	 */
+	///
+	/// @brief 从字符串中解析 int32_t.
+	///
+	/// @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
+	/// @return int32_t
+	///
 	int32_t ParseInt32(base::String const &str);
 
-	/**
-	 * @brief 从字符串中解析 int32_t.
-	 *
-	 * @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
-	 * @param base 指定字符串中数字的进制数。
-	 * @return int32_t
-	 */
+	///
+	/// @brief 从字符串中解析 int32_t.
+	///
+	/// @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
+	/// @param base 指定字符串中数字的进制数。
+	///
+	/// @return int32_t
+	///
 	int32_t ParseInt32(base::String const &str, int32_t base);
 
-	/**
-	 * @brief 从字符串中解析 int64_t.
-	 *
-	 * @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
-	 * @return int64_t
-	 */
+	///
+	/// @brief 从字符串中解析 int64_t.
+	///
+	/// @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
+	/// @return int64_t
+	///
 	int64_t ParseInt64(base::String const &str);
 
-	/**
-	 * @brief 从字符串中解析 int64_t.
-	 *
-	 * @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
-	 * @param base 指定字符串中数字的进制数。
-	 * @return int64_t
-	 */
+	///
+	/// @brief 从字符串中解析 int64_t.
+	///
+	/// @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
+	/// @param base 指定字符串中数字的进制数。
+	///
+	/// @return int64_t
+	///
 	int64_t ParseInt64(base::String const &str, int32_t base);
 
-	/**
-	 * @brief 从字符串中解析 double.
-	 *
-	 * @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
-	 * @return double
-	 */
+	///
+	/// @brief 从字符串中解析 double.
+	///
+	/// @param str 可以是十进制数字符串，0x 开头的 16 进制数字符串，0 开头的 8 进制数字符串。
+	/// @return double
+	///
 	double ParseDouble(base::String const &str);
 
-	/**
-	 * @brief 从字符串中解析 double
-	 *
-	 * @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
-	 * @param base 指定字符串中数字的进制数。
-	 * @return double
-	 */
+	///
+	/// @brief 从字符串中解析 double
+	///
+	/// @param str 可以是 10 进制，16 进制，8 进制数的字符串，但是不能有 0x 之类的前缀。
+	/// @param base 指定字符串中数字的进制数。
+	///
+	/// @return double
+	///
 	double ParseDouble(base::String const &str, int32_t base);
 } // namespace base
