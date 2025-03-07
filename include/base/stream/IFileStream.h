dfba605fe7de9f13df1ae75fea4fa759d9a39a5b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-08 00:26:29 +0800

    注释

--- include/base/stream/IFileStream.h
+++ include/base/stream/IFileStream.h
@@ -7,100 +7,99 @@
 
 namespace base
 {
-	/**
-	 * @brief 文件流接口。
-	 *
-	 */
+	///
+	/// @brief 文件流接口。
+	///
+	///
 	class IFileStream :
 		public base::Stream
 	{
 	public:
 		/* #region 流属性 */
 
-		/**
-		 * @brief 本流能否读取。
-		 *
-		 * @return true 能读取。
-		 * @return false 不能读取。
-		 */
+		///
+		/// @brief 本流能否读取。
+		///
+		/// @return true 能读取。
+		/// @return false 不能读取。
+		///
 		virtual bool CanRead() const = 0;
 
-		/**
-		 * @brief 本流能否写入。
-		 *
-		 * @return true 能写入。
-		 * @return false 不能写入。
-		 */
+		///
+		/// @brief 本流能否写入。
+		///
+		/// @return true 能写入。
+		/// @return false 不能写入。
+		///
 		virtual bool CanWrite() const = 0;
 
-		/**
-		 * @brief 本流能否定位。
-		 *
-		 * @return true 能定位。
-		 * @return false 不能定位。
-		 */
+		///
+		/// @brief 本流能否定位。
+		///
+		/// @return true 能定位。
+		/// @return false 不能定位。
+		///
 		virtual bool CanSeek() const = 0;
 
-		/**
-		 * @brief 流的长度
-		 *
-		 * @return int64_t
-		 */
+		///
+		/// @brief 流的长度
+		///
+		/// @return int64_t
+		///
 		virtual int64_t Length() const = 0;
 
-		/**
-		 * @brief 设置流的长度。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置流的长度。
+		///
+		/// @param value
+		///
 		virtual void SetLength(int64_t value) = 0;
 
-		/**
-		 * @brief 流当前的位置。
-		 *
-		 * @return int64_t
-		 */
+		///
+		/// @brief 流当前的位置。
+		///
+		/// @return int64_t
+		///
 		virtual int64_t Position() const = 0;
 
-		/**
-		 * @brief 设置流当前的位置。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置流当前的位置。
+		///
+		/// @param value
+		///
 		virtual void SetPosition(int64_t value) = 0;
 		/* #endregion */
 
 		/* #region 读写冲关 */
 
-		/**
-		 * @brief 将本流的数据读取到 span 中。
-		 *
-		 * @param span
-		 * @return int32_t
-		 */
+		///
+		/// @brief 将本流的数据读取到 span 中。
+		///
+		/// @param span
+		/// @return int32_t
+		///
 		virtual int32_t Read(base::Span const &span) = 0;
 
-		/**
-		 * @brief 将 span 中的数据写入本流。
-		 *
-		 * @param span
-		 */
+		///
+		/// @brief 将 span 中的数据写入本流。
+		///
+		/// @param span
+		///
 		virtual void Write(base::ReadOnlySpan const &span) = 0;
 
-		/**
-		 * @brief 冲洗流。
-		 *
-		 * @note 对于写入的数据，作用是将其从内部缓冲区转移到底层。
-		 *
-		 * @note 对于内部的可以读取但尚未读取的数据，一般不会有什么作用。Flush 没见过对可读数据生效的。
-		 */
+		///
+		/// @brief 冲洗流。
+		///
+		/// @note 对于写入的数据，作用是将其从内部缓冲区转移到底层。
+		/// @note 对于内部的可以读取但尚未读取的数据，一般不会有什么作用。Flush 没见过对可读数据生效的。
+		///
 		virtual void Flush() = 0;
 
-		/**
-		 * @brief 关闭流。
-		 *
-		 * @note 关闭后对流的操作将会引发异常。
-		 */
+		///
+		/// @brief 关闭流。
+		///
+		/// @note 关闭后对流的操作将会引发异常。
+		///
 		virtual void Close() = 0;
 		/* #endregion */
 	};
@@ -109,35 +108,36 @@ namespace base
 	{
 		namespace file
 		{
-			/**
-			 * @brief 以只读方式打开文件。文件必须存在，否则会抛出异常。
-			 *
-			 * @param path 文件的路径。
-			 *
-			 * @return std::shared_ptr<base::IFileStream> 成功打开则返回 base::IFileStream 对象。
-			 * 失败会抛出异常，不会返回空指针。
-			 */
+			///
+			/// @brief 以只读方式打开文件。文件必须存在，否则会抛出异常。
+			///
+			/// @param path 文件的路径。
+			///
+			/// @return std::shared_ptr<base::IFileStream> 成功打开则返回 base::IFileStream 对象。
+			/// 失败会抛出异常，不会返回空指针。
+			///
 			std::shared_ptr<base::IFileStream> OpenReadOnly(std::string const &path);
 
-			/**
-			 * @brief 用打开模式打开一个存在的文件。如果文件不存在，此函数不会创建文件。
-			 *
-			 * @param path 文件路径
-			 *
-			 * @return std::shared_ptr<base::IFileStream> 如果文件存在，且成功打开，则返回 base::IFileStream 对象。
-			 * 失败会抛出异常，不会返回空指针。
-			 */
+			///
+			/// @brief 用打开模式打开一个存在的文件。如果文件不存在，此函数不会创建文件。
+			///
+			/// @param path 文件路径
+			///
+			/// @return std::shared_ptr<base::IFileStream> 如果文件存在，且成功打开，
+			/// 则返回 base::IFileStream 对象。失败会抛出异常，不会返回空指针。
+			///
 			std::shared_ptr<base::IFileStream> OpenExisting(std::string const &path);
 
-			/**
-			 * @brief 用创建模式打开文件流，无论如何都会创建一个新的空白文件。会覆盖旧的。
-			 *
-			 * @param path 文件路径
-			 *
-			 * @return std::shared_ptr<base::IFileStream> 创建文件成功则返回 base::IFileStream 对象。
-			 * 创建失败会抛出异常，不会返回空指针。
-			 */
+			///
+			/// @brief 用创建模式打开文件流，无论如何都会创建一个新的空白文件。会覆盖旧的。
+			///
+			/// @param path 文件路径。
+			///
+			/// @return std::shared_ptr<base::IFileStream> 创建文件成功则返回 base::IFileStream 对象。
+			/// 创建失败会抛出异常，不会返回空指针。
+			///
 			std::shared_ptr<base::IFileStream> CreateNewAnyway(std::string const &path);
+
 		} // namespace file
 	} // namespace di
 } // namespace base
--- include/base/stream/Stream.h
+++ include/base/stream/Stream.h
@@ -8,10 +8,9 @@
 
 namespace base
 {
-	/**
-	 * @brief 流接口。
-	 *
-	 */
+	///
+	/// @brief 流接口。
+	///
 	class Stream
 	{
 	public:
@@ -20,146 +19,146 @@ namespace base
 	public:
 		/* #region 流属性 */
 
-		/**
-		 * @brief 本流能否读取。
-		 *
-		 * @return true 能读取。
-		 * @return false 不能读取。
-		 */
+		///
+		/// @brief 本流能否读取。
+		///
+		/// @return true 能读取。
+		/// @return false 不能读取。
+		///
 		virtual bool CanRead() const = 0;
 
-		/**
-		 * @brief 本流能否写入。
-		 *
-		 * @return true 能写入。
-		 * @return false 不能写入。
-		 */
+		///
+		/// @brief 本流能否写入。
+		///
+		/// @return true 能写入。
+		/// @return false 不能写入。
+		///
 		virtual bool CanWrite() const = 0;
 
-		/**
-		 * @brief 本流能否定位。
-		 *
-		 * @return true 能定位。
-		 * @return false 不能定位。
-		 */
+		///
+		/// @brief 本流能否定位。
+		///
+		/// @return true 能定位。
+		/// @return false 不能定位。
+		///
 		virtual bool CanSeek() const = 0;
 
-		/**
-		 * @brief 流的长度
-		 *
-		 * @return int64_t
-		 */
+		///
+		/// @brief 流的长度
+		///
+		/// @return int64_t
+		///
 		virtual int64_t Length() const = 0;
 
-		/**
-		 * @brief 设置流的长度。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置流的长度。
+		///
+		/// @param value
+		///
 		virtual void SetLength(int64_t value) = 0;
 
-		/**
-		 * @brief 流当前的位置。
-		 *
-		 * @return int64_t
-		 */
+		///
+		/// @brief 流当前的位置。
+		///
+		/// @return int64_t
+		///
 		virtual int64_t Position() const = 0;
 
-		/**
-		 * @brief 设置流当前的位置。
-		 *
-		 * @param value
-		 */
+		///
+		/// @brief 设置流当前的位置。
+		///
+		/// @param value
+		///
 		virtual void SetPosition(int64_t value) = 0;
 		/* #endregion */
 
 		/* #region 读写冲关 */
 
-		/**
-		 * @brief 将本流的数据读取到 span 中。
-		 *
-		 * @param span
-		 * @return int32_t
-		 */
+		///
+		/// @brief 将本流的数据读取到 span 中。
+		///
+		/// @param span
+		/// @return int32_t
+		///
 		virtual int32_t Read(base::Span const &span) = 0;
 
-		/**
-		 * @brief 将 span 中的数据写入本流。
-		 *
-		 * @param span
-		 */
+		///
+		/// @brief 将 span 中的数据写入本流。
+		///
+		/// @param span
+		///
 		virtual void Write(base::ReadOnlySpan const &span) = 0;
 
-		/**
-		 * @brief 冲洗流。
-		 *
-		 * @note 对于写入的数据，作用是将其从内部缓冲区转移到底层。
-		 *
-		 * @note 对于内部的可以读取但尚未读取的数据，一般不会有什么作用。Flush 没见过对可读数据生效的。
-		 */
+		///
+		/// @brief 冲洗流。
+		///
+		/// @note 对于写入的数据，作用是将其从内部缓冲区转移到底层。
+		/// @note 对于内部的可以读取但尚未读取的数据，一般不会有什么作用。Flush 没见过对可读数据生效的。
+		///
 		virtual void Flush() = 0;
 
-		/**
-		 * @brief 关闭流。
-		 *
-		 * @note 关闭后对流的操作将会引发异常。
-		 */
+		///
+		/// @brief 关闭流。
+		///
+		/// @note 关闭后对流的操作将会引发异常。
+		///
 		virtual void Close() = 0;
 		/* #endregion */
 
 		/* #region 接口扩展 */
 
-		/**
-		 * @brief 从流中读取数据写入 buffer。
-		 *
-		 * @param buffer 要将读取到的数据写入的缓冲区。
-		 * @param offset 将读取到的数据写入 dst_buf 时的起始位置。
-		 * @param count 要读取的字节数。
-		 * @return int32_t 实际读取的字节数。如果返回 0，说明此流结束。
-		 */
+		///
+		/// @brief 从流中读取数据写入 buffer。
+		///
+		/// @param buffer 要将读取到的数据写入的缓冲区。
+		/// @param offset 将读取到的数据写入 dst_buf 时的起始位置。
+		/// @param count 要读取的字节数。
+		/// @return int32_t 实际读取的字节数。如果返回 0，说明此流结束。
+		///
 		int32_t Read(uint8_t *buffer, int32_t offset, int32_t count);
 
-		/**
-		 * @brief 从流中精确读取等于 span 大小的字节数写入 span 中。
-		 *
-		 * @note 如果遇到流末尾，无法满足要求，则读取到的字节数会小于 span 的长度。
-		 *
-		 * @param span
-		 * @return int32_t 读取了多少字节。
-		 */
+		///
+		/// @brief 从流中精确读取等于 span 大小的字节数写入 span 中。
+		///
+		/// @note 如果遇到流末尾，无法满足要求，则读取到的字节数会小于 span 的长度。
+		///
+		/// @param span
+		/// @return int32_t 读取了多少字节。
+		///
 		int32_t ReadExactly(base::Span const &span);
 
-		/**
-		 * @brief 从流中读取精确数量的字节数写入 buffer 中。
-		 *
-		 * @note 如果遇到流末尾，无法满足要求，则读取到的字节数会小于 count。
-		 *
-		 * @param buffer
-		 * @param offset
-		 * @param count
-		 *
-		 * @return int32_t 读取到的字节数。
-		 * @note 一般等于 count，除非到达流末尾，无法满足要求了。调用者应该检查返回值，判断是否满足要求。
-		 */
+		///
+		/// @brief 从流中读取精确数量的字节数写入 buffer 中。
+		///
+		/// @note 如果遇到流末尾，无法满足要求，则读取到的字节数会小于 count。
+		///
+		/// @param buffer
+		/// @param offset
+		/// @param count
+		///
+		/// @return int32_t int32_t 读取到的字节数。
+		/// @note 一般等于 count，除非到达流末尾，无法满足要求了。调用者应该检查返回值，判断是否满足要求。
+		///
 		int32_t ReadExactly(uint8_t *buffer, int32_t offset, int32_t count);
 
-		/**
-		 * @brief 将 buffer 中的数据写入流中。
-		 *
-		 * @param buffer
-		 * @param offset
-		 * @param count
-		 */
+		///
+		/// @brief 将 buffer 中的数据写入流中。
+		///
+		/// @param buffer
+		/// @param offset
+		/// @param count
+		///
 		void Write(uint8_t const *buffer, int32_t offset, int32_t count);
 
-		/**
-		 * @brief 将本流拷贝到 dst_stream 中。
-		 *
-		 * @param dst_stream
-		 * @param cancellationToken
-		 */
+		///
+		/// @brief 将本流拷贝到 dst_stream 中。
+		///
+		/// @param dst_stream
+		/// @param cancellationToken
+		///
 		void CopyTo(std::shared_ptr<base::Stream> dst_stream,
 					std::shared_ptr<base::CancellationToken> cancellationToken);
+
 		/* #endregion */
 	};
 } // namespace base
