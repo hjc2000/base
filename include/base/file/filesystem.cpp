9c90b159fa9b60bf4f643bf6c53d2c249074b333
hjc2000 <hjc.666@foxmail.com>
2025-03-18 17:22:06 +0800

    filesystem

--- include/base/file/filesystem.cpp
+++ include/base/file/filesystem.cpp
@@ -1,1 +1,65 @@
 #include "filesystem.h"
+
+#if HAS_THREAD
+	#include <unistd.h>
+
+/* #region 访问权限检查 */
+
+bool base::filesystem::is_readable(std::string const &path)
+{
+	return access(path.c_str(), R_OK);
+}
+
+bool base::filesystem::is_writeable(std::string const &path)
+{
+	return access(path.c_str(), W_OK);
+}
+
+bool base::filesystem::is_excuteable(std::string const &path)
+{
+	return access(path.c_str(), X_OK);
+}
+
+/* #endregion */
+
+/* #region 目标类型检查 */
+
+bool base::filesystem::is_directory(std::string const &path)
+{
+	return std::filesystem::is_directory(path);
+}
+
+bool base::filesystem::is_regular_file(std::string const &path)
+{
+	return std::filesystem::is_regular_file(path);
+}
+
+bool base::filesystem::is_symbolic_link(std::string const &path)
+{
+	return std::filesystem::is_symlink(path);
+}
+
+/* #endregion */
+
+bool base::filesystem::exists(std::string const &path)
+{
+	return std::filesystem::exists(path);
+}
+
+std::string base::filesystem::read_symlink(std::string const &path)
+{
+	if (!is_symbolic_link(path))
+	{
+		throw std::runtime_error{CODE_POS_STR + "传进来的路径必须是一个符号链接的路径。"};
+	}
+
+	std::filesystem::path target_path = std::filesystem::read_symlink(path);
+	return target_path.string();
+}
+
+void base::filesystem::create_directory(std::string const &path)
+{
+	std::filesystem::create_directory(path);
+}
+
+#endif
--- include/base/file/filesystem.h
+++ include/base/file/filesystem.h
@@ -1,9 +1,100 @@
 #pragma once
+#include <string>
 
 namespace base
 {
 	namespace filesystem
 	{
+		/* #region 访问权限检查 */
 
-	}
+		///
+		/// @brief 检查文件或目录是否可读。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_readable(std::string const &path);
+
+		///
+		/// @brief 检查文件或目录是否可写。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_writeable(std::string const &path);
+
+		///
+		/// @brief 检查文件或目录是否可执行。
+		///
+		/// @note 对于目录，执行的意思是进入该目录。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_excuteable(std::string const &path);
+		/* #endregion */
+
+		/* #region 目标类型检查 */
+
+		///
+		/// @brief 检查指定路径是否是一个目录。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_directory(std::string const &path);
+
+		///
+		/// @brief 检查指定路径是否是一个常规的文件。
+		///
+		/// @note 常规文件是文件系统中储存在磁盘等介质中的数据，不是设备文件或套接字之类的抽象出来
+		/// 映射到文件系统中的文件。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_regular_file(std::string const &path);
+
+		///
+		/// @brief 检查指定路径是否是一个符号链接。
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool is_symbolic_link(std::string const &path);
+		/* #endregion */
+
+		///
+		/// @brief 检查指定路径是否存在。
+		///
+		/// @note 不管它是目录还是文件，只要存在就返回 true.
+		///
+		/// @param path
+		/// @return true
+		/// @return false
+		///
+		bool exists(std::string const &path);
+
+		///
+		/// @brief 读取符号链接指向的路径。
+		///
+		/// @param path
+		/// @return std::string
+		///
+		std::string read_symlink(std::string const &path);
+
+		///
+		/// @brief 创建一个目录。
+		///
+		/// @param path
+		///
+		void create_directory(std::string const &path);
+
+	} // namespace filesystem
 } // namespace base
