4ec77706bdd5cef8d41e5efc960b56e23d354760
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-19 21:52:25 +0800

    注释

--- include/base/IIdToken.h
+++ include/base/IIdToken.h
@@ -3,25 +3,25 @@
 
 namespace base
 {
-	/**
-	 * @brief 基于 ID 的令牌。
-	 *
-	 */
+	///
+	/// @brief 基于 ID 的令牌。
+	///
+	///
 	class IIdToken
 	{
 	public:
-		/**
-		 * @brief 本令牌对象的提供者。
-		 *
-		 * @return void*
-		 */
+		///
+		/// @brief 本令牌对象的提供者。
+		///
+		/// @return void*
+		///
 		virtual void *Provider() const = 0;
 
-		/**
-		 * @brief 令牌 ID.
-		 *
-		 * @return uint64_t
-		 */
+		///
+		/// @brief 令牌 ID.
+		///
+		/// @return uint64_t
+		///
 		virtual uint64_t ID() const = 0;
 	};
 } // namespace base
