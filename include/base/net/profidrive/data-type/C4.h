97788aba2810b5a64f9866122d70d3abd0f6b86b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-19 22:26:33 +0800

    注释

--- include/base/net/profidrive/data-type/C4.h
+++ include/base/net/profidrive/data-type/C4.h
@@ -8,55 +8,44 @@ namespace base
 {
 	namespace profidrive
 	{
-		/**
-		 * @brief profidrive 行规特定类型: C4.
-		 *
-		 */
+		///
+		/// @brief profidrive 行规特定类型: C4.
+		///
+		///
 		class C4 final
 		{
 		private:
 			base::Fraction _value;
-
-			/**
-			 * @brief 自适应转换器。
-			 * @note 将远程字节序设置为大端序后，在转换时能够根据本机字节序决定是否翻转。
-			 */
 			base::AutoBitConverter _converter{std::endian::big};
 
 			int32_t Factor() const;
 
 		public:
-			/**
-			 * @brief 无参构造函数。
-			 *
-			 */
 			C4() = default;
 
-			/**
-			 * @brief 从 profinet 收到大端序的数据后原封不动地将字节序列传进来。
-			 *
-			 * @param span
-			 */
+			///
+			/// @brief 从 profinet 收到大端序的数据后原封不动地将字节序列传进来。
+			///
+			/// @param span
+			///
 			C4(base::ReadOnlySpan const &span);
 
-			/**
-			 * @brief 通过实际的分数值构造行规特定数据类型。
-			 *
-			 * @param value
-			 */
+			///
+			/// @brief 通过实际的分数值构造行规特定数据类型。
+			///
+			/// @param value
+			///
 			C4(base::Fraction const &value);
 
-		public:
-			/**
-			 * @brief 强制转换为分数类型。
-			 *
-			 * @return base::Fraction
-			 */
+			///
+			/// @brief 强制转换为分数类型。
+			///
+			/// @return base::Fraction
+			///
 			explicit operator base::Fraction() const;
 
 			base::Array<uint8_t, 4> BufferForSending() const;
 
-		public:
 			C4 operator+(C4 const &right_value) const
 			{
 				return base::profidrive::C4{_value + right_value._value};
