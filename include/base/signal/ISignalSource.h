49dbbcb637d02426cf5d44d7ada97883df5901f5
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-19 23:03:19 +0800

    注释

--- include/base/signal/ISignalSource.h
+++ include/base/signal/ISignalSource.h
@@ -3,36 +3,36 @@
 
 namespace base
 {
-	/**
-	 * @brief 信号源接口。
-	 *
-	 * @tparam SignalType 信号值的类型。
-	 */
+	///
+	/// @brief 信号源接口。
+	///
+	/// @tparam SignalType 信号值的类型。
+	///
 	template <typename SignalType>
 	class ISignalSource
 	{
 	public:
-		/**
-		 * @brief 采样间隔。
-		 *
-		 * @return base::Seconds
-		 */
+		///
+		/// @brief 采样间隔。
+		///
+		/// @return base::Seconds
+		///
 		virtual base::Seconds SampleInterval() const = 0;
 
-		/**
-		 * @brief 打开采样器。
-		 *
-		 * @param sample_interval
-		 */
+		///
+		/// @brief 打开采样器。
+		///
+		/// @param sample_interval
+		///
 		virtual void Open(base::Seconds const &sample_interval) = 0;
 
-		/**
-		 * @brief 采样一次。
-		 *
-		 * @note 此调用会阻塞，直到时间距离上一次采样过去了 SampleInterval
-		 *
-		 * @return SignalType 采样值。
-		 */
+		///
+		/// @brief 采样一次。
+		///
+		/// @note 此调用会阻塞，直到时间距离上一次采样过去了 SampleInterval.
+		///
+		/// @return SignalType 采样值。
+		///
 		virtual SignalType Sample() = 0;
 	};
 } // namespace base
--- include/base/signal/PeriodicSamplingClock.h
+++ include/base/signal/PeriodicSamplingClock.h
@@ -3,56 +3,55 @@
 
 namespace base
 {
-	/**
-	 * @brief 周期采样时钟。
-	 * @note 时间大于一个周期时会自动减去整数倍的周期，调整到一个周期以内。
-	 */
+	///
+	/// @brief 周期采样时钟。
+	///
+	/// @note 时间大于一个周期时会自动减去整数倍的周期，调整到一个周期以内。
+	///
 	class PeriodicSamplingClock final
 	{
 	private:
 		base::Seconds _period{};
 		base::Seconds _current_time{};
 
-		/**
-		 * @brief 将 _time 调整到 1 个最小正周期以内。
-		 *
-		 */
+		///
+		/// @brief 将 _time 调整到 1 个最小正周期以内。
+		///
+		///
 		void AdjustTime();
 
 	public:
-		/**
-		 * @brief 周期采样时钟。
-		 *
-		 * @param period
-		 */
+		///
+		/// @brief 周期采样时钟。
+		///
+		/// @param period
+		///
 		PeriodicSamplingClock(base::Seconds period);
 
-	public:
 		PeriodicSamplingClock &operator+=(base::Seconds value);
 		PeriodicSamplingClock &operator-=(base::Seconds value);
 		PeriodicSamplingClock &operator*=(base::Seconds value);
 		PeriodicSamplingClock &operator/=(base::Seconds value);
 
-	public:
-		/**
-		 * @brief 本时钟的周期。
-		 *
-		 * @return base::Seconds
-		 */
+		///
+		/// @brief 本时钟的周期。
+		///
+		/// @return base::Seconds
+		///
 		base::Seconds Period() const;
 
-		/**
-		 * @brief 当前时间。
-		 *
-		 * @return base::Seconds
-		 */
+		///
+		/// @brief 当前时间。
+		///
+		/// @return base::Seconds
+		///
 		base::Seconds CurrentTime() const;
 
-		/**
-		 * @brief 相当于调用 CurrentTime 方法。
-		 *
-		 * @return base::Seconds
-		 */
+		///
+		/// @brief 相当于调用 CurrentTime 方法。
+		///
+		/// @return base::Seconds
+		///
 		explicit operator base::Seconds() const;
 	};
 } // namespace base
