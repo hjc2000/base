bc54daf3f04c306b56f6fb8556901bee65b2b3cc
hjc2000 <hjc.666@foxmail.com>
2025-03-19 16:49:08 +0800

    define

--- include/base/define.h
+++ include/base/define.h
@@ -1,10 +1,36 @@
 #pragma once
 
-// 静态全局变量。
+///
+/// @brief 静态全局变量。
+///
+///
 #define static_global static
 
-// 静态函数
+///
+/// @brief 静态函数
+///
+///
 #define static_function static
 
-// 静态字段
+///
+/// @brief 静态字段
+///
+///
 #define static_field static
+
+#ifdef __cplusplus
+
+template <typename T>
+void ProhibitedInMCU()
+{
+	static_assert(false, "单片机中禁止使用。");
+}
+
+	// 如果没有线程，说明是单片机，此时将下面的 std 功能取消，因为非常耗费 flash 空间。
+	#if !HAS_THREAD
+		#define cout ProhibitedInMCU<bool>()
+		#define endl ProhibitedInMCU<bool>()
+		#define format(...) ProhibitedInMCU<bool>()
+	#endif // !HAS_THREAD
+
+#endif //__cplusplus
