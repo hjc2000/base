ab4cd2c0ddcc8cf442bf439aac4a30e1253227c4
hjc2000 <hjc.666@foxmail.com>
2025-03-19 16:53:26 +0800

    define

--- include/base/define.h
+++ include/base/define.h
@@ -17,20 +17,3 @@
 ///
 ///
 #define static_field static
-
-#ifdef __cplusplus
-
-template <typename T>
-void ProhibitedInMCU()
-{
-	static_assert(false, "单片机中禁止使用。");
-}
-
-	// 如果没有线程，说明是单片机，此时将下面的 std 功能取消，因为非常耗费 flash 空间。
-	#if !HAS_THREAD
-		#define cout ProhibitedInMCU<bool>()
-		#define endl ProhibitedInMCU<bool>()
-		#define format(...) ProhibitedInMCU<bool>()
-	#endif // !HAS_THREAD
-
-#endif //__cplusplus
