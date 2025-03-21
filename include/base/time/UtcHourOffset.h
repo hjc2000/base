b7821fc681fa9838616eda8264f3098ddb78d6f7
hjc2000 <hjc.666@foxmail.com>
2025-03-21 11:07:30 +0800

    UtcHourOffset

--- include/base/time/UtcHourOffset.h
+++ include/base/time/UtcHourOffset.h
@@ -3,19 +3,36 @@
 
 namespace base
 {
+	///
+	/// @brief UTC 小时偏移。
+	///
 	class UtcHourOffset
 	{
 	private:
 		int64_t _value{};
 
 	public:
+		///
+		/// @brief UTC + 0.
+		///
+		///
 		UtcHourOffset() = default;
 
+		///
+		/// @brief UTC + value.
+		///
+		/// @param value
+		///
 		explicit UtcHourOffset(int64_t value)
 			: _value(value)
 		{
 		}
 
+		///
+		/// @brief 偏移的小时。
+		///
+		/// @return int64_t
+		///
 		int64_t Value() const
 		{
 			return _value;
