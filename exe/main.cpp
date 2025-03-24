d0d8587b705819b86832a24be08cae63da7c5188
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-25 00:31:19 +0800

    test_func

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,8 +1,25 @@
 #include "base/time/DateTime.h"
 #include <cstdlib>
+#include <iostream>
 #include <stdlib.h>
+
+class Test
+{
+public:
+	Test()
+	{
+		std::cout << "构造" << std::endl;
+	}
+};
+
+void test_func()
+{
+	std::cout << "第一条语句" << std::endl;
+	static Test test;
+}
 
 int main()
 {
-	base::test::TestDateTime();
+	test_func();
+	test_func();
 }
