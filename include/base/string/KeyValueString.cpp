6745d392b79aa129d577d984c6b9c804b4dba605
hjc2000 <hjc.666@foxmail.com>
2025-03-24 10:24:07 +0800

    try

--- include/base/string/KeyValueString.cpp
+++ include/base/string/KeyValueString.cpp
@@ -1,29 +1,44 @@
 #include "KeyValueString.h"
 #include "base/string/define.h"
 #include "base/string/StringSplitOptions.h"
+#include <exception>
 #include <stdexcept>
 
 base::KeyValueString::KeyValueString(base::String const key_value_string)
 {
-	base::StringSplitOptions options;
-	options.remove_empty_substring = false;
-	options.trim_each_substring = true;
-	auto strs = key_value_string.Split('=', options);
-	if (strs.Count() < 2)
+	try
 	{
-		throw std::invalid_argument{CODE_POS_STR + "非法键值对字符串。"};
-	}
+		base::StringSplitOptions options;
+		options.remove_empty_substring = false;
+		options.trim_each_substring = true;
+		auto strs = key_value_string.Split('=', options);
+		if (strs.Count() < 2)
+		{
+			throw std::invalid_argument{CODE_POS_STR + "非法键值对字符串。"};
+		}
 
-	_key = strs[0];
-	_value = strs[1];
-	CheckKeyValue();
+		_key = strs[0];
+		_value = strs[1];
+		CheckKeyValue();
+	}
+	catch (std::exception const &e)
+	{
+		throw std::runtime_error{CODE_POS_STR + e.what()};
+	}
 }
 
 base::KeyValueString::KeyValueString(base::String const &key, base::String const &value)
-	: _key(key),
-	  _value(value)
 {
-	CheckKeyValue();
+	try
+	{
+		_key = key;
+		_value = value;
+		CheckKeyValue();
+	}
+	catch (std::exception const &e)
+	{
+		throw std::runtime_error{CODE_POS_STR + e.what()};
+	}
 }
 
 void base::KeyValueString::CheckKeyValue() const
