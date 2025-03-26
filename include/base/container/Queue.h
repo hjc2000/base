5cf24695e7cc229bc9e717bb30738f2c4c63b0f8
hjc2000 <hjc.666@foxmail.com>
2025-03-26 11:08:17 +0800

    Queue

--- include/base/container/Queue.h
+++ include/base/container/Queue.h
@@ -5,11 +5,13 @@
 
 namespace base
 {
+	///
 	/// @brief 队列
 	/// @note 本队列装的类必须实现拷贝构造函数和赋值运算符。
 	/// @note 禁止使用 new 创建出对象后将对象的指针装入本队列，当退队或清空队列时会造成内存泄漏。
 	///
 	/// @tparam T
+	///
 	template <typename T>
 	class Queue final :
 		public IQueue<T>
@@ -18,20 +20,25 @@ namespace base
 		std::queue<T> _queue;
 
 	public:
+		///
 		/// @brief 构造一个空的队列。
+		///
 		Queue() = default;
 
-	public:
+		///
 		/// @brief 当前队列中元素的数量。
 		/// @return
+		///
 		int32_t Count() const override
 		{
 			return _queue.size();
 		}
 
+		///
 		/// @brief 退队
 		/// @return
 		/// @exception 如果队列为空，会抛出 underflow_error
+		///
 		T Dequeue() override
 		{
 			if (_queue.empty())
@@ -44,9 +51,11 @@ namespace base
 			return ret;
 		}
 
+		///
 		/// @brief 尝试退队
 		/// @param out 从队列里拿出来的元素会被赋值给 out，这要求 out 的类要实现赋值运算符。
 		/// @return 退队成功返回 true，失败返回 false
+		///
 		bool TryDequeue(T &out) override
 		{
 			if (_queue.empty())
@@ -59,23 +68,21 @@ namespace base
 			return true;
 		}
 
+		///
 		/// @brief 入队
 		/// @param obj
+		///
 		void Enqueue(T const &obj) override
 		{
 			_queue.push(obj);
 		}
 
+		///
 		/// @brief 清空队列。
+		///
 		void Clear() override
 		{
-			/* 构造一个空的队列，然后让 _queue 与空队列交换。退出作用域后，empty_queue
-			 * 被销毁，所有元素都丢失。
-			 *
-			 * 这要求 T 类型必须不是在堆上创建的，否则会内存泄漏，因为本函数并不会 delete 每一个元素。
-			 */
-			std::queue<T> empty_queue;
-			_queue.swap(empty_queue);
+			_queue = std::queue<T>{};
 		}
 	};
 } // namespace base
