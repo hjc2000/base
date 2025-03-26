cbcb7b6b989f240c2adb8bc4f075a97cf410f0bc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-26 23:52:08 +0800

    PositionRange

--- include/base/math/PositionRange.h
+++ include/base/math/PositionRange.h
@@ -3,6 +3,12 @@
 
 namespace base
 {
+	///
+	/// @brief 由 2 个点定义的位置范围。例如 (x1, y1) , (x2, y2) 指示的范围
+	/// 就是从 x1 到 x2 ，从 y1 到 y2 之间围成的矩形。
+	///
+	/// (x1, y1) , (x2, y2) 这两个点也算在范围内。
+	///
 	class PositionRange
 	{
 	private:
