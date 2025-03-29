169d27e9011de1bd3681cd1d78ad86b966434790
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-03-30 00:48:50 +0800

    十二平均律

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,8 +1,33 @@
-#include "base/container/SafeQueue.h"
+#include <cmath>
 #include <cstdlib>
+#include <iostream>
 #include <stdlib.h>
+#include <string>
 
 int main()
 {
-	base::test::TestSafeQueue();
+	double a = std::pow(2, 1.0 / 12);
+	std::cout << "a = " << a << std::endl;
+
+	std::string names[] = {
+		"C",
+		"C#",
+		"D",
+		"D#",
+		"E",
+		"F",
+		"F#",
+		"G",
+		"G#",
+		"A",
+		"A#",
+		"B",
+	};
+
+	for (int i = 0; i < 7 * 12; i++)
+	{
+		std::string name = names[i % 12] + std::to_string(i / 12 + 1);
+		double f = 440.0 * std::pow(a, i - 45);
+		std::cout << name << "\t --> \t" << f << std::endl;
+	}
 }
