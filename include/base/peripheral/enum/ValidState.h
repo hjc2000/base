4a4655146c5063468a396acf3f3ce7384fee5393
hjc2000 <hjc.666@foxmail.com>
2025-04-03 09:51:02 +0800

    ValidState

---
+++ include/base/peripheral/enum/ValidState.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace base
+{
+	namespace Enum
+	{
+		///
+		/// @brief 有效状态。
+		///
+		///
+		enum class ValidState
+		{
+			Invalid,
+			Valid,
+		};
+	} // namespace Enum
+} // namespace base
