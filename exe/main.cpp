ae1890763ddc03a3452a1565eab89a406cc9f00c
hjc2000 <hjc.666@foxmail.com>
2025-04-08 09:34:23 +0800

    ReadBits

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,6 +1,8 @@
+#include "base/bit/bit.h"
 #include "base/math/Fraction.h"
 #include "base/wrapper/number-wrapper.h"
 #include <cmath>
+#include <cstdint>
 #include <cstdlib>
 #include <iostream>
 #include <numbers>
@@ -8,21 +10,28 @@
 
 int main()
 {
-	base::Fraction f{base::Double{std::numbers::pi}};
-	std::cout << "分数: " << f << std::endl;
+	{
+		base::Fraction f{base::Double{std::numbers::pi}};
+		std::cout << "分数: " << f << std::endl;
 
-	std::cout << "std::numbers::pi: \t\t"
-			  << std::setprecision(17)
-			  << std::numbers::pi
-			  << std::endl;
+		std::cout << "std::numbers::pi: \t\t"
+				  << std::setprecision(17)
+				  << std::numbers::pi
+				  << std::endl;
 
-	std::cout << "分数表示的 pi 转为 double: \t"
-			  << std::setprecision(17)
-			  << static_cast<double>(f)
-			  << std::endl;
+		std::cout << "分数表示的 pi 转为 double: \t"
+				  << std::setprecision(17)
+				  << static_cast<double>(f)
+				  << std::endl;
 
-	std::cout << "误差: "
-			  << std::setprecision(17)
-			  << static_cast<double>(f) - std::numbers::pi
-			  << std::endl;
+		std::cout << "误差: "
+				  << std::setprecision(17)
+				  << static_cast<double>(f) - std::numbers::pi
+				  << std::endl;
+	}
+
+	{
+		uint32_t reg = 5;
+		std::cout << base::bit::ReadBits(reg, 2, 3) << std::endl;
+	}
 }
