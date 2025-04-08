4c8d4291be6b27cb83765dc5c1561dca22bf0154
hjc2000 <hjc.666@foxmail.com>
2025-04-08 17:06:34 +0800

    region

--- include/base/math/Position.cpp
+++ include/base/math/Position.cpp
@@ -8,6 +8,8 @@ base::Position::Position(int64_t x, int64_t y)
 {
 }
 
+/* #region 比较运算符 */
+
 bool base::Position::operator==(base::Position const &another) const
 {
 	std::array<int64_t, 2> arr{_x, _y};
@@ -42,3 +44,5 @@ bool base::Position::operator>=(base::Position const &another) const
 	std::array<int64_t, 2> another_arr{another._x, another._y};
 	return arr >= another_arr;
 }
+
+/* #endregion */
