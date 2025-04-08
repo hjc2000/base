ea15f0d0356ab7c99f087f7652eefc48bc549446
hjc2000 <hjc.666@foxmail.com>
2025-04-08 10:03:44 +0800

    MotorControlMode

--- include/base/enum.h
+++ include/base/enum.h
@@ -51,5 +51,31 @@ namespace base
 			///
 			HasWarning,
 		};
+
+		///
+		/// @brief 电机控制模式。
+		///
+		///
+		enum class MotorControlMode
+		{
+			///
+			/// @brief 位置控制模式。
+			///
+			///
+			Position,
+
+			///
+			/// @brief 速度控制模式。
+			///
+			///
+			Speed,
+
+			///
+			/// @brief 转矩控制模式。
+			///
+			///
+			Torque,
+		};
+
 	} // namespace Enum
 } // namespace base
