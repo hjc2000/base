5a267c7c8c1a135fb8f4064b685f16a03cb9d2bc
hjc2000 <hjc.666@foxmail.com>
2025-04-10 11:06:16 +0800

    GpioPin

---
+++ include/base/peripheral/GpioPin.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "gpio_handle.h"
+#include <string>
+
+namespace base
+{
+	namespace gpio
+	{
+		class GpioPin
+		{
+		private:
+			sp_gpio_pin_handle _handle{};
+
+		public:
+			GpioPin(sp_gpio_pin_handle const &h)
+				: _handle(h)
+			{
+			}
+
+			std::string Name() const
+			{
+				return base::gpio::pin_name(_handle);
+			}
+
+			bool ReadPin() const
+			{
+				return base::gpio::read_pin(_handle);
+			}
+
+			void WritePin(bool value)
+			{
+				base::gpio::write_pin(_handle, value);
+			}
+
+			void TogglePin(sp_gpio_pin_handle const &h)
+			{
+				base::gpio::toggle_pin(_handle);
+			}
+		};
+
+	} // namespace gpio
+} // namespace base
--- include/base/peripheral/gpio_handle.h
+++ include/base/peripheral/gpio_handle.h
@@ -130,28 +130,28 @@ namespace base
 
 		/* #endregion */
 
-		class gpio_handle;
-		using sp_gpio_handle = std::shared_ptr<base::gpio::gpio_handle>;
+		class gpio_pin_handle;
+		using sp_gpio_pin_handle = std::shared_ptr<base::gpio::gpio_pin_handle>;
 
 		/* #region 打开函数 */
 
-		sp_gpio_handle open_as_input_mode(base::gpio::PullMode pull_mode,
-										  base::gpio::TriggerEdge trigger_edge);
+		sp_gpio_pin_handle open_as_input_mode(base::gpio::PullMode pull_mode,
+											  base::gpio::TriggerEdge trigger_edge);
 
-		sp_gpio_handle open_as_output_mode(base::gpio::PullMode pull_mode,
-										   base::gpio::DriveMode drive_mode);
+		sp_gpio_pin_handle open_as_output_mode(base::gpio::PullMode pull_mode,
+											   base::gpio::DriveMode drive_mode);
 
-		sp_gpio_handle open_as_alternate_function_mode(base::gpio::AlternateFunction af,
-													   base::gpio::PullMode pull_mode,
-													   base::gpio::DriveMode drive_mode);
+		sp_gpio_pin_handle open_as_alternate_function_mode(base::gpio::AlternateFunction af,
+														   base::gpio::PullMode pull_mode,
+														   base::gpio::DriveMode drive_mode);
 
 		/* #endregion */
 
-		std::string pin_name(sp_gpio_handle const &h);
+		std::string pin_name(sp_gpio_pin_handle const &h);
 
-		bool read_pin(sp_gpio_handle const &h);
-		void write_pin(sp_gpio_handle const &h, bool value);
-		void toggle_pin(sp_gpio_handle const &h);
+		bool read_pin(sp_gpio_pin_handle const &h);
+		void write_pin(sp_gpio_pin_handle const &h, bool value);
+		void toggle_pin(sp_gpio_pin_handle const &h);
 
 		///
 		/// @brief 注册中断回调函数。
@@ -159,14 +159,14 @@ namespace base
 		/// @param h
 		/// @param callback_func
 		///
-		void register_interrupt_callback(sp_gpio_handle const &h, std::function<void()> callback_func);
+		void register_interrupt_callback(sp_gpio_pin_handle const &h, std::function<void()> callback_func);
 
 		///
 		/// @brief 取消注册中断回调函数。
 		///
 		/// @param h
 		///
-		void unregister_interrupt_callback(sp_gpio_handle const &h);
+		void unregister_interrupt_callback(sp_gpio_pin_handle const &h);
 
 	} // namespace gpio
 } // namespace base
