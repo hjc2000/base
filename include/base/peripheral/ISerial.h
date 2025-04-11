4e00adaf6b06f97b599d06a65f60382d041501e6
hjc2000 <hjc.666@foxmail.com>
2025-04-11 16:37:59 +0800

    serial_handle

--- include/base/peripheral/ISerial.h
+++ include/base/peripheral/ISerial.h
@@ -2,124 +2,13 @@
 #include "base/container/IDictionary.h"
 #include "base/define.h"
 #include "base/stream/Stream.h"
+#include "serial_parameter.h"
 #include <string>
 
 namespace base
 {
 	namespace serial
 	{
-		/* #region 串口属性 */
-
-		///
-		/// @brief 校验方式
-		///
-		enum class Parity
-		{
-			///
-			/// @brief 不校验。
-			///
-			None,
-
-			///
-			/// @brief 偶校验。
-			///
-			Even,
-
-			///
-			/// @brief 奇校验。
-			///
-			Odd,
-		};
-
-		///
-		/// @brief 停止位位数。
-		///
-		enum class StopBits
-		{
-			///
-			/// @brief 0.5
-			///
-			ZeroPointFive,
-
-			///
-			/// @brief 1
-			///
-			One,
-
-			///
-			/// @brief 1.5
-			///
-			OnePointFive,
-
-			///
-			/// @brief 2
-			///
-			Two,
-		};
-
-		///
-		/// @brief 硬件流控。
-		///
-		enum class HardwareFlowControl
-		{
-			None,
-			RTS,
-			CTS,
-			RTS_CTS,
-		};
-
-		///
-		/// @brief 串口数据传输方向。
-		///
-		enum class Direction
-		{
-			RX,
-			TX,
-			RX_TX,
-		};
-
-		///
-		/// @brief 波特率。
-		///
-		class BaudRate
-		{
-		private:
-			uint32_t _value = 0;
-
-		public:
-			explicit BaudRate(uint32_t value)
-				: _value(value)
-			{
-			}
-
-			uint32_t Value() const
-			{
-				return _value;
-			}
-		};
-
-		///
-		/// @brief 数据位位数。
-		///
-		class DataBits
-		{
-		private:
-			uint8_t _value = 0;
-
-		public:
-			explicit DataBits(uint8_t value)
-				: _value(value)
-			{
-			}
-
-			uint8_t Value() const
-			{
-				return _value;
-			}
-		};
-
-		/* #endregion */
-
 		///
 		/// @brief 串口接口。
 		///
---
+++ include/base/peripheral/serial_handle.h
@@ -0,0 +1,100 @@
+#pragma once
+#include "serial_parameter.h"
+#include <memory>
+#include <string>
+#include <vector>
+
+namespace base
+{
+	namespace serial
+	{
+		class serial_handle;
+		using sp_serial_handle = std::shared_ptr<serial_handle>;
+
+		///
+		/// @brief 通过串口名称打开串口。
+		///
+		/// @param name 串口名称。
+		///		@note 通用操作系统中使用这种方式。
+		///
+		/// @return base::serial::sp_serial_handle
+		///
+		base::serial::sp_serial_handle open(std::string const &name);
+
+		///
+		/// @brief 通过串口 ID 打开串口。
+		///
+		/// @param serial_id 串口 ID.
+		/// 	@note 单片机中使用这种方式。例如想要打开 UART1 就传入 1.
+		///
+		/// @return base::serial::sp_serial_handle
+		///
+		base::serial::sp_serial_handle open(int serial_id);
+
+		///
+		/// @brief 获取串口名称。
+		///
+		/// @param h
+		/// @return std::string
+		///
+		std::string name(base::serial::sp_serial_handle const &h);
+
+		/* #region 启动串口 */
+
+		///
+		/// @brief 启动串口。
+		///
+		/// @param h
+		/// @param direction
+		/// @param baud_rate
+		/// @param data_bits
+		/// @param parity
+		/// @param stop_bits
+		/// @param hardware_flow_control
+		///
+		void start(base::serial::sp_serial_handle const &h,
+				   base::serial::Direction direction,
+				   base::serial::BaudRate const &baud_rate,
+				   base::serial::DataBits const &data_bits,
+				   base::serial::Parity parity,
+				   base::serial::StopBits stop_bits,
+				   base::serial::HardwareFlowControl hardware_flow_control);
+
+		///
+		/// @brief 启动串口。
+		///
+		/// @param h
+		///
+		inline void start(base::serial::sp_serial_handle const &h)
+		{
+			start(h,
+				  base::serial::Direction::RX_TX,
+				  base::serial::BaudRate{115200},
+				  base::serial::DataBits{8},
+				  base::serial::Parity::None,
+				  base::serial::StopBits::One,
+				  base::serial::HardwareFlowControl::None);
+		}
+
+		///
+		/// @brief 启动串口。
+		///
+		/// @param h
+		/// @param baud_rate
+		///
+		inline void start(base::serial::sp_serial_handle const &h,
+						  base::serial::BaudRate const &baud_rate)
+		{
+			start(h,
+				  base::serial::Direction::RX_TX,
+				  baud_rate,
+				  base::serial::DataBits{8},
+				  base::serial::Parity::None,
+				  base::serial::StopBits::One,
+				  base::serial::HardwareFlowControl::None);
+		}
+
+		/* #endregion */
+
+	} // namespace serial
+} // namespace base
