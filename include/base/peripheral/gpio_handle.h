589d37a537e620e799edd1074b465b876eaaad0e
hjc2000 <hjc.666@foxmail.com>
2025-04-11 16:14:59 +0800

    注释

--- include/base/peripheral/gpio_handle.h
+++ include/base/peripheral/gpio_handle.h
@@ -12,18 +12,49 @@ namespace base
 		class gpio_pin_handle;
 		using sp_gpio_pin_handle = std::shared_ptr<base::gpio::gpio_pin_handle>;
 
+		///
+		/// @brief 打开 GPIO 引脚。
+		///
+		/// @note 打开后即占用该引脚，无法再次打开，除非让 gpio_pin_handle 析构。
+		///
+		/// @param port
+		/// @param pin
+		/// @return base::gpio::sp_gpio_pin_handle
+		///
 		base::gpio::sp_gpio_pin_handle open(base::gpio::PortEnum port, uint32_t pin);
 
 		/* #region 初始化函数 */
 
+		///
+		/// @brief 初始化为输入模式。
+		///
+		/// @param h
+		/// @param pull_mode
+		/// @param trigger_edge
+		///
 		void initialize_as_input_mode(base::gpio::sp_gpio_pin_handle const &h,
 									  base::gpio::PullMode pull_mode,
 									  base::gpio::TriggerEdge trigger_edge);
 
+		///
+		/// @brief 初始化为输出模式。
+		///
+		/// @param h
+		/// @param pull_mode
+		/// @param drive_mode
+		///
 		void initialize_as_output_mode(base::gpio::sp_gpio_pin_handle const &h,
 									   base::gpio::PullMode pull_mode,
 									   base::gpio::DriveMode drive_mode);
 
+		///
+		/// @brief 初始化为复用功能模式。
+		///
+		/// @param h
+		/// @param af
+		/// @param pull_mode
+		/// @param drive_mode
+		///
 		void initialize_as_alternate_function_mode(base::gpio::sp_gpio_pin_handle const &h,
 												   base::gpio::AlternateFunction af,
 												   base::gpio::PullMode pull_mode,
@@ -31,11 +62,41 @@ namespace base
 
 		/* #endregion */
 
+		///
+		/// @brief 获取引脚名称。
+		///
+		/// @param h
+		/// @return std::string
+		///
 		std::string pin_name(base::gpio::sp_gpio_pin_handle const &h);
 
+		/* #region 读写引脚电平 */
+
+		///
+		/// @brief 读引脚电平。
+		///
+		/// @param h
+		/// @return true
+		/// @return false
+		///
 		bool read_pin(base::gpio::sp_gpio_pin_handle const &h);
+
+		///
+		/// @brief 写引脚电平。
+		///
+		/// @param h
+		/// @param value
+		///
 		void write_pin(base::gpio::sp_gpio_pin_handle const &h, bool value);
+
+		///
+		/// @brief 翻转引脚电平。
+		///
+		/// @param h
+		///
 		void toggle_pin(base::gpio::sp_gpio_pin_handle const &h);
+
+		/* #endregion */
 
 		///
 		/// @brief 注册中断回调函数。
