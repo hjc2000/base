77113518598a1a0c056ccdab1fc5ba40b1d2f8e8
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-13 19:50:28 +0800

    KeyScanner

---
+++ include/base/peripheral/key/KeyScanner.cpp
@@ -0,0 +1,43 @@
+#include "KeyScanner.h" // IWYU pragma: keep
+#include "base/task/delay.h"
+#include <cstddef>
+
+void base::key::KeyScanner::ScanKeysNoDelay(boost::dynamic_bitset<> &out)
+{
+	for (size_t i = 0; i < _keys.size(); i++)
+	{
+		out[i] = _keys[i].KeyIsDown();
+	}
+}
+
+base::key::KeyScanner::KeyScanner(std::initializer_list<base::key::Key> keys)
+{
+	for (base::key::Key const &key : keys)
+	{
+		_keys.push_back(key);
+	}
+}
+
+void base::key::KeyScanner::ScanKeys()
+{
+	_last_scan_result = _current_scan_result;
+	ScanKeysNoDelay(_no_delay_scan_result1);
+	base::task::Delay(std::chrono::milliseconds{10});
+	ScanKeysNoDelay(_no_delay_scan_result2);
+	_current_scan_result = _no_delay_scan_result1 & _no_delay_scan_result2;
+}
+
+bool base::key::KeyScanner::HasKeyDownEvent(int key_index)
+{
+	return _current_scan_result[key_index] && (!_last_scan_result[key_index]);
+}
+
+bool base::key::KeyScanner::HasKeyUpEvent(int key_index)
+{
+	return (!_current_scan_result[key_index]) && _last_scan_result[key_index];
+}
+
+bool base::key::KeyScanner::HasKeyPressedEvent(int key_index)
+{
+	return _current_scan_result[key_index];
+}
---
+++ include/base/peripheral/key/KeyScanner.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "boost/dynamic_bitset/dynamic_bitset.hpp"
+#include "Key.h"
+#include <initializer_list>
+#include <vector>
+
+namespace base
+{
+	namespace key
+	{
+		class KeyScanner
+		{
+		private:
+			std::vector<base::key::Key> _keys;
+			boost::dynamic_bitset<> _last_scan_result;
+			boost::dynamic_bitset<> _current_scan_result;
+			boost::dynamic_bitset<> _no_delay_scan_result1;
+			boost::dynamic_bitset<> _no_delay_scan_result2;
+
+			void ScanKeysNoDelay(boost::dynamic_bitset<> &out);
+
+		public:
+			KeyScanner(std::initializer_list<base::key::Key> keys);
+
+			void ScanKeys();
+
+			bool HasKeyDownEvent(int key_index);
+
+			bool HasKeyUpEvent(int key_index);
+
+			bool HasKeyPressedEvent(int key_index);
+		};
+	} // namespace key
+} // namespace base
