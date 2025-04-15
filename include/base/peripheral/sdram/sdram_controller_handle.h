46caeba45c102f6098cc15e86e283a6e2d96bab3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-16 00:42:15 +0800

    base::sdram::

--- include/base/peripheral/sdram/sdram_controller_handle.h
+++ include/base/peripheral/sdram/sdram_controller_handle.h
@@ -16,17 +16,17 @@ namespace base
 		/// @note 如果只有一个 SDRAM 控制器，不需要指定打开哪一个 SDRAM 控制器，
 		/// 可以使用本函数。
 		///
-		/// @return std::shared_ptr<sdram_controller_handle>
+		/// @return std::shared_ptr<base::sdram::sdram_controller_handle>
 		///
-		std::shared_ptr<sdram_controller_handle> open();
+		std::shared_ptr<base::sdram::sdram_controller_handle> open();
 
 		///
 		/// @brief 打开指定的 SDRAM 控制器。
 		///
 		/// @param sdram_controller_id
-		/// @return std::shared_ptr<sdram_controller_handle>
+		/// @return std::shared_ptr<base::sdram::sdram_controller_handle>
 		///
-		std::shared_ptr<sdram_controller_handle> open(int sdram_controller_id);
+		std::shared_ptr<base::sdram::sdram_controller_handle> open(int sdram_controller_id);
 
 		///
 		/// @brief 将 SDRAM 控制器初始化为读突发模式。写不突发。
@@ -39,7 +39,7 @@ namespace base
 		/// @param data_width
 		/// @param read_burst_length
 		///
-		void initialize_as_read_burst_mode(sdram_controller_handle &h,
+		void initialize_as_read_burst_mode(base::sdram::sdram_controller_handle &h,
 										   base::sdram::ISDRAMTimingProvider const &timing_provider,
 										   base::sdram::BankCount const &bank_count,
 										   base::sdram::RowBitCount const &row_bit_count,
@@ -52,14 +52,14 @@ namespace base
 		///
 		/// @param h
 		///
-		void send_precharge_all_command(sdram_controller_handle &h);
+		void send_precharge_all_command(base::sdram::sdram_controller_handle &h);
 
 		///
 		/// @brief 发送自动刷新命令。
 		///
 		/// @param h
 		///
-		void send_auto_refresh_command(sdram_controller_handle &h);
+		void send_auto_refresh_command(base::sdram::sdram_controller_handle &h);
 
 		///
 		/// @brief 发送指定次数的自动刷新命令。
@@ -67,7 +67,7 @@ namespace base
 		/// @param h
 		/// @param refresh_times
 		///
-		void send_auto_refresh_command(sdram_controller_handle &h, int refresh_times);
+		void send_auto_refresh_command(base::sdram::sdram_controller_handle &h, int refresh_times);
 
 		///
 		/// @brief 写模式寄存器。
@@ -75,7 +75,7 @@ namespace base
 		/// @param h
 		/// @param value
 		///
-		void write_mode_register(sdram_controller_handle &h, uint32_t value);
+		void write_mode_register(base::sdram::sdram_controller_handle &h, uint32_t value);
 
 		///
 		/// @brief 控制器被打开后所使用的时序。
@@ -83,7 +83,7 @@ namespace base
 		/// @param h
 		/// @return base::sdram::sdram_timing const&
 		///
-		base::sdram::sdram_timing const &timing(sdram_controller_handle &h);
+		base::sdram::sdram_timing const &timing(base::sdram::sdram_controller_handle &h);
 
 		///
 		/// @brief 此 SDRAM 控制器所管理的内存段。打开 SDRAM 后，对着这个地址区域读写数据即可读写 SDRAM.
@@ -94,7 +94,7 @@ namespace base
 		/// @param h
 		/// @return base::Span
 		///
-		base::Span span(sdram_controller_handle &h);
+		base::Span span(base::sdram::sdram_controller_handle &h);
 
 	} // namespace sdram
 } // namespace base
