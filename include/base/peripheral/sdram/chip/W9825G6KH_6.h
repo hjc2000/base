c859b5a881c17e88b3d8e6462a7b080ea3f4666f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-16 20:17:23 +0800

    W9825G6KH_6

---
+++ include/base/peripheral/sdram/chip/W9825G6KH_6.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "base/peripheral/sdram/SdramController.h"
+
+namespace base
+{
+	namespace sdram
+	{
+		namespace chip
+		{
+			class W9825G6KH_6
+			{
+			private:
+				base::sdram::SdramController _controller;
+
+			public:
+				///
+				/// @brief 构造函数。
+				/// @param controller 需要一个控制器来控制 SDRAM 芯片。
+				///
+				W9825G6KH_6(base::sdram::SdramController const &controller);
+
+				///
+				/// @brief 打开 SDRAM.
+				///
+				/// @note 分为 2 步：
+				/// 	@li 打开 SDRAM 控制器。
+				/// 	@li 初始化 SDRAM 芯片。
+				///
+				void Open();
+
+				///
+				/// @brief 此 SDRAM 的内存段。
+				/// @return
+				///
+				base::Span Span() const;
+			};
+		} // namespace chip
+	} // namespace sdram
+} // namespace base
