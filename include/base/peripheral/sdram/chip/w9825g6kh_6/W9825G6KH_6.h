a817649039e157783e5211d2aed9a17eb1b605d2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-17 01:29:45 +0800

    W9825G6KH_6

--- include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.h
+++ include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.h
@@ -1,5 +1,9 @@
 #pragma once
+#include "base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6_TimingProvider.h"
 #include "base/peripheral/sdram/SdramController.h"
+#include "base/task/delay.h"
+#include "ModeRegister.h"
+#include <cstdint>
 
 namespace base
 {
@@ -31,13 +35,39 @@ namespace base
 					/// 	@li 打开 SDRAM 控制器。
 					/// 	@li 初始化 SDRAM 芯片。
 					///
-					void Open();
+					void Open()
+					{
+						_controller.InitializeAsReadBurstMode(base::sdram::W9825G6KH_6_TimingProvider{},
+															  base::sdram::BankCount{4},
+															  base::sdram::RowBitCount{13},
+															  base::sdram::ColumnBitCount{9},
+															  base::sdram::DataWidth{16},
+															  base::sdram::ReadBurstLength{1});
+
+						// SDRAM控制器初始化完成以后还需要按照如下顺序初始化SDRAM
+						base::task::Delay(std::chrono::microseconds{500});
+						_controller.SendPrechargeAllCommand();
+						_controller.SendAutoRefreshCommand(8);
+
+						base::sdram::chip::w9825g6kh_6::ModeRegister reg{};
+						reg.SetReadingBurstLength(1);
+						reg.SetReadingBurstMode(base::sdram::chip::w9825g6kh_6::ReadingBurstMode::Sequential);
+						reg.SetCasLatency(_controller.Timing().cas_latency());
+						reg.SetWritingBurstMode(base::sdram::chip::w9825g6kh_6::WritingBurstMode::Single);
+						_controller.WriteModeRegister(reg.Value());
+					}
 
 					///
 					/// @brief 此 SDRAM 的内存段。
 					/// @return
 					///
-					base::Span Span() const;
+					base::Span Span() const
+					{
+						return base::Span{
+							_controller.Span().Buffer(),
+							std::min<int32_t>(32 * 1024 * 1024, _controller.Span().Size()),
+						};
+					}
 				};
 			} // namespace w9825g6kh_6
 		} // namespace chip
