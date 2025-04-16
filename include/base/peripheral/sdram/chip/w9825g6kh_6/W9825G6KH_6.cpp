c20682961f38afb55c6522c71f5d1fd57826496a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-17 00:51:13 +0800

    W9825G6KH_6

--- include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.cpp
+++ include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.cpp
@@ -1,23 +1,8 @@
 #include "W9825G6KH_6.h"
 #include "base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6_TimingProvider.h"
 #include "base/task/delay.h"
-
-// SDRAM配置参数
-#define SDRAM_MODEREG_BURST_LENGTH_1 ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_LENGTH_2 ((uint16_t)0x0001)
-#define SDRAM_MODEREG_BURST_LENGTH_4 ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8 ((uint16_t)0x0004)
-
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED ((uint16_t)0x0008)
-
-#define SDRAM_MODEREG_CAS_LATENCY_2 ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3 ((uint16_t)0x0030)
-
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD ((uint16_t)0x0000)
-
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE ((uint16_t)0x0200)
+#include "ModeRegister.h"
+#include <cstdint>
 
 base::sdram::chip::w9825g6kh_6::W9825G6KH_6::W9825G6KH_6(base::sdram::SdramController const &controller)
 	: _controller(controller)
@@ -38,28 +23,18 @@ void base::sdram::chip::w9825g6kh_6::W9825G6KH_6::Open()
 	_controller.SendPrechargeAllCommand();
 	_controller.SendAutoRefreshCommand(8);
 
-	uint32_t cas_letency = SDRAM_MODEREG_CAS_LATENCY_2;
-	if (_controller.Timing().cas_latency() == 3)
-	{
-		cas_letency = SDRAM_MODEREG_CAS_LATENCY_3;
-	}
-
-	/**
-	 * 发送 “加载模式寄存器” 命令。
-	 * 		@li 配置模式寄存器,SDRAM的bit0~bit2为指定突发访问的长度
-	 * 		@li bit3为指定突发访问的类型，bit4~bit6为CAS值，bit7和bit8为运行模式
-	 * 		@li bit9为指定的写突发模式，bit10和bit11位保留位
-	 */
-	uint32_t sdram_mod_register = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1 | // 设置突发长度:1(可以是1/2/4/8)
-								  SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL |    // 设置突发类型:连续(可以是连续/交错)
-								  cas_letency |                            // 设置CAS值:3(可以是2/3)
-								  SDRAM_MODEREG_OPERATING_MODE_STANDARD |  // 设置操作模式:0,标准模式
-								  SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;    // 设置突发写模式:1,单点访问
-
-	_controller.WriteModeRegister(sdram_mod_register);
+	base::sdram::chip::w9825g6kh_6::ModeRegister reg{};
+	reg.SetReadingBurstLength(1);
+	reg.SetReadingBurstMode(base::sdram::chip::w9825g6kh_6::ReadingBurstMode::Sequential);
+	reg.SetCasLatency(_controller.Timing().cas_latency());
+	reg.SetWritingBurstMode(base::sdram::chip::w9825g6kh_6::WritingBurstMode::Single);
+	_controller.WriteModeRegister(reg.Value());
 }
 
 base::Span base::sdram::chip::w9825g6kh_6::W9825G6KH_6::Span() const
 {
-	return base::Span{_controller.Span().Buffer(), 32 * 1024 * 1024};
+	return base::Span{
+		_controller.Span().Buffer(),
+		std::min<int32_t>(32 * 1024 * 1024, _controller.Span().Size()),
+	};
 }
--- include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.h
+++ include/base/peripheral/sdram/chip/w9825g6kh_6/W9825G6KH_6.h
@@ -1,7 +1,5 @@
 #pragma once
-#include "base/bit/bit.h"
 #include "base/peripheral/sdram/SdramController.h"
-#include <cstdint>
 
 namespace base
 {
@@ -11,105 +9,6 @@ namespace base
 		{
 			namespace w9825g6kh_6
 			{
-				///
-				/// @brief 读突发模式。
-				///
-				///
-				enum class ReadingBurstMode
-				{
-					///
-					/// @brief 连续地突发。
-					///
-					///
-					Sequential,
-
-					///
-					/// @brief 交错地突发。
-					///
-					///
-					Interleaved,
-				};
-
-				///
-				/// @brief 写突发模式。
-				///
-				///
-				enum class WritingBurstMode
-				{
-					///
-					/// @brief 编程的突发模式。
-					///
-					///
-					Programmed,
-
-					///
-					/// @brief 单点突发模式。即一次写入一个列，实际上就是不突发。
-					///
-					///
-					Single,
-				};
-
-				class ModeRegister
-				{
-				private:
-					uint32_t _value = 0;
-
-				public:
-					constexpr uint32_t Value() const
-					{
-						return _value;
-					}
-
-					constexpr uint32_t ReadingBurstLength() const
-					{
-						uint32_t value = base::bit::ReadBits(_value, 0, 3);
-						value *= 2;
-						if (value == 0)
-						{
-							value = 1;
-						}
-
-						return value;
-					}
-
-					constexpr void SetReadingBurstLength(uint32_t value)
-					{
-						base::bit::WriteBits(_value, 0, 3, value / 2);
-					}
-
-					constexpr base::sdram::chip::w9825g6kh_6::ReadingBurstMode ReadingBurstMode() const
-					{
-						if (base::bit::ReadBit(_value, 3))
-						{
-							return base::sdram::chip::w9825g6kh_6::ReadingBurstMode::Interleaved;
-						}
-
-						return ReadingBurstMode::Sequential;
-					}
-
-					constexpr void SetReadingBurstMode(base::sdram::chip::w9825g6kh_6::ReadingBurstMode value)
-					{
-						if (value == base::sdram::chip::w9825g6kh_6::ReadingBurstMode::Interleaved)
-						{
-							base::bit::WriteBit(_value, 3, true);
-						}
-						else
-						{
-							base::bit::WriteBit(_value, 3, false);
-						}
-					}
-
-					constexpr base::sdram::chip::w9825g6kh_6::WritingBurstMode WritingBurstMode() const
-					{
-						if (base::bit::ReadBit(_value, 9))
-						{
-							return base::sdram::chip::w9825g6kh_6::WritingBurstMode::Single;
-						}
-
-						return base::sdram::chip::w9825g6kh_6::WritingBurstMode::Programmed;
-					}
-				};
-
 				class W9825G6KH_6
 				{
 				private:
