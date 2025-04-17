b1bc8fe7c9e7411e32397ca67297b7ea74dcdb1d
hjc2000 <hjc.666@foxmail.com>
2025-04-17 16:05:51 +0800

    led_handle

--- include/base/peripheral/led/led_handle.h
+++ include/base/peripheral/led/led_handle.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "base/define.h"
 #include "base/peripheral/led/parameter.h"
+#include "base/ui/Color.h"
+#include <memory>
 
 namespace base
 {
@@ -8,10 +10,57 @@ namespace base
 	{
 		class led_handle;
 
+		/* #region open */
+
+		///
+		/// @brief 根据 LED 的 ID 打开 LED.
+		///
+		/// @param led_id
+		/// @return std::shared_ptr<led_handle>
+		///
+		std::shared_ptr<led_handle> open(int led_id);
+
+		///
+		/// @brief 根据 LED 的颜色打开 LED.
+		///
+		/// @param color
+		/// @return std::shared_ptr<led_handle>
+		///
+		std::shared_ptr<led_handle> open(base::ui::Color color);
+
+		/* #endregion */
+
+		/* #region 控制 LED 开关 */
+
+		///
+		/// @brief 打开 LED.
+		///
+		/// @param h
+		///
 		void turn_on(base::led::led_handle &h);
+
+		///
+		/// @brief 关闭 LED.
+		///
+		/// @param h
+		///
 		void turn_off(base::led::led_handle &h);
+
+		///
+		/// @brief 翻转 LED.
+		///
+		/// @param h
+		///
 		void toggle(base::led::led_handle &h);
 
+		/* #endregion */
+
+		///
+		/// @brief 获取 LED 灯的状态。
+		///
+		/// @param h
+		/// @return base::led::State
+		///
 		base::led::State state(base::led::led_handle &h);
 
 		///
---
+++ include/base/ui/Color.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace base
+{
+	namespace ui
+	{
+		enum class Color
+		{
+			Red,
+			Green,
+			Blue,
+		};
+	} // namespace ui
+} // namespace base
