9c40e05a794c2e32b37d92a31d210d0d98bc91f9
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-19 23:52:16 +0800

    requires

--- include/base/bit/bit.h
+++ include/base/bit/bit.h
@@ -58,40 +58,9 @@ namespace base
 		/// @param num
 		/// @return
 		///
-		constexpr int HighZeroCount(uint8_t num)
-		{
-			return std::countl_zero(num);
-		}
-
-		///
-		/// @brief 从最高位开始数，有多少个连续的 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighZeroCount(uint16_t num)
-		{
-			return std::countl_zero(num);
-		}
-
-		///
-		/// @brief 从最高位开始数，有多少个连续的 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighZeroCount(uint32_t num)
-		{
-			return std::countl_zero(num);
-		}
-
-		///
-		/// @brief 从最高位开始数，有多少个连续的 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighZeroCount(uint64_t num)
+		template <typename RegisterType>
+			requires(std::is_integral_v<RegisterType>)
+		constexpr int HighZeroCount(RegisterType num)
 		{
 			return std::countl_zero(num);
 		}
@@ -104,52 +73,12 @@ namespace base
 		/// @param num
 		/// @return
 		///
-		constexpr int HighestOneBitIndex(uint8_t num)
+		template <typename RegisterType>
+			requires(std::is_integral_v<RegisterType>)
+		constexpr int HighestOneBitIndex(RegisterType num)
 		{
 			int count = HighZeroCount(num);
-			return 8 - count - 1;
-		}
-
-		///
-		/// @brief 最高位的 1 的索引。
-		///
-		/// @note 例如 0x1 的 bit0 是最高位的 1，于是返回 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighestOneBitIndex(uint16_t num)
-		{
-			int count = HighZeroCount(num);
-			return 16 - count - 1;
-		}
-
-		///
-		/// @brief 最高位的 1 的索引。
-		///
-		/// @note 例如 0x1 的 bit0 是最高位的 1，于是返回 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighestOneBitIndex(uint32_t num)
-		{
-			int count = HighZeroCount(num);
-			return 32 - count - 1;
-		}
-
-		///
-		/// @brief 最高位的 1 的索引。
-		///
-		/// @note 例如 0x1 的 bit0 是最高位的 1，于是返回 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr int HighestOneBitIndex(uint64_t num)
-		{
-			int count = HighZeroCount(num);
-			return 64 - count - 1;
+			return sizeof(num) * 8 - count - 1;
 		}
 
 		///
@@ -160,46 +89,9 @@ namespace base
 		/// @param num
 		/// @return
 		///
-		constexpr uint8_t AlignToLeft(uint8_t num)
-		{
-			return num << HighZeroCount(num);
-		}
-
-		///
-		/// @brief 将 num 进行左对齐。
-		///
-		/// @note 即将 num 进行左移位，使最高位的 1 位于最高有效位，即去除所有的前导 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr uint16_t AlignToLeft(uint16_t num)
-		{
-			return num << HighZeroCount(num);
-		}
-
-		///
-		/// @brief 将 num 进行左对齐。
-		///
-		/// @note 即将 num 进行左移位，使最高位的 1 位于最高有效位，即去除所有的前导 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr uint32_t AlignToLeft(uint32_t num)
-		{
-			return num << HighZeroCount(num);
-		}
-
-		///
-		/// @brief 将 num 进行左对齐。
-		///
-		/// @note 即将 num 进行左移位，使最高位的 1 位于最高有效位，即去除所有的前导 0.
-		///
-		/// @param num
-		/// @return
-		///
-		constexpr uint64_t AlignToLeft(uint64_t num)
+		template <typename RegisterType>
+			requires(std::is_integral_v<RegisterType>)
+		constexpr uint8_t AlignToLeft(RegisterType num)
 		{
 			return num << HighZeroCount(num);
 		}
