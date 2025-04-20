beaa830a1d841f552ffe8cff5f8dabcdfa1add5c
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-20 12:55:58 +0800

    phy

---
+++ include/base/peripheral/ethernet/phy/YT8512CPhyDriver.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "base/peripheral/ethernet/EthernetController.h"
+#include "base/peripheral/ethernet/parameter.h"
+#include "base/peripheral/ethernet/phy/IPhyDriver.h"
+
+namespace base
+{
+	namespace ethernet
+	{
+		class YT8512CPhyDriver :
+			public base::ethernet::IPhyDriver
+		{
+		private:
+			base::ethernet::EthernetController _ethernet_controller;
+
+		public:
+			YT8512CPhyDriver(base::ethernet::EthernetController const &controller);
+
+			virtual base::ethernet::EthernetController &EthernetController() override;
+
+			/// @brief 获取此网口的双工模式。
+			/// @return
+			virtual base::ethernet::DuplexMode DuplexMode() override;
+
+			/// @brief 获取此网口的速度。
+			/// @return
+			virtual base::bps Speed() override;
+		};
+
+	} // namespace ethernet
+} // namespace base
