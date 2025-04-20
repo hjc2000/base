62c83d1bce3a0af0b79ce3997055a1c4c9c664fd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-20 14:28:47 +0800

    MutexEthernetPort

--- include/base/peripheral/ethernet/MutexEthernetPort.h
+++ include/base/peripheral/ethernet/MutexEthernetPort.h
@@ -12,10 +12,6 @@ namespace base
 			public base::ethernet::IEthernetPort
 		{
 		private:
-			/// @brief 控制用的锁。
-			/// @note 控制与收发无关，所以要使用另外的一个锁。
-			std::shared_ptr<base::IMutex> _lock = base::CreateIMutex();
-
 			/// @brief 接收用的锁。
 			/// @note 以太网是双工的，接收和发送不能使用同一个锁。
 			std::shared_ptr<base::IMutex> _receiving_lock = base::CreateIMutex();
@@ -27,35 +23,58 @@ namespace base
 			base::ethernet::IEthernetPort *_port = nullptr;
 
 		public:
-			MutexEthernetPort(base::ethernet::IEthernetPort *port);
+			MutexEthernetPort(base::ethernet::IEthernetPort *port)
+			{
+				_port = port;
+			}
 
 			/// @brief 打开以太网端口。
 			/// @param mac MAC 地址。
-			void Open(base::Mac const &mac) override;
+			virtual void Open(base::Mac const &mac) override
+			{
+				_port->Open(mac);
+			}
 
 			/// @brief 发送。
 			/// @param spans
-			void Send(std::vector<base::ReadOnlySpan> const &spans) override;
+			virtual void Send(std::vector<base::ReadOnlySpan> const &spans) override
+			{
+				base::LockGuard l{*_sending_lock};
+				_port->Send(spans);
+			}
 
 			/// @brief 发送单个 span.
 			/// @note 默认实现是基于 void Send(base::IEnumerable<base::ReadOnlySpan> const &spans).
 			/// @param span
-			void Send(base::ReadOnlySpan const &span) override;
+			virtual void Send(base::ReadOnlySpan const &span) override
+			{
+				base::LockGuard l{*_sending_lock};
+				_port->Send(span);
+			}
 
 			/// @brief 收到以太网帧会触发此事件。
 			/// @note 事件回调中会传入一个装有完整的以太网帧的 base::ReadOnlySpan.
 			/// @return
-			virtual base::IEvent<base::ReadOnlySpan> &ReceivingEhternetFrameEvent() override;
+			virtual base::IEvent<base::ReadOnlySpan> &ReceivingEhternetFrameEvent() override
+			{
+				return _port->ReceivingEhternetFrameEvent();
+			}
 
 			/// @brief 连接事件。
 			/// @note 链路层连接建立后会触发事件。
 			/// @return
-			virtual base::IEvent<> &ConnectedEvent() override;
+			virtual base::IEvent<> &ConnectedEvent() override
+			{
+				return _port->ConnectedEvent();
+			}
 
 			/// @brief 断开连接事件。
 			/// @note 链路层连接断开后会触发事件。
 			/// @return
-			virtual base::IEvent<> &DisconnectedEvent() override;
+			virtual base::IEvent<> &DisconnectedEvent() override
+			{
+				return _port->DisconnectedEvent();
+			}
 		};
 	} // namespace ethernet
 } // namespace base
