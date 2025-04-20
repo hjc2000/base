36952e53bd87d8bca0b0153206711a49fdc7ba9d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-20 22:04:57 +0800

    create_mutex

--- include/base/task/mutex_handle.cpp
+++ include/base/task/mutex_handle.cpp
@@ -22,6 +22,11 @@ public:
 
 /* #region 全局函数 */
 
+std::shared_ptr<base::task::mutex_handle> base::task::create_mutex()
+{
+	return std::shared_ptr<base::task::mutex_handle>{new base::task::mutex_handle{}};
+}
+
 void base::task::lock(base::task::mutex_handle &h)
 {
 	h.lock();
