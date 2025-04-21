5551a23f1f542b239d169f123e18071928da75c3
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-04-21 22:01:53 +0800

    注释

--- include/base/container/IQueue.h
+++ include/base/container/IQueue.h
@@ -3,30 +3,46 @@
 
 namespace base
 {
-	/// @brief 队列接口
-	/// @tparam T
+	///
+	/// @brief 队列接口。
+	///
+	///
 	template <typename T>
 	class IQueue
 	{
 	public:
+		///
 		/// @brief 队列中元素个数。
+		///
 		/// @return
+		///
 		virtual int32_t Count() const = 0;
 
+		///
 		/// @brief 退队。队列为空会抛出异常。
 		/// @return
+		///
 		virtual T Dequeue() = 0;
 
+		///
 		/// @brief 尝试退队。
+		///
 		/// @param out
+		///
 		/// @return
+		///
 		virtual bool TryDequeue(T &out) = 0;
 
+		///
 		/// @brief 入队。
+		///
 		/// @param obj
+		///
 		virtual void Enqueue(T const &obj) = 0;
 
+		///
 		/// @brief 清空队列。
+		///
 		virtual void Clear() = 0;
 	};
 } // namespace base
