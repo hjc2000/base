700e6d4ae8ae4578b3808a1ca3ab910607dfb054
hjc2000 <hjc.666@foxmail.com>
2025-04-24 15:21:10 +0800

    cache

--- include/base/peripheral/cache/cache.h
+++ include/base/peripheral/cache/cache.h
@@ -1,6 +1,5 @@
 #pragma once
-#include <stddef.h>
-#include <stdint.h>
+#include <cstddef>
 
 namespace base
 {
@@ -11,21 +10,21 @@ namespace base
 		///
 		/// @note 会同时使能 D-Cache 和 I-Cache.
 		///
-		void Enable();
+		void enable();
 
 		///
 		/// @brief 失能缓存。
 		///
 		/// @note 会同时失能 D-Cache 和 I-Cache.
 		///
-		void Disable();
+		void disable();
 
 		///
 		/// @brief 是 D-Cache 无效。
 		///
 		/// @note 即将 D-Cache 的数据标记为过期的，重新从内存中读取数据放到 D-Cache 中。
 		///
-		void InvalidateDCache();
+		void invalidate_d_cache();
 
 		///
 		/// @brief 将指定内存地址处的指定大小的数据进行 D-Cache 无效化。即如果这个地方的数据
@@ -34,7 +33,7 @@ namespace base
 		/// @param address
 		/// @param size
 		///
-		void InvalidateDCache(void *address, int32_t size);
+		void invalidate_d_cache(void *address, size_t size);
 
 		///
 		/// @brief 清除 D-Cache。
@@ -42,12 +41,12 @@ namespace base
 		/// @note 即将原本从内存中读取到 D-Cache 中，并且已经修改过的 D-Cache 的数据写入
 		/// 内存中而不是仅仅停留在 D-Cache 中。
 		///
-		void CleanDCache();
+		void clean_d_cache();
 
 		///
 		/// @brief 清除并且无效化 D-Cache.
 		///
-		void CleanAndInvalidateDCache();
+		void clean_and_invalidate_d_cache();
 
 	} // namespace cache
 } // namespace base
