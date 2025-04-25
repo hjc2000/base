944c94fe73c1be0fe57eafcad4706c6a4ef4da92
hjc2000 <hjc.666@foxmail.com>
2025-04-25 16:13:03 +0800

    constexpr

--- include/base/embedded/heap/MemoryBlockLinkListNode.h
+++ include/base/embedded/heap/MemoryBlockLinkListNode.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/bit/bit.h"
 #include <cstddef>
 
 namespace base
@@ -20,6 +21,38 @@ namespace base
 			///
 			size_t _size{};
 		};
+
+		/* Check if multiplying a and b will result in overflow. */
+		constexpr bool HeapMultiplyWillOverflow(size_t a, size_t b)
+		{
+			return ((a) > 0) && ((b) > (SIZE_MAX / (a)));
+		}
+
+		/* Check if adding a and b will result in overflow. */
+		constexpr bool HeapAddWillOverflow(size_t a, size_t b)
+		{
+			return (a) > (SIZE_MAX - (b));
+		}
+
+		constexpr bool HeapBlockSizeIsValid(size_t _size)
+		{
+			return (_size & base::bit::MSB<size_t>()) == 0;
+		}
+
+		constexpr bool HeapBlockIsAllocated(MemoryBlockLinkListNode *pxBlock)
+		{
+			return ((pxBlock->_size) & base::bit::MSB<size_t>()) != 0;
+		}
+
+		constexpr void HeapAllocateBlock(MemoryBlockLinkListNode *pxBlock)
+		{
+			(pxBlock->_size) |= base::bit::MSB<size_t>();
+		}
+
+		constexpr void HeapFreeBlock(MemoryBlockLinkListNode *pxBlock)
+		{
+			(pxBlock->_size) &= ~base::bit::MSB<size_t>();
+		}
 
 	} // namespace heap
 } // namespace base
