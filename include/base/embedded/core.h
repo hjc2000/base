f83c9d45c5998cdbcd9daebb8108762b035dee0d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-03 22:40:02 +0800

    core

---
+++ include/base/embedded/core.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace base
+{
+	namespace core
+	{
+		///
+		/// @brief 执行复位重启。就好像重新上电一样，只不过没有实际断电。
+		/// 也等同于通过复位引脚复位。
+		///
+		void reset();
+
+	} // namespace core
+} // namespace base
