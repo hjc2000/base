d4546a603ee74ba2ccd6a0a2c8cd540a35bb6ffd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-03 23:03:18 +0800

    void initialize();

--- include/base/embedded/core.h
+++ include/base/embedded/core.h
@@ -5,6 +5,12 @@ namespace base
 	namespace core
 	{
 		///
+		/// @brief 执行初始化。需要在 main 的开头调用。
+		///
+		///
+		void initialize();
+
+		///
 		/// @brief 执行复位重启。就好像重新上电一样，只不过没有实际断电。
 		/// 也等同于通过复位引脚复位。
 		///
