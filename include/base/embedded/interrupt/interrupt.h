582a465c024fca910d92c97fe63a25eb740bcaa2
hjc2000 <hjc.666@foxmail.com>
2025-05-06 15:24:00 +0800

    interrupt

--- include/base/embedded/interrupt/interrupt.h
+++ include/base/embedded/interrupt/interrupt.h
@@ -27,5 +27,53 @@ namespace base
 		///
 		void enable_interrupt(int32_t irq, int32_t priority) noexcept;
 
+		///
+		/// @brief 禁用全局中断。
+		///
+		///
+		void disable_global_interrupt() noexcept;
+
+		///
+		/// @brief 使能全局中断。
+		///
+		///
+		void enable_global_interrupt() noexcept;
+
+		///
+		/// @brief 全局中断是否被禁用了。
+		///
+		/// @return
+		///
+		bool is_global_interrupt_disabled() noexcept;
+
+		///
+		/// @brief 递归地禁用全局中断。
+		///
+		///
+		void disable_global_interrupt_recursive() noexcept;
+
+		///
+		/// @brief 递归地使能全局中断。
+		///
+		///
+		void enable_global_interrupt_recursive() noexcept;
+
+		///
+		/// @brief 构造时禁用全局中断，析构时使能全局中断。
+		///
+		class GlobalInterruptionGuard
+		{
+		public:
+			GlobalInterruptionGuard()
+			{
+				disable_global_interrupt_recursive();
+			}
+
+			~GlobalInterruptionGuard()
+			{
+				enable_global_interrupt_recursive();
+			}
+		};
+
 	} // namespace interrupt
 } // namespace base
