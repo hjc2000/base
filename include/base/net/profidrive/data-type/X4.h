4f7455071dfe3a90e4a7cdd59a366680025411cb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-10 12:06:51 +0800

    X4

--- include/base/net/profidrive/data-type/X4.h
+++ include/base/net/profidrive/data-type/X4.h
@@ -23,9 +23,14 @@ namespace base
 			///
 			/// @return int32_t
 			///
-			int32_t Factor() const;
+			int32_t Factor() const
+			{
+				return static_cast<int64_t>(1 << 28);
+			}
 
 		public:
+			/* #region 构造函数 */
+
 			X4() = default;
 
 			///
@@ -33,17 +38,43 @@ namespace base
 			///
 			/// @param span
 			///
-			X4(base::ReadOnlySpan const &span);
+			X4(base::ReadOnlySpan const &span)
+			{
+				// 行规特定数据类型用一个整型来储存它的值，这个整型值可以认为是将分数的实际值乘上 Factor
+				// 放大后截断为整型。
+				//
+				// 想要获得分数的实际值，就将这个整型除以 Factor.
+				int32_t x4 = _converter.FromBytes<int32_t>(span);
+				_value = base::Fraction{x4, Factor()};
+			}
 
 			///
 			/// @brief 通过实际的分数值构造行规特定数据类型。
 			///
 			/// @param value
 			///
-			X4(base::Fraction const &value);
+			X4(base::Fraction const &value)
+			{
+				_value = value;
+			}
 
-			explicit operator base::Fraction() const;
-			base::Array<uint8_t, 4> BufferForSending() const;
+			/* #endregion */
+
+			base::Fraction const &Value() const
+			{
+				return _value;
+			}
+
+			base::Array<uint8_t, 4> BufferForSending() const
+			{
+				// 行规特定数据类型用一个整型来储存它的值，这个整型值可以认为是将分数的实际值乘上 Factor
+				// 放大后截断为整型。
+				int32_t x4 = static_cast<int32_t>(_value * Factor());
+
+				base::Array<uint8_t, 4> buffer;
+				_converter.GetBytes(x4, buffer.Span());
+				return buffer;
+			}
 
 			X4 operator+(X4 const &right_value) const
 			{
