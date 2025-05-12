bb9f39b75caaffb1f3af5c2cabac62e5f5aa9a6f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-05-13 00:23:36 +0800

    注释

--- include/base/net/ethernet/EthernetFrameReader.h
+++ include/base/net/ethernet/EthernetFrameReader.h
@@ -54,7 +54,7 @@ namespace base
 			///
 			/// @brief 802.1Q标签。大小：4 字节。
 			///
-			/// @return base::ReadOnlySpan
+			/// @return
 			///
 			base::ReadOnlySpan VlanTag() const
 			{
@@ -69,8 +69,7 @@ namespace base
 			///
 			/// @brief 是否具有 802.1Q标签。
 			///
-			/// @return true
-			/// @return false
+			/// @return
 			///
 			bool HasVlanTag() const
 			{
@@ -82,17 +81,17 @@ namespace base
 			///
 			/// @brief 类型或长度。
 			///
-			/// 	@note 整型值小于等于 1500，则表示长度，大于 1500 则表示帧类型。当含义是帧类型时，
-			/// 	这个值是用来给上层的处理程序看的，让上层的处理程序决定将以太网帧送往哪个协议处理程序。
+			/// @note 整型值小于等于 1500，则表示长度，大于 1500 则表示帧类型。当含义是帧类型时，
+			/// 这个值是用来给上层的处理程序看的，让上层的处理程序决定将以太网帧送往哪个协议处理程序。
 			///
-			/// 	@note 1500 的 16 进制：0x05dc.
+			/// @note 1500 的 16 进制：0x05dc.
 			///
-			/// 	@note 下面这几个值都大于 1500：
-			/// 		@li 0x0800：表示 IPv4 数据包。
-			/// 		@li 0x0806：表示 ARP 请求。
-			/// 		@li 0x86DD：表示 IPv6 数据包。
+			/// @note 下面这几个值都大于 1500：
+			/// 	@li 0x0800：表示 IPv4 数据包。
+			/// 	@li 0x0806：表示 ARP 请求。
+			/// 	@li 0x86DD：表示 IPv6 数据包。
 			///
-			/// @return base::ethernet::LengthOrTypeEnum
+			/// @return
 			///
 			base::ethernet::LengthOrTypeEnum TypeOrLength() const
 			{
@@ -111,19 +110,19 @@ namespace base
 			///
 			/// @brief 载荷数据。
 			///
-			/// 	@note 字节数的取值范围：[46, 1500].
+			/// @note 字节数的取值范围：[46, 1500].
 			///
-			/// 	@note 巨型帧可以超过 1500 字节。但是需要网络设备支持，否则会导致无法传输。
+			/// @note 巨型帧可以超过 1500 字节。但是需要网络设备支持，否则会导致无法传输。
 			///
-			/// 	@note 如果不满 46 字节，需要后面填充 0，使其达到 46 字节。
+			/// @note 如果不满 46 字节，需要后面填充 0，使其达到 46 字节。
 			///
-			/// 	@note 因为可能会有填充，所以需要靠 TypeOrLength 属性来识别出有效字节数。
+			/// @note 因为可能会有填充，所以需要靠 TypeOrLength 属性来识别出有效字节数。
 			///
-			/// 	@note 这里返回的 ReadOnlySpan 是构造函数中交给本对象的 ReadOnlySpan
-			/// 	去除以太网头部后的全部剩余空间，并不是实际的有效载荷。本类是以太网帧类，
-			/// 	无法识别出有效载荷的长度，这种工作只能交给上层。
+			/// @note 这里返回的 ReadOnlySpan 是构造函数中交给本对象的 ReadOnlySpan
+			/// 去除以太网头部后的全部剩余空间，并不是实际的有效载荷。本类是以太网帧类，
+			/// 无法识别出有效载荷的长度，这种工作只能交给上层。
 			///
-			/// @return base::ReadOnlySpan
+			/// @return
 			///
 			base::ReadOnlySpan Payload() const
 			{
@@ -140,7 +139,7 @@ namespace base
 			///
 			/// @brief 序列化为 json
 			///
-			/// @return base::Json
+			/// @return
 			///
 			base::Json ToJson() const override
 			{
