bbf234d1d2897a22116fe4357e6f2a93b7b85d04
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-05 00:16:17 +0800

    docs(tool.h): 完善函数参数注释说明

--- include/base/file/tool.h
+++ include/base/file/tool.h
@@ -10,8 +10,8 @@ namespace base
 			///
 			/// @brief 递归遍历指定路径下的文件，移动到输出目录并重命名为数字。
 			///
-			/// @param input_path
-			/// @param temp_path
+			/// @param input_path 要被处理的文件所在的文件夹。
+			/// @param temp_path 临时输出目录。
 			///
 			void batch_rename_files_to_numbers(base::Path const &input_path,
 											   base::Path const &temp_path);
