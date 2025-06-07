b39228df902a2647aa826973827b48058a2ce601
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-07 22:57:06 +0800

    feat(profidrive): 为ZSW1状态字添加位操作方法和详细注释
    添加ZSW1状态字的位操作方法，包括11个状态位的读写接口和详细注释说明每个位的含义

--- include/base/net/profidrive/telegram/ReceivedTelegram3.h
+++ include/base/net/profidrive/telegram/ReceivedTelegram3.h
@@ -32,6 +32,205 @@ namespace base
 				_zsw1 = value;
 			}
 
+			/* #region ZSW1 的各个位代表的属性 */
+
+			///
+			/// @brief 为 true 表示接通准备就绪。
+			///
+			/// @return
+			///
+			bool ZSW1_ReadyForSwitchingOn() const
+			{
+				return _zsw1[0];
+			}
+
+			void Set_ZSW1_ReadyForSwitchingOn(bool value)
+			{
+				_zsw1[0] = value;
+			}
+
+			///
+			/// @brief 为 true 表示操作准备就绪。
+			///
+			/// @return
+			///
+			bool ZSW1_ReadyForOperation() const
+			{
+				return _zsw1[1];
+			}
+
+			void Set_ZSW1_ReadyForOperation(bool value)
+			{
+				_zsw1[1] = value;
+			}
+
+			///
+			/// @brief 为 true 表示操作使能，驱动器开始跟随设定点。
+			///
+			/// @return
+			///
+			bool ZSW1_OperationEnabled() const
+			{
+				return _zsw1[2];
+			}
+
+			void Set_ZSW1_OperationEnabled(bool value)
+			{
+				_zsw1[2] = value;
+			}
+
+			///
+			/// @brief 为 true 表示驱动器存在错误。
+			///
+			/// @return
+			///
+			bool ZSW1_Fault() const
+			{
+				return _zsw1[3];
+			}
+
+			void Set_ZSW1_Fault(bool value)
+			{
+				_zsw1[3] = value;
+			}
+
+			///
+			/// @brief 为 true 表示惯性停机无效，为 false 表示惯性停机有效。
+			///
+			/// @return
+			///
+			bool ZSW1_CoastStopOff() const
+			{
+				return _zsw1[4];
+			}
+
+			void Set_ZSW1_CoastStopOff(bool value)
+			{
+				_zsw1[4] = value;
+			}
+
+			///
+			/// @brief 为 true 表示惯性停机有效。
+			///
+			/// @return
+			///
+			bool ZSW1_CoastStopOn() const
+			{
+				return !_zsw1[4];
+			}
+
+			void Set_ZSW1_CoastStopOn(bool value)
+			{
+				_zsw1[4] = !value;
+			}
+
+			///
+			/// @brief 为 true 表示快速停机无效，为 false 表示快速停机有效。
+			///
+			/// @return
+			///
+			bool ZSW1_QuickStopOff() const
+			{
+				return _zsw1[5];
+			}
+
+			void Set_ZSW1_QuickStopOff(bool value)
+			{
+				_zsw1[5] = value;
+			}
+
+			///
+			/// @brief 为 true 表示快速停机有效，为 false 表示快速停机无效。
+			///
+			/// @return
+			///
+			bool ZSW1_QuickStopOn() const
+			{
+				return !_zsw1[5];
+			}
+
+			void Set_ZSW1_QuickStopOn(bool value)
+			{
+				_zsw1[5] = !value;
+			}
+
+			///
+			/// @brief 为 true 表示接通被禁止，为 false 表示接通没有被禁止。
+			///
+			/// @return
+			///
+			bool ZSW1_SwitchingOnInhibited() const
+			{
+				return _zsw1[6];
+			}
+
+			void Set_ZSW1_SwitchingOnInhibited(bool value)
+			{
+				_zsw1[6] = value;
+			}
+
+			///
+			/// @brief 为 true 表示存在警告，为 false 表示不存在警告。
+			///
+			/// @return
+			///
+			bool ZSW1_Warning() const
+			{
+				return _zsw1[7];
+			}
+
+			void Set_ZSW1_Warning(bool value)
+			{
+				_zsw1[7] = value;
+			}
+
+			///
+			/// @brief 为 true 表示速度偏差在容忍范围内，为 false 表示速度偏差超出容忍范围。
+			///
+			/// @return
+			///
+			bool ZSW1_SpeedErrorWithinTolerance() const
+			{
+				return _zsw1[8];
+			}
+
+			void Set_ZSW1_SpeedErrorWithinTolerance(bool value)
+			{
+				_zsw1[8] = value;
+			}
+
+			///
+			/// @brief 为 true 表示有控制请求，为 false 表示没有控制请求。
+			///
+			/// @return
+			///
+			bool ZSW1_HasControlRequest() const
+			{
+				return _zsw1[9];
+			}
+
+			void Set_ZSW1_HasControlRequest(bool value)
+			{
+				_zsw1[9] = value;
+			}
+
+			///
+			/// @brief 为 true 表示速度达到或超过比较值，为 false 表示速度未达到比较值。
+			///
+			/// @return
+			///
+			bool ZSW1_SpeedReachesComparison() const
+			{
+				return _zsw1[10];
+			}
+
+			void Set_ZSW1_SpeedReachesComparison(bool value)
+			{
+				_zsw1[10] = value;
+			}
+
+			/* #endregion */
+
 			///
 			/// @brief 实际转速。
 			///
