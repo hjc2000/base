00ce09ed8335186df9de125eefeaa7a0d86dbff6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-07 11:55:58 +0800

    refactor(sdram): 优化W9825G6KH-6芯片时序参数计算
    移除时序计算中保守的额外时钟周期，并调整CAS延迟为2。这些修改基于实际测试结果，使时序参数更精确反映硬件需求。

--- include/base/embedded/sdram/chip/w9825g6kh_6/TimingProvider.h
+++ include/base/embedded/sdram/chip/w9825g6kh_6/TimingProvider.h
@@ -33,17 +33,16 @@ namespace base
 					{
 						base::Nanoseconds clock_period{clock_frequency};
 
-						// 计算时序时，很多地方额外加了一个 clock_period，这是为了提供更加保守的时序。
 						base::sdram::sdram_timing ret{};
 						ret.set_row_count(8192);
 						ret.set_clock_frequency(clock_frequency);
-						ret.set_t_rsc(2 * clock_period + clock_period);
-						ret.set_t_xsr(base::Nanoseconds{72} + clock_period);
-						ret.set_t_ras(base::Nanoseconds{42} + clock_period);
-						ret.set_t_rc(base::Nanoseconds{60} + clock_period);
-						ret.set_t_wr(2 * clock_period + clock_period);
-						ret.set_t_rp(base::Nanoseconds{15} + clock_period);
-						ret.set_t_rcd(base::Nanoseconds{15} + clock_period);
+						ret.set_t_rsc(2 * clock_period);
+						ret.set_t_xsr(base::Nanoseconds{72});
+						ret.set_t_ras(base::Nanoseconds{42});
+						ret.set_t_rc(base::Nanoseconds{60});
+						ret.set_t_wr(2 * clock_period);
+						ret.set_t_rp(base::Nanoseconds{15});
+						ret.set_t_rcd(base::Nanoseconds{15});
 
 						// 提前 10ms 刷新
 						ret.set_t_ref(base::Nanoseconds{std::chrono::milliseconds{64 - 10}});
@@ -54,8 +53,7 @@ namespace base
 						}
 						else
 						{
-							// 为了保险，始终使用 3，不知道怎么回事，家里的开发板用 2 一跑就崩。
-							ret.set_cas_latency(3);
+							ret.set_cas_latency(2);
 						}
 
 						return ret;
