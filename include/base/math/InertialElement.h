a9ee79358dd2a3c03220cfac8b0cf245bf92416f
hjc2000 <hjc.666@foxmail.com>
2025-06-13 09:54:35 +0800

    refactor(math): 将InertialElement重构为模板类并内联实现
    将InertialElement从具体类重构为模板类，移除cpp文件并将所有实现内联到头文件中。同时优化了内部类的命名和文档注释。

--- include/base/math/InertialElement.h
+++ include/base/math/InertialElement.h
@@ -2,86 +2,147 @@
 
 namespace base
 {
-	/// @brief 惯性时间常数
-	class InertialElement_TimeConstant
-	{
-	private:
-		double _value = 0;
-
-	public:
-		explicit InertialElement_TimeConstant(double value)
-		{
-			_value = value;
-		}
-
-		double Value() const
-		{
-			return _value;
-		}
-	};
-
-	/// @brief 采样周期
-	class InertialElement_SampleInterval
-	{
-	private:
-		double _value = 0;
-
-	public:
-		explicit InertialElement_SampleInterval(double value)
-		{
-			_value = value;
-		}
-
-		double Value() const
-		{
-			return _value;
-		}
-	};
-
+	///
 	/// @brief 一阶惯性环节。
+	///
+	///
+	template <typename T>
 	class InertialElement final
-
 	{
 	private:
-		double _inertial_time_constant = 0;
-		double _sample_interval = 0.001;
-		double _kx = 1;
-		double _ky = 1;
-		double _current_output = 0;
+		T _inertial_time_constant{};
+		T _sample_interval{};
+		T _kx{};
+		T _ky{};
+		T _current_output{};
 
 	public:
+		///
+		/// @brief 惯性时间常数
+		///
+		///
+		class TimeConstant
+		{
+		private:
+			T _value = 0;
+
+		public:
+			explicit TimeConstant(T value)
+			{
+				_value = value;
+			}
+
+			T Value() const
+			{
+				return _value;
+			}
+		};
+
+		///
+		/// @brief 采样周期
+		///
+		///
+		class SampleInterval
+		{
+		private:
+			T _value = 0;
+
+		public:
+			explicit SampleInterval(T value)
+			{
+				_value = value;
+			}
+
+			T Value() const
+			{
+				return _value;
+			}
+		};
+
+		///
 		/// @brief 一阶惯性环节。
+		///
 		/// @param inertial_time_constant 惯性时间常数。
 		/// @param sample_interval 采样周期。
-		InertialElement(InertialElement_TimeConstant const &inertial_time_constant,
-						InertialElement_SampleInterval const &sample_interval);
+		///
+		InertialElement(base::InertialElement<T>::TimeConstant const &inertial_time_constant,
+						base::InertialElement<T>::SampleInterval const &sample_interval)
+		{
+			ChangeParameter(inertial_time_constant, sample_interval);
+		}
 
+		///
 		/// @brief 向惯性环节输入一个值，并获取该值产生的输出。
+		///
 		/// @param x
+		///
 		/// @return
-		double Input(double x);
+		///
+		T Input(T x)
+		{
+			_current_output = _ky * _current_output + _kx * x;
+			return _current_output;
+		}
 
+		///
 		/// @brief 当前的输出。
+		///
 		/// @return
-		double CurrentOutput() const;
+		///
+		T CurrentOutput() const
+		{
+			return _current_output;
+		}
 
+		///
 		/// @brief 直接更改当前输出值。
+		///
 		/// @note 可以用来更改惯性环节的初态。
+		///
 		/// @param value
-		void SetCurrentOutput(double value);
+		///
+		void SetCurrentOutput(T value)
+		{
+			_current_output = value;
+		}
 
+		///
 		/// @brief 更改惯性时间常数
+		///
 		/// @param inertial_time_constant
-		void ChangeParameter(InertialElement_TimeConstant const &inertial_time_constant);
+		///
+		void ChangeParameter(base::InertialElement<T>::TimeConstant const &inertial_time_constant)
+		{
+			_inertial_time_constant = inertial_time_constant.Value();
+			_kx = _sample_interval / (_inertial_time_constant + _sample_interval);
+			_ky = _inertial_time_constant / (_inertial_time_constant + _sample_interval);
+		}
 
+		///
 		/// @brief 更改采样周期
+		///
 		/// @param sample_interval
-		void ChangeParameter(InertialElement_SampleInterval const &sample_interval);
+		///
+		void ChangeParameter(base::InertialElement<T>::SampleInterval const &sample_interval)
+		{
+			_sample_interval = sample_interval.Value();
+			_kx = _sample_interval / (_inertial_time_constant + _sample_interval);
+			_ky = _inertial_time_constant / (_inertial_time_constant + _sample_interval);
+		}
 
+		///
 		/// @brief 更改惯性时间常数和采样周期。
+		///
 		/// @param inertial_time_constant
 		/// @param sample_interval
-		void ChangeParameter(InertialElement_TimeConstant const &inertial_time_constant,
-							 InertialElement_SampleInterval const &sample_interval);
+		///
+		void ChangeParameter(base::InertialElement<T>::TimeConstant const &inertial_time_constant,
+							 base::InertialElement<T>::SampleInterval const &sample_interval)
+		{
+			_inertial_time_constant = inertial_time_constant.Value();
+			_sample_interval = sample_interval.Value();
+			_kx = _sample_interval / (_inertial_time_constant + _sample_interval);
+			_ky = _inertial_time_constant / (_inertial_time_constant + _sample_interval);
+		}
 	};
 } // namespace base
