54a2c2740cbbbdd6b5214119437ed78d50e6208e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-21 21:52:08 +0800

    "移除未使用的头文件引用，清理 main.cpp 中的测试代码"

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,10 +1,6 @@
-#include "base/bit/bit.h"
 #include "base/math/Fraction.h"
-#include "base/string/Parse.h"
-#include "base/string/ToHexString.h"
 #include "base/wrapper/number-wrapper.h"
 #include <cmath>
-#include <cstdint>
 #include <cstdlib>
 #include <iomanip>
 #include <iostream>
@@ -33,14 +29,5 @@ int main()
 				  << std::setprecision(precision)
 				  << static_cast<double>(f) - std::numbers::pi
 				  << std::endl;
-	}
-
-	base::test::test_parse_double();
-
-	{
-		constexpr uint64_t a = static_cast<uint64_t>(0x123) << 52;
-		constexpr uint64_t b = base::bit::ReadBits(a, 52, 63);
-		std::cout << base::ToHexString(a) << std::endl;
-		std::cout << base::ToHexString(b) << std::endl;
 	}
 }
