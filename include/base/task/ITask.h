23a3aa07ba52202233f31a984dcffef78311c348
hjc2000 <hjc.666@foxmail.com>
2025-06-23 16:31:23 +0800

    "为 ITask.h 中的 IsCompleted 和 Wait 方法添加详细注释说明"

--- include/base/task/ITask.h
+++ include/base/task/ITask.h
@@ -7,8 +7,17 @@ namespace base
 		class ITask
 		{
 		public:
+			///
+			/// @brief 任务已经完成。
+			///
+			/// @return 已完成会返回 true，否则返回 false.
+			///
 			virtual bool IsCompleted() const = 0;
 
+			///
+			/// @brief 等待任务完成。会阻塞当前线程。
+			///
+			///
 			virtual void Wait() = 0;
 		};
 
