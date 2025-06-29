cfc41c6a735bae152e15dde9e1b5d76149ab1e84
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-29 22:52:36 +0800

    "重构 ThreadPool 实现，将类定义与实现分离到不同文件"

--- include/base/task/ThreadPool.cpp
+++ include/base/task/ThreadPool.cpp
@@ -1,1 +1,181 @@
 #include "ThreadPool.h" // IWYU pragma: keep
+#include "base/Console.h"
+#include "base/string/define.h"
+#include "base/task/TaskCompletionSignal.h"
+#include "task.h"
+#include <exception>
+#include <stdexcept>
+
+/* #region TaskContext */
+
+class base::task::ThreadPool::Task final :
+	public base::task::ITask
+{
+private:
+	std::function<void()> _func{};
+	base::task::TaskCompletionSignal _signal{false};
+
+public:
+	Task(std::function<void()> const &func)
+		: _func(func)
+	{
+	}
+
+	void operator()() noexcept
+	{
+		base::task::TaskCompletionSignalGuard g{_signal};
+		if (_func == nullptr)
+		{
+			return;
+		}
+
+		try
+		{
+			_func();
+		}
+		catch (std::exception const &e)
+		{
+			base::console.WriteError(CODE_POS_STR + e.what());
+		}
+		catch (...)
+		{
+			base::console.WriteError(CODE_POS_STR + "未知异常。");
+		}
+	}
+
+	///
+	/// @brief 任务已经完成。
+	///
+	/// @return 已完成会返回 true，否则返回 false.
+	///
+	virtual bool IsCompleted() const override
+	{
+		return _signal.IsCompleted();
+	}
+
+	///
+	/// @brief 等待任务完成。会阻塞当前线程。
+	///
+	///
+	virtual void Wait() override
+	{
+		_signal.Wait();
+	}
+
+}; // class TaskContext
+
+/* #endregion */
+
+/* #region Worker */
+
+class base::task::ThreadPool::Worker final :
+	base::IDisposable
+{
+private:
+	base::BlockingQueue<std::shared_ptr<Task>> &_task_queue;
+	std::shared_ptr<base::task::ITask> _task{};
+	std::atomic_bool _disposed = false;
+
+	void ThreadFunc()
+	{
+		while (true)
+		{
+			try
+			{
+				if (_disposed)
+				{
+					return;
+				}
+
+				std::shared_ptr<Task> task = _task_queue.Dequeue();
+				if (task == nullptr)
+				{
+					continue;
+				}
+
+				(*task)();
+			}
+			catch (...)
+			{
+			}
+		}
+	}
+
+public:
+	Worker(base::BlockingQueue<std::shared_ptr<Task>> &task_queue)
+		: _task_queue(task_queue)
+	{
+		_task = base::task::run([this]()
+								{
+									ThreadFunc();
+								});
+	}
+
+	~Worker()
+	{
+		Dispose();
+	}
+
+	///
+	/// @brief 主动释放对象，让对象不再能够工作。
+	///
+	///
+	virtual void Dispose() override
+	{
+		if (_disposed)
+		{
+			return;
+		}
+
+		_disposed = true;
+		_task->Wait();
+	}
+
+}; // class Worker
+
+/* #endregion */
+
+base::task::ThreadPool::ThreadPool(int32_t thread_count)
+	: _task_queue(thread_count)
+{
+	if (thread_count <= 0)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "线程数必须大于 0."};
+	}
+
+	_thread_count = thread_count;
+	for (int32_t i = 0; i < _thread_count; i++)
+	{
+		_workers.push_back(std::shared_ptr<Worker>{new Worker{_task_queue}});
+	}
+}
+
+void base::task::ThreadPool::Dispose()
+{
+	if (_disposed)
+	{
+		return;
+	}
+
+	_disposed = true;
+
+	// 先释放队列，让其不再具有阻塞能力。
+	_task_queue.Dispose();
+
+	for (auto &worker : _workers)
+	{
+		worker->Dispose();
+	}
+}
+
+std::shared_ptr<base::task::ITask> base::task::ThreadPool::Run(std::function<void()> const &task_func)
+{
+	if (_disposed)
+	{
+		throw base::ObjectDisposedException{};
+	}
+
+	std::shared_ptr<Task> task{new Task{task_func}};
+	_task_queue.Enqueue(task);
+	return task;
+}
--- include/base/task/ThreadPool.h
+++ include/base/task/ThreadPool.h
@@ -1,17 +1,11 @@
 #pragma once
-#include "base/Console.h"
 #include "base/container/BlockingQueue.h"
 #include "base/IDisposable.h"
-#include "base/string/define.h"
-#include "base/task/TaskCompletionSignal.h"
 #include "ITask.h"
-#include "task.h"
 #include <atomic>
 #include <cstdint>
-#include <exception>
 #include <functional>
 #include <memory>
-#include <stdexcept>
 #include <vector>
 
 namespace base
@@ -22,134 +16,8 @@ namespace base
 			base::IDisposable
 		{
 		private:
-			/* #region TaskContext */
-
-			class Task final :
-				public base::task::ITask
-			{
-			private:
-				std::function<void()> _func{};
-				base::task::TaskCompletionSignal _signal{false};
-
-			public:
-				Task(std::function<void()> const &func)
-					: _func(func)
-				{
-				}
-
-				void operator()() noexcept
-				{
-					base::task::TaskCompletionSignalGuard g{_signal};
-					if (_func == nullptr)
-					{
-						return;
-					}
-
-					try
-					{
-						_func();
-					}
-					catch (std::exception const &e)
-					{
-						base::console.WriteError(CODE_POS_STR + e.what());
-					}
-					catch (...)
-					{
-						base::console.WriteError(CODE_POS_STR + "未知异常。");
-					}
-				}
-
-				///
-				/// @brief 任务已经完成。
-				///
-				/// @return 已完成会返回 true，否则返回 false.
-				///
-				virtual bool IsCompleted() const override
-				{
-					return _signal.IsCompleted();
-				}
-
-				///
-				/// @brief 等待任务完成。会阻塞当前线程。
-				///
-				///
-				virtual void Wait() override
-				{
-					_signal.Wait();
-				}
-
-			}; // class TaskContext
-
-			/* #endregion */
-
-			/* #region Worker */
-
-			class Worker final :
-				base::IDisposable
-			{
-			private:
-				base::BlockingQueue<std::shared_ptr<Task>> &_task_queue;
-				std::shared_ptr<base::task::ITask> _task{};
-				std::atomic_bool _disposed = false;
-
-				void ThreadFunc()
-				{
-					while (true)
-					{
-						try
-						{
-							if (_disposed)
-							{
-								return;
-							}
-
-							std::shared_ptr<Task> task = _task_queue.Dequeue();
-							if (task == nullptr)
-							{
-								continue;
-							}
-
-							(*task)();
-						}
-						catch (...)
-						{
-						}
-					}
-				}
-
-			public:
-				Worker(base::BlockingQueue<std::shared_ptr<Task>> &task_queue)
-					: _task_queue(task_queue)
-				{
-					_task = base::task::run([this]()
-											{
-												ThreadFunc();
-											});
-				}
-
-				~Worker()
-				{
-					Dispose();
-				}
-
-				///
-				/// @brief 主动释放对象，让对象不再能够工作。
-				///
-				///
-				virtual void Dispose() override
-				{
-					if (_disposed)
-					{
-						return;
-					}
-
-					_disposed = true;
-					_task->Wait();
-				}
-
-			}; // class Worker
-
-			/* #endregion */
+			class Task;
+			class Worker;
 
 			int32_t _thread_count = 1;
 			std::vector<std::shared_ptr<Worker>> _workers;
@@ -157,20 +25,7 @@ namespace base
 			std::atomic_bool _disposed = false;
 
 		public:
-			ThreadPool(int32_t thread_count)
-				: _task_queue(thread_count)
-			{
-				if (thread_count <= 0)
-				{
-					throw std::invalid_argument{CODE_POS_STR + "线程数必须大于 0."};
-				}
-
-				_thread_count = thread_count;
-				for (int32_t i = 0; i < _thread_count; i++)
-				{
-					_workers.push_back(std::shared_ptr<Worker>{new Worker{_task_queue}});
-				}
-			}
+			ThreadPool(int32_t thread_count);
 
 			~ThreadPool()
 			{
@@ -181,23 +36,7 @@ namespace base
 			/// @brief 主动释放对象，让对象不再能够工作。
 			///
 			///
-			virtual void Dispose() override
-			{
-				if (_disposed)
-				{
-					return;
-				}
-
-				_disposed = true;
-
-				// 先释放队列，让其不再具有阻塞能力。
-				_task_queue.Dispose();
-
-				for (auto &worker : _workers)
-				{
-					worker->Dispose();
-				}
-			}
+			virtual void Dispose() override;
 
 			///
 			/// @brief 线程池是否已被释放。
@@ -214,17 +53,7 @@ namespace base
 			///
 			/// @param task_func
 			///
-			std::shared_ptr<base::task::ITask> Run(std::function<void()> const &task_func)
-			{
-				if (_disposed)
-				{
-					throw base::ObjectDisposedException{};
-				}
-
-				std::shared_ptr<Task> task{new Task{task_func}};
-				_task_queue.Enqueue(task);
-				return task;
-			}
+			std::shared_ptr<base::task::ITask> Run(std::function<void()> const &task_func);
 		};
 
 	} // namespace task
