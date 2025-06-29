ad765b34731e57fa0feaee8ee24fedc7913563ac
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-29 17:23:16 +0800

    "移除 main.cpp 中未使用的线程池相关代码，仅保留分数计算演示功能"

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,8 +1,5 @@
 #include "base/math/Fraction.h"
-#include "base/task/delay.h"
-#include "base/task/ThreadPool.h"
 #include "base/wrapper/number-wrapper.h"
-#include <chrono>
 #include <cmath>
 #include <cstdlib>
 #include <iomanip>
@@ -32,30 +29,5 @@ int main()
 				  << std::setprecision(precision)
 				  << static_cast<double>(f) - std::numbers::pi
 				  << std::endl;
-	}
-
-	{
-		base::task::ThreadPool pool{2};
-
-		pool.Run([&]()
-				 {
-					 while (!pool.Disposed())
-					 {
-						 std::cout << "66666666666666666666" << std::endl;
-						 base::task::Delay(std::chrono::milliseconds{1000});
-					 }
-				 });
-
-		pool.Run([&]()
-				 {
-					 while (!pool.Disposed())
-					 {
-						 std::cout << "77777777777777777777" << std::endl;
-						 base::task::Delay(std::chrono::milliseconds{1000});
-					 }
-				 });
-
-		std::cin.get();
-		pool.Dispose();
 	}
 }
