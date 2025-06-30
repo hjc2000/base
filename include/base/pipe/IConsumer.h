41f15947e5648d5967e7146cef5b8dbbe1c6aa0d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-06-30 21:10:46 +0800

    "优化管道接口文档注释，补充方法说明并统一注释格式"

--- include/base/pipe/IConsumer.h
+++ include/base/pipe/IConsumer.h
@@ -6,11 +6,16 @@ namespace base
 	class IConsumer
 	{
 	public:
+		///
 		/// @brief 向消费者送入数据
+		///
 		/// @param data
+		///
 		virtual void SendData(T &data) = 0;
 
+		///
 		/// @brief 冲洗消费者
+		///
 		virtual void Flush() = 0;
 	};
 } // namespace base
