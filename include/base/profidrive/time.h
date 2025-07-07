76cd09c20cf99837fca8f0c07661073145b84aa8
hjc2000 <hjc.666@foxmail.com>
2025-07-07 16:15:18 +0800

    "新增 PROFIdrive 时间处理模块，实现毫秒/秒级时间获取接口"

---
+++ include/base/profidrive/time.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <cstdint>
+
+namespace base
+{
+	namespace profidrive
+	{
+		int64_t time_in_milliseconds();
+
+		int64_t time_in_seconds();
+
+	} // namespace profidrive
+} // namespace base
