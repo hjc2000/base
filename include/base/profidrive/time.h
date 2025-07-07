1456b00334d94919fce316f8c26fcf72e637938f
hjc2000 <hjc.666@foxmail.com>
2025-07-07 16:22:10 +0800

    "完善 PROFIdrive 时间模块文档，为毫秒/秒级时间获取接口添加详细注释"

--- include/base/profidrive/time.h
+++ include/base/profidrive/time.h
@@ -5,8 +5,18 @@ namespace base
 {
 	namespace profidrive
 	{
+		///
+		/// @brief 获取单位为毫秒的时间。
+		///
+		/// @return
+		///
 		int64_t time_in_milliseconds();
 
+		///
+		/// @brief 获取单位为秒的时间。
+		///
+		/// @return
+		///
 		int64_t time_in_seconds();
 
 	} // namespace profidrive
