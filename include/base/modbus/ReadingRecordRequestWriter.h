a367d0e64f850b4855f168ec3a93036c834b1560
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-13 15:14:50 +0800

    "为ReadingRecordRequestWriter添加方法注释并完善功能，支持记录数量写入和CRC校验"

--- include/base/modbus/ReadingRecordRequestWriter.h
+++ include/base/modbus/ReadingRecordRequestWriter.h
@@ -19,19 +19,64 @@ namespace base
 				_adu_writer.WriteFunctionCode(0x03);
 			}
 
+			///
+			/// @brief 站号。
+			///
+			/// @return
+			///
 			uint8_t StationNumber() const
 			{
 				return _adu_writer.StationNumber();
 			}
 
+			///
+			/// @brief 写入站号。
+			///
+			/// @param value
+			///
 			void WriteStationNumber(uint8_t value) const
 			{
 				_adu_writer.WriteStationNumber(value);
 			}
 
+			///
+			/// @brief 写入要读取记录的起始地址。
+			///
+			/// @param value
+			///
 			void WriteStartAddress(uint16_t value)
 			{
 				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入要读取的记录数量。
+			///
+			/// @param value
+			///
+			void WriteRecordCount(uint16_t value)
+			{
+				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入 CRC 校验。
+			///
+			///
+			void WriteCrc()
+			{
+				_adu_writer.WriteCrc();
+			}
+
+			///
+			/// @brief 获取待发送的 span, 这里面包括了从站号到 CRC16 的所有数据，
+			/// 可以直接全部发送出去。
+			///
+			/// @return
+			///
+			base::ReadOnlySpan SpanForSending() const
+			{
+				return _adu_writer.SpanForSending();
 			}
 		};
 
