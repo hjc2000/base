1375b70d79bc2b5cba7a065cdc3d49dec402cf74
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-13 23:09:28 +0800

    "重构串口波特率计算逻辑，将frames_baud_count实现移至serial_handle模块并优化接口调用"

--- include/base/embedded/serial/Serial.h
+++ include/base/embedded/serial/Serial.h
@@ -167,63 +167,7 @@ namespace base
 			///
 			uint32_t FramesBaudCount(uint32_t frame_count) const
 			{
-				uint32_t baud_count = 0;
-
-				// 1 位起始位
-				baud_count += 1 * frame_count;
-
-				// data_bits 位数据位
-				baud_count += DataBits() * frame_count;
-
-				if (Parity() != base::serial::Parity::None)
-				{
-					// 1 位校验位
-					baud_count += 1 * frame_count;
-				}
-
-				// 停止位
-				switch (StopBits())
-				{
-				case base::serial::StopBits::ZeroPointFive:
-					{
-						// 每 2 个帧占用 1 个位。
-						baud_count += frame_count / 2;
-
-						// 还剩余半个位，当成 1 位。
-						if (frame_count % 2 > 0)
-						{
-							baud_count += 1;
-						}
-
-						break;
-					}
-				default:
-				case base::serial::StopBits::One:
-					{
-						baud_count += 1 * frame_count;
-						break;
-					}
-				case base::serial::StopBits::OnePointFive:
-					{
-						// 每 2 个帧占用 3 个位。
-						baud_count += (frame_count / 2) * 3;
-
-						// 还剩余 1.5 个位，当成 2 位。
-						if (frame_count % 2 > 0)
-						{
-							baud_count += 2;
-						}
-
-						break;
-					}
-				case base::serial::StopBits::Two:
-					{
-						baud_count += 2 * frame_count;
-						break;
-					}
-				}
-
-				return baud_count;
+				return base::serial::frames_baud_count(*_handle, frame_count);
 			}
 
 			/* #endregion */
