5d11651a6f86f64c641bb5857e3d5060b2dc0100
hjc2000 <hjc.666@foxmail.com>
2025-07-15 16:22:54 +0800

    "新增Modbus读位请求读者实现，支持起始地址和位数量读取功能"

---
+++ include/base/modbus/ReadingBitsRequestReader.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "base/bit/AutoBitConverter.h"
+#include "base/container/Range.h"
+#include "base/modbus/AduReader.h"
+#include "base/modbus/FunctionCode.h"
+#include "base/stream/ReadOnlySpan.h"
+#include "base/string/define.h"
+#include <cstdint>
+#include <stdexcept>
+
+namespace base
+{
+	namespace modbus
+	{
+		class ReadingBitsRequestReader
+		{
+		private:
+			base::modbus::AduReader _adu_reader;
+
+			void CheckFunctionCode() const
+			{
+				uint8_t function_code = _adu_reader.FunctionCode();
+				if (function_code == base::modbus::FunctionCode::ReadBits)
+				{
+					return;
+				}
+
+				throw std::runtime_error{CODE_POS_STR + "错误的功能码。"};
+			}
+
+		public:
+			ReadingBitsRequestReader(base::ReadOnlySpan const &span)
+				: _adu_reader(span)
+			{
+				CheckFunctionCode();
+			}
+
+			///
+			/// @brief 站号。
+			///
+			/// @return
+			///
+			uint8_t StationNumber() const
+			{
+				return _adu_reader.StationNumber();
+			}
+
+			///
+			/// @brief 要读取的位的起始地址。
+			///
+			/// @return
+			///
+			uint16_t StartAddress() const
+			{
+				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{0, 2}];
+				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+			}
+
+			///
+			/// @brief 要读取的位的数量。
+			///
+			/// @return
+			///
+			uint16_t BitCount() const
+			{
+				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{2, 4}];
+				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+			}
+		};
+
+	} // namespace modbus
+} // namespace base
