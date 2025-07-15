c9d4caa7433e0974c44143fa705948f131bb3c71
hjc2000 <hjc.666@foxmail.com>
2025-07-15 09:11:44 +0800

    "重构SoftWareTimeoutSerial实现，将方法实现从头文件迁移至源文件并优化头文件依赖"

--- include/base/embedded/serial/SoftWareTimeoutSerial.cpp
+++ include/base/embedded/serial/SoftWareTimeoutSerial.cpp
@@ -1,1 +1,95 @@
 #include "SoftWareTimeoutSerial.h" // IWYU pragma: keep
+#include "base/container/Range.h"
+#include "base/math/Fraction.h"
+#include "base/string/define.h"
+#include "base/task/delay.h"
+#include "base/task/task.h"
+#include "base/unit/Seconds.h"
+#include <stdexcept>
+
+void base::serial::SoftWareTimeoutSerial::Initialize(std::shared_ptr<base::serial::Serial> const &serial,
+													 int32_t receiving_buffer_size,
+													 int32_t timeout_frame_count)
+{
+	if (serial == nullptr)
+	{
+		throw std::invalid_argument{CODE_POS_STR + "不能传入空指针。"};
+	}
+
+	_serial = serial;
+	_receiving_stream = std::shared_ptr<base::BlockingCircleBufferMemoryStream>{new base::BlockingCircleBufferMemoryStream{receiving_buffer_size}};
+
+	{
+		uint32_t baud_rate = _serial->BaudRate();
+		uint32_t frames_baud_count = _serial->FramesBaudCount(timeout_frame_count);
+		base::Seconds timeout_seconds{base::Fraction{frames_baud_count, baud_rate}};
+		_receiving_timeout = static_cast<std::chrono::nanoseconds>(timeout_seconds);
+	}
+}
+
+void base::serial::SoftWareTimeoutSerial::ReceivingThreadFunc()
+{
+	uint8_t buffer[128];
+	base::Span span{buffer, sizeof(buffer)};
+
+	while (true)
+	{
+		if (_closed)
+		{
+			return;
+		}
+
+		int32_t have_read = _serial->Read(span);
+		_receiving_stream->Write(base::ReadOnlySpan{buffer, have_read});
+	}
+}
+
+base::serial::SoftWareTimeoutSerial::SoftWareTimeoutSerial(std::shared_ptr<base::serial::Serial> const &serial,
+														   int32_t receiving_buffer_size,
+														   int32_t timeout_frame_count)
+{
+	Initialize(serial, receiving_buffer_size, timeout_frame_count);
+
+	_receiving_thread_exit = base::task::run(
+		[this]()
+		{
+			ReceivingThreadFunc();
+		});
+}
+
+base::serial::SoftWareTimeoutSerial::SoftWareTimeoutSerial(std::shared_ptr<base::serial::Serial> const &serial,
+														   int32_t receiving_buffer_size,
+														   int32_t timeout_frame_count,
+														   size_t receiving_thread_stack_size)
+{
+	Initialize(serial, receiving_buffer_size, timeout_frame_count);
+
+	_receiving_thread_exit = base::task::run(receiving_thread_stack_size,
+											 [this]()
+											 {
+												 ReceivingThreadFunc();
+											 });
+}
+
+int32_t base::serial::SoftWareTimeoutSerial::Read(base::Span const &span)
+{
+	int32_t have_read = 0;
+
+	while (true)
+	{
+		if (have_read >= span.Size())
+		{
+			break;
+		}
+
+		have_read += _receiving_stream->Read(span[base::Range{have_read, span.Size()}]);
+		base::task::Delay(_receiving_timeout);
+		if (_receiving_stream->Length() == 0)
+		{
+			// 等待超时时间后没有新的数据到来，断帧。
+			break;
+		}
+	}
+
+	return have_read;
+}
--- include/base/embedded/serial/SoftWareTimeoutSerial.h
+++ include/base/embedded/serial/SoftWareTimeoutSerial.h
@@ -1,22 +1,15 @@
 #pragma once
-#include "base/container/Range.h"
 #include "base/embedded/serial/Serial.h"
 #include "base/exception/NotSupportedException.h"
-#include "base/math/Fraction.h"
 #include "base/stream/BlockingCircleBufferMemoryStream.h"
 #include "base/stream/ReadOnlySpan.h"
 #include "base/stream/Span.h"
 #include "base/stream/Stream.h"
-#include "base/string/define.h"
-#include "base/task/delay.h"
 #include "base/task/ITask.h"
-#include "base/task/task.h"
-#include "base/unit/Seconds.h"
 #include <chrono>
 #include <cstddef>
 #include <cstdint>
 #include <memory>
-#include <stdexcept>
 
 namespace base
 {
@@ -38,40 +31,9 @@ namespace base
 
 			void Initialize(std::shared_ptr<base::serial::Serial> const &serial,
 							int32_t receiving_buffer_size,
-							int32_t timeout_frame_count)
-			{
-				if (serial == nullptr)
-				{
-					throw std::invalid_argument{CODE_POS_STR + "不能传入空指针。"};
-				}
+							int32_t timeout_frame_count);
 
-				_serial = serial;
-				_receiving_stream = std::shared_ptr<base::BlockingCircleBufferMemoryStream>{new base::BlockingCircleBufferMemoryStream{receiving_buffer_size}};
-
-				{
-					uint32_t baud_rate = _serial->BaudRate();
-					uint32_t frames_baud_count = _serial->FramesBaudCount(timeout_frame_count);
-					base::Seconds timeout_seconds{base::Fraction{frames_baud_count, baud_rate}};
-					_receiving_timeout = static_cast<std::chrono::nanoseconds>(timeout_seconds);
-				}
-			}
-
-			void ReceivingThreadFunc()
-			{
-				uint8_t buffer[128];
-				base::Span span{buffer, sizeof(buffer)};
-
-				while (true)
-				{
-					if (_closed)
-					{
-						return;
-					}
-
-					int32_t have_read = _serial->Read(span);
-					_receiving_stream->Write(base::ReadOnlySpan{buffer, have_read});
-				}
-			}
+			void ReceivingThreadFunc();
 
 		public:
 			///
@@ -83,16 +45,7 @@ namespace base
 			///
 			SoftWareTimeoutSerial(std::shared_ptr<base::serial::Serial> const &serial,
 								  int32_t receiving_buffer_size,
-								  int32_t timeout_frame_count)
-			{
-				Initialize(serial, receiving_buffer_size, timeout_frame_count);
-
-				_receiving_thread_exit = base::task::run(
-					[this]()
-					{
-						ReceivingThreadFunc();
-					});
-			}
+								  int32_t timeout_frame_count);
 
 			///
 			/// @brief
@@ -105,16 +58,7 @@ namespace base
 			SoftWareTimeoutSerial(std::shared_ptr<base::serial::Serial> const &serial,
 								  int32_t receiving_buffer_size,
 								  int32_t timeout_frame_count,
-								  size_t receiving_thread_stack_size)
-			{
-				Initialize(serial, receiving_buffer_size, timeout_frame_count);
-
-				_receiving_thread_exit = base::task::run(receiving_thread_stack_size,
-														 [this]()
-														 {
-															 ReceivingThreadFunc();
-														 });
-			}
+								  size_t receiving_thread_stack_size);
 
 			~SoftWareTimeoutSerial()
 			{
@@ -239,28 +183,7 @@ namespace base
 			/// @param span
 			/// @return int32_t
 			///
-			virtual int32_t Read(base::Span const &span) override
-			{
-				int32_t have_read = 0;
-
-				while (true)
-				{
-					if (have_read >= span.Size())
-					{
-						break;
-					}
-
-					have_read += _receiving_stream->Read(span[base::Range{have_read, span.Size()}]);
-					base::task::Delay(_receiving_timeout);
-					if (_receiving_stream->Length() == 0)
-					{
-						// 等待超时时间后没有新的数据到来，断帧。
-						break;
-					}
-				}
-
-				return have_read;
-			}
+			virtual int32_t Read(base::Span const &span) override;
 
 			///
 			/// @brief 将 span 中的数据写入本流。
