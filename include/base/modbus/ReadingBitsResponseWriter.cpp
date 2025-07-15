0f46c75ae64a376cfca8cc69cc0d290c4f6d68b5
hjc2000 <hjc.666@foxmail.com>
2025-07-15 16:32:14 +0800

    新增Modbus读位响应作者实现，支持站号、功能码、数据及CRC写入功能

---
+++ include/base/modbus/ReadingBitsResponseWriter.cpp
@@ -0,0 +1,1 @@
+#include "ReadingBitsResponseWriter.h" // IWYU pragma: keep
---
+++ include/base/modbus/ReadingBitsResponseWriter.h
@@ -0,0 +1,94 @@
+#pragma once
+#include "base/modbus/AduWriter.h"
+#include "base/modbus/FunctionCode.h"
+#include "base/stream/Span.h"
+#include <cstdint>
+
+namespace base
+{
+	namespace modbus
+	{
+		class ReadingBitsResponseWriter
+		{
+		private:
+			base::modbus::AduWriter _adu_writer;
+
+		public:
+			ReadingBitsResponseWriter(base::Span const &span)
+				: _adu_writer(span)
+			{
+			}
+
+			///
+			/// @brief 写入站号。
+			///
+			/// @param value
+			///
+			void WriteStationNumber(uint8_t value) const
+			{
+				_adu_writer.WriteStationNumber(value);
+			}
+
+			///
+			/// @brief 写入功能码。
+			///
+			///
+			void WriteFunctionCode() const
+			{
+				_adu_writer.WriteFunctionCode(static_cast<uint8_t>(base::modbus::FunctionCode::ReadBits));
+			}
+
+			///
+			/// @brief 写入数据字节数。
+			///
+			/// @param value
+			///
+			void WriteByteCount(uint8_t value)
+			{
+				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入数据。
+			///
+			/// @param span
+			///
+			void WriteData(base::ReadOnlySpan const &span)
+			{
+				_adu_writer.WriteData(span);
+			}
+
+			///
+			/// @brief 写入数据。
+			///
+			/// @param value
+			///
+			template <typename ValueType>
+			void WriteData(ValueType value)
+			{
+				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入 CRC16.
+			///
+			///
+			void WriteCrc()
+			{
+				_adu_writer.WriteCrc();
+			}
+
+			///
+			/// @brief 获取待发送的 span, 这里面包括了从站号到 CRC16 的所有数据，
+			/// 可以直接全部发送出去。
+			///
+			/// @return
+			///
+			base::ReadOnlySpan SpanForSending() const
+			{
+				return _adu_writer.SpanForSending();
+			}
+		};
+
+	} // namespace modbus
+} // namespace base
