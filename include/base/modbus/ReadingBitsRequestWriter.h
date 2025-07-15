4b3e1770571505e505f70b61ca82ef9a83aef94b
hjc2000 <hjc.666@foxmail.com>
2025-07-15 16:06:49 +0800

    "新增Modbus读位请求作者实现，支持起始地址、位数量及CRC写入功能"

---
+++ include/base/modbus/ReadingBitsRequestWriter.h
@@ -0,0 +1,82 @@
+#pragma once
+#include "base/modbus/AduWriter.h"
+#include "FunctionCode.h"
+#include <cstdint>
+
+namespace base
+{
+	namespace modbus
+	{
+		class ReadingBitsRequestWriter
+		{
+		private:
+			base::modbus::AduWriter _adu_writer;
+
+		public:
+			ReadingBitsRequestWriter(base::Span const &span)
+				: _adu_writer(span)
+			{
+			}
+
+			///
+			/// @brief 写入站号。
+			///
+			/// @param value
+			///
+			void WriteStationNumber(uint8_t value) const
+			{
+				_adu_writer.WriteStationNumber(value);
+			}
+
+			///
+			/// @brief 写入功能码。
+			///
+			///
+			void WriteFunctionCode() const
+			{
+				_adu_writer.WriteFunctionCode(static_cast<uint8_t>(base::modbus::FunctionCode::ReadBits));
+			}
+
+			///
+			/// @brief 写入要读取的位的起始地址。
+			///
+			/// @param value
+			///
+			void WriteStartAddress(uint16_t value)
+			{
+				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入要读取的位的数量。
+			///
+			/// @param value
+			///
+			void WriteBitCount(uint16_t value)
+			{
+				_adu_writer.WriteData(value);
+			}
+
+			///
+			/// @brief 写入 CRC.
+			///
+			///
+			void WriteCrc()
+			{
+				_adu_writer.WriteCrc();
+			}
+
+			///
+			/// @brief 获取待发送的 span, 这里面包括了从站号到 CRC16 的所有数据，
+			/// 可以直接全部发送出去。
+			///
+			/// @return
+			///
+			base::ReadOnlySpan SpanForSending() const
+			{
+				return _adu_writer.SpanForSending();
+			}
+		};
+
+	} // namespace modbus
+} // namespace base
