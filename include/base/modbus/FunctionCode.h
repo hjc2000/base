fe78d3e5194623fbeee3d76f3c60743cfa1df380
hjc2000 <hjc.666@foxmail.com>
2025-07-16 10:38:29 +0800

    "优化FunctionCode.h文档，补充功能码枚举命名空间的设计原理和实现细节说明"

--- include/base/modbus/FunctionCode.h
+++ include/base/modbus/FunctionCode.h
@@ -34,6 +34,15 @@ namespace base
 			}
 		};
 
+		///
+		/// @brief 功能码枚举。
+		///
+		/// @note 放到独立的命名空间中进行枚举，方便用户和厂商进行扩展。如果放到类中就无法扩展了。
+		///
+		/// @note 使用 constexpr 函数而非 constexpr 变量是方便厂商和用户扩展时在里面进行一些计算。
+		/// 此外，constexpr 函数在代码高亮中显示的颜色与命名空间的白色文字不同，方便区分。如果使用
+		/// constexpr 变量，显示的也是白色，不利于区分。
+		///
 		namespace function_code
 		{
 			///
