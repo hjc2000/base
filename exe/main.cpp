9dd1bd436a0a28fcd70409ab8ad0d4d2bf848aed
hjc2000 <hjc.666@foxmail.com>
2025-07-21 09:31:57 +0800

    "移除main.cpp中未使用的Fraction相关代码及冗余的头文件引用"

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,44 +1,18 @@
-#include "base/math/Fraction.h"
 #include "base/math/InertialElement.h"
 #include "base/math/Int64Fraction.h"
 #include "base/modbus/ModbusCrc16.h"
 #include "base/stream/memcmp.h"
 #include "base/stream/ReadOnlySpan.h"
 #include "base/string/ToHexString.h"
-#include "base/wrapper/number-wrapper.h"
 #include <array>
 #include <cmath>
 #include <cstdint>
 #include <cstdlib>
-#include <iomanip>
 #include <iostream>
-#include <numbers>
 #include <stdlib.h>
 
 int main()
 {
-	{
-		base::Fraction f{base::Double{std::numbers::pi}};
-		constexpr int precision = 512;
-
-		std::cout << "分数: " << f << std::endl;
-
-		std::cout << "std::numbers::pi: \t\t"
-				  << std::setprecision(precision)
-				  << std::numbers::pi
-				  << std::endl;
-
-		std::cout << "分数表示的 pi 转为 double: \t"
-				  << std::setprecision(precision)
-				  << static_cast<double>(f)
-				  << std::endl;
-
-		std::cout << "误差: "
-				  << std::setprecision(precision)
-				  << static_cast<double>(f) - std::numbers::pi
-				  << std::endl;
-	}
-
 	{
 		base::InertialElement<base::Int64Fraction> inertial_element{
 			base::Int64Fraction{1, static_cast<int64_t>(1e6)},
