5959847f4dbb901589c4779a6fcc979f6d4bc909
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-27 00:13:51 +0800

    将测试程序名称从"test"重命名为"base-test"并添加target_total_install安装目标

--- project.cmake
+++ project.cmake
@@ -16,8 +16,9 @@ endif()
 # 添加测试程序
 if(("${platform}" STREQUAL "msys") OR
    ("${platform}" STREQUAL "msys-clang"))
-	set(exe_name "test")
+	set(exe_name "base-test")
 	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
 	target_link_libraries(${exe_name} ${ProjectName})
 	target_install(${exe_name})
+	target_total_install(${exe_name})
 endif()
