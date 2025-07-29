918bbbcd842a7f319166efe23164e9eea5422a0a
hjc2000 <hjc.666@foxmail.com>
2025-07-29 14:51:41 +0800

    const

--- include/base/container/IFilter.h
+++ include/base/container/IFilter.h
@@ -15,7 +15,7 @@ namespace base
 		///
 		/// @return 能通过过滤器则返回 true, 否则返回 false.
 		///
-		virtual bool Pass(T const &value) = 0;
+		virtual bool Pass(T const &value) const = 0;
 	};
 
 } // namespace base
