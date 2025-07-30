0f7987af5fccfd25c067232503410941f0982aeb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-31 00:17:32 +0800

    为Int64Fraction和Fraction类添加调试输出和pi精度测试功能

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,5 +1,6 @@
 #include "base/math/InertialElement.h"
 #include "base/math/Int64Fraction.h"
+#include "base/string/define.h"
 #include "base/wrapper/number-wrapper.h"
 #include <cstdint>
 #include <iostream>
@@ -7,6 +8,8 @@
 int main()
 {
 	{
+		std::cout << CODE_POS_STR;
+
 		base::InertialElement<base::Int64Fraction> inertial_element{
 			base::Int64Fraction{1, static_cast<int64_t>(1e6)},
 			base::Int64Fraction{1, static_cast<int64_t>(1e9)},
@@ -25,8 +28,34 @@ int main()
 	}
 
 	{
+		std::cout << CODE_POS_STR;
+
+		base::Fraction f{base::Double{std::numbers::pi}};
+		constexpr int precision = 512;
+
+		std::cout << "分数: " << f << std::endl;
+
+		std::cout << "std::numbers::pi: \t\t"
+				  << std::setprecision(precision)
+				  << std::numbers::pi
+				  << std::endl;
+
+		std::cout << "分数表示的 pi 转为 double: \t"
+				  << std::setprecision(precision)
+				  << static_cast<double>(f)
+				  << std::endl;
+
+		std::cout << "误差: "
+				  << std::setprecision(precision)
+				  << static_cast<double>(f) - std::numbers::pi
+				  << std::endl;
+	}
+
+	{
+		std::cout << CODE_POS_STR;
+
 		float f_pi = static_cast<float>(std::numbers::pi);
-		base::Fraction f{base::Float{f_pi}};
+		base::Int64Fraction f{base::Float{f_pi}};
 		constexpr int precision = 512;
 
 		std::cout << "分数: " << f << std::endl;
