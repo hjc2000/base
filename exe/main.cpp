08d5ebdf7d27400f552d91d70fcb83f34d64030e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-07-31 00:19:49 +0800

    为Int64Fraction类添加pi精度测试功能并优化调试输出格式

--- exe/main.cpp
+++ exe/main.cpp
@@ -8,7 +8,8 @@
 int main()
 {
 	{
-		std::cout << CODE_POS_STR;
+		std::cout << std::endl
+				  << CODE_POS_STR;
 
 		base::InertialElement<base::Int64Fraction> inertial_element{
 			base::Int64Fraction{1, static_cast<int64_t>(1e6)},
@@ -28,7 +29,8 @@ int main()
 	}
 
 	{
-		std::cout << CODE_POS_STR;
+		std::cout << std::endl
+				  << CODE_POS_STR;
 
 		base::Fraction f{base::Double{std::numbers::pi}};
 		constexpr int precision = 512;
@@ -52,7 +54,33 @@ int main()
 	}
 
 	{
-		std::cout << CODE_POS_STR;
+		std::cout << std::endl
+				  << CODE_POS_STR;
+
+		base::Int64Fraction f{base::Double{std::numbers::pi}};
+		constexpr int precision = 512;
+
+		std::cout << "分数: " << f << std::endl;
+
+		std::cout << "std::numbers::pi: \t\t"
+				  << std::setprecision(precision)
+				  << std::numbers::pi
+				  << std::endl;
+
+		std::cout << "分数表示的 pi 转为 double: \t"
+				  << std::setprecision(precision)
+				  << static_cast<double>(f)
+				  << std::endl;
+
+		std::cout << "误差: "
+				  << std::setprecision(precision)
+				  << static_cast<double>(f) - std::numbers::pi
+				  << std::endl;
+	}
+
+	{
+		std::cout << std::endl
+				  << CODE_POS_STR;
 
 		float f_pi = static_cast<float>(std::numbers::pi);
 		base::Int64Fraction f{base::Float{f_pi}};
