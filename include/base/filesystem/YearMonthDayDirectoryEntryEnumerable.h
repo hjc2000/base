4c4010449addb21dbd4d02dcc06cd5fc6e675bde
hjc2000 <hjc.666@foxmail.com>
2025-08-01 17:13:22 +0800

    实现YearMonthDayDirectoryEntryEnumerable迭代器的年份/月份/日期层级遍历逻辑

--- include/base/filesystem/YearMonthDayDirectoryEntryEnumerable.h
+++ include/base/filesystem/YearMonthDayDirectoryEntryEnumerable.h
@@ -24,21 +24,87 @@ namespace base
 			{
 			private:
 				YearMonthDayDirectoryEntryEnumerable &_enumerable;
+
+				///
+				/// @brief 用来迭代基路径的迭代器。
+				///
+				///
+				std::shared_ptr<base::IEnumerator<base::DirectoryEntry const>> _base_dir_iterator;
 				std::shared_ptr<base::IEnumerator<base::DirectoryEntry const>> _year_dir_iterator;
 				std::shared_ptr<base::IEnumerator<base::DirectoryEntry const>> _month_dir_iterator;
 				std::shared_ptr<base::IEnumerator<base::DirectoryEntry const>> _day_dir_iterator;
 				std::shared_ptr<base::IEnumerator<base::DirectoryEntry const>> _file_iterator;
 
+				///
+				/// @brief 移动到下一年。
+				///
+				/// @warning 只能被 MoveToNextMonth 调用。
+				///
+				///
 				void MoveToNextYear()
 				{
+					if (_base_dir_iterator == nullptr)
+					{
+						base::filesystem::DirectoryEntryEnumerable enumerable{_enumerable._base_path};
+						_base_dir_iterator = enumerable.GetEnumerator();
+						return;
+					}
+
+					if (_base_dir_iterator->IsEnd())
+					{
+						// 没有下一个年份了
+						return;
+					}
+
+					_base_dir_iterator->Add();
+				}
+
+				void MoveToNextMonth()
+				{
+					if (_year_dir_iterator == nullptr || _year_dir_iterator->IsEnd())
+					{
+						// _year_dir_iterator 为空，还没有迭代过年份，或者 _year_dir_iterator 已经迭代
+						// 完了，需要切换到下一个年份目录然后重新构造 _year_dir_iterator.
+						MoveToNextYear();
+						if (_base_dir_iterator->IsEnd())
+						{
+							return;
+						}
+
+						base::DirectoryEntry entry = _base_dir_iterator->CurrentValue();
+						base::Path year_dir_path = entry.Path();
+						base::filesystem::DirectoryEntryEnumerable enumerable{year_dir_path};
+						_year_dir_iterator = enumerable.GetEnumerator();
+						return;
+					}
+
+					_year_dir_iterator->Add();
+				}
+
+				void MoveToNextDay()
+				{
+					if (_month_dir_iterator == nullptr || _month_dir_iterator->IsEnd())
+					{
+						MoveToNextMonth();
+						if (_year_dir_iterator->IsEnd())
+						{
+							return;
+						}
+
+						base::DirectoryEntry entry = _year_dir_iterator->CurrentValue();
+						base::Path month_dir_path = entry.Path();
+						base::filesystem::DirectoryEntryEnumerable enumerable{month_dir_path};
+						_month_dir_iterator = enumerable.GetEnumerator();
+						return;
+					}
+
+					_month_dir_iterator->Add();
 				}
 
 			public:
 				Enumerator(YearMonthDayDirectoryEntryEnumerable &enumerable)
 					: _enumerable(enumerable)
 				{
-					base::filesystem::DirectoryEntryEnumerable dir_enumerable{_enumerable._base_path};
-					_year_dir_iterator = dir_enumerable.GetEnumerator();
 				}
 
 				///
