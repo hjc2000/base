bbdaee04e8a3b631dcf2678523159bf181498697
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-01 22:04:47 +0800

    添加LeftOpenRightClosedInterval类并修正OpenInterval类的边界检查条件

--- include/base/math/Interval.h
+++ include/base/math/Interval.h
@@ -159,9 +159,9 @@ namespace base
 		///
 		constexpr OpenInterval(T const &left, T const &right)
 		{
-			if (right <= left)
+			if (right < left)
 			{
-				throw std::invalid_argument{CODE_POS_STR + "区间右端点不能小于等于左端点。"};
+				throw std::invalid_argument{CODE_POS_STR + "区间右端点不能小于左端点。"};
 			}
 
 			_left = left;
@@ -197,6 +197,84 @@ namespace base
 		constexpr bool IsInRange(T const &value) const
 		{
 			return value > _left && value < _right;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间外。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsOutOfRange(T const &value) const
+		{
+			return !IsInRange(value);
+		}
+	};
+
+	///
+	/// @brief 左开右区间。(Left, Right].
+	///
+	template <typename T>
+	class LeftOpenRightClosedInterval
+	{
+	private:
+		T _left{};
+		T _right{};
+
+	public:
+		///
+		/// @brief 构造函数。
+		///
+		/// @note 构造出来的区间左右端点是 T 类型的默认值。
+		///
+		constexpr LeftOpenRightClosedInterval() = default;
+
+		///
+		/// @brief 构造函数。
+		///
+		/// @param left 区间左端点。
+		/// @param right 区间右端点。
+		///
+		constexpr LeftOpenRightClosedInterval(T const &left, T const &right)
+		{
+			if (right < left)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "区间右端点不能小于左端点。"};
+			}
+
+			_left = left;
+			_right = right;
+		}
+
+		///
+		/// @brief 区间左端点。
+		///
+		/// @return T
+		///
+		constexpr T Left() const
+		{
+			return _left;
+		}
+
+		///
+		/// @brief 区间右端点。
+		///
+		/// @return T
+		///
+		constexpr T Right() const
+		{
+			return _right;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间内。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsInRange(T const &value) const
+		{
+			return value > _left && value <= _right;
 		}
 
 		///
