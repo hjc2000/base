6b1f19bd101a233897685da39df38356375e03c1
hjc2000 <hjc.666@foxmail.com>
2025-08-01 15:21:12 +0800

    重构测试代码，将InertialElement测试逻辑迁移至TestInertialElement模块

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,33 +1,9 @@
-#include "base/math/InertialElement.h"
-#include "base/math/Int64Fraction.h"
-#include "base/string/define.h"
 #include "base/test/TestFraction.h"
-#include <cstdint>
-#include <iostream>
+#include "base/test/TestInertialElement.h"
 
 int main()
 {
-	{
-		std::cout << std::endl
-				  << CODE_POS_STR;
-
-		base::InertialElement<base::Int64Fraction> inertial_element{
-			base::Int64Fraction{1, static_cast<int64_t>(1e6)},
-			base::Int64Fraction{1, static_cast<int64_t>(1e9)},
-			base::Int64Fraction{1, static_cast<int64_t>(1) << 32},
-		};
-
-		std::cout << "Kx: " << inertial_element.Kx() << std::endl;
-		std::cout << "Ky: " << inertial_element.Ky() << std::endl;
-
-		for (int i = 0; i < 10000; i++)
-		{
-			inertial_element.Input(100);
-		}
-
-		std::cout << base::floor(inertial_element.CurrentOutput()) << std::endl;
-	}
-
+	base::test::TestInertialElement();
 	base::test::TestFraction();
 	base::test::TestInt64Fraction();
 }
---
+++ include/base/test/TestInertialElement.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace base
+{
+	namespace test
+	{
+		void TestInertialElement();
+
+	} // namespace test
+} // namespace base
