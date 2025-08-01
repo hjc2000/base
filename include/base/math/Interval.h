d010aaf387aad4315d52b63d90badcff69e0ecdb
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-01 22:01:41 +0800

    添加IntervalType枚举和OpenInterval类，支持多种区间类型定义和操作

--- include/base/math/Interval.h
+++ include/base/math/Interval.h
@@ -4,6 +4,57 @@
 
 namespace base
 {
+	enum class IntervalType
+	{
+		///
+		/// @brief 闭区间。
+		///
+		///
+		Closed,
+
+		///
+		/// @brief 开区间。
+		///
+		///
+		Open,
+
+		///
+		/// @brief 左开右闭区间。
+		///
+		///
+		LeftOpenRightClosed,
+
+		///
+		/// @brief 左闭右开区间。
+		///
+		///
+		LeftClosedRightOpen,
+
+		///
+		/// @brief 左边负无穷，右边开的区间。
+		///
+		///
+		LeftInfiniteRightOpen,
+
+		///
+		/// @brief 左边负无穷，右边闭的区间。
+		///
+		///
+		LeftInfiniteRightClosed,
+
+		///
+		/// @brief 左边开，右边正无穷的区间。
+		///
+		///
+		LeftOpenRightInfinite,
+
+		///
+		/// @brief 左边闭，右边正无穷的区间。
+		///
+		///
+		LeftClosedRightInfinite
+	};
+
 	///
 	/// @brief 闭区间。[Left, Right].
 	///
@@ -30,7 +81,7 @@ namespace base
 		///
 		constexpr ClosedInterval(T const &left, T const &right)
 		{
-			if (left > right)
+			if (right < left)
 			{
 				throw std::invalid_argument{CODE_POS_STR + "区间右端点不能小于左端点。"};
 			}
@@ -80,6 +131,90 @@ namespace base
 		{
 			return !IsInRange(value);
 		}
+	};
+
+	///
+	/// @brief 开区间。(Left, Right).
+	///
+	template <typename T>
+	class OpenInterval
+	{
+	private:
+		T _left{};
+		T _right{};
+
+	public:
+		///
+		/// @brief 构造函数。
+		///
+		/// @note 构造出来的区间左右端点是 T 类型的默认值。
+		///
+		constexpr OpenInterval() = default;
+
+		///
+		/// @brief 构造函数。
+		///
+		/// @param left 区间左端点。
+		/// @param right 区间右端点。
+		///
+		constexpr OpenInterval(T const &left, T const &right)
+		{
+			if (right <= left)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "区间右端点不能小于等于左端点。"};
+			}
+
+			_left = left;
+			_right = right;
+		}
+
+		///
+		/// @brief 区间左端点。
+		///
+		/// @return T
+		///
+		constexpr T Left() const
+		{
+			return _left;
+		}
+
+		///
+		/// @brief 区间右端点。
+		///
+		/// @return T
+		///
+		constexpr T Right() const
+		{
+			return _right;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间内。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsInRange(T const &value) const
+		{
+			return value > _left && value < _right;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间外。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsOutOfRange(T const &value) const
+		{
+			return !IsInRange(value);
+		}
+	};
+
+	class Interval
+	{
+	public:
+		constexpr Interval() = default;
 	};
 
 } // namespace base
