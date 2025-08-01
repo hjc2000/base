29ade01621260cf2ff0b0485499b13bfc66982dd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-01 22:16:08 +0800

    添加LeftOpenRightInfiniteInterval和LeftClosedRightInfiniteInterval类，支持左开右无限和左闭右无限区间类型

--- include/base/math/Interval.h
+++ include/base/math/Interval.h
@@ -487,6 +487,126 @@ namespace base
 		}
 	};
 
+	///
+	/// @brief 左边开，右边正无穷的区间。 (Left, +inf).
+	///
+	template <typename T>
+	class LeftOpenRightInfiniteInterval
+	{
+	private:
+		T _left{};
+
+	public:
+		///
+		/// @brief 构造函数。
+		///
+		/// @note 构造出来的区间左右端点是 T 类型的默认值。
+		///
+		constexpr LeftOpenRightInfiniteInterval() = default;
+
+		///
+		/// @brief 构造函数。
+		///
+		/// @param left 区间右端点。
+		///
+		constexpr LeftOpenRightInfiniteInterval(T const &left)
+		{
+			_left = left;
+		}
+
+		///
+		/// @brief 区间左端点。
+		///
+		/// @return T
+		///
+		constexpr T Left() const
+		{
+			return _left;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间内。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsInRange(T const &value) const
+		{
+			return value > _left;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间外。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsOutOfRange(T const &value) const
+		{
+			return !IsInRange(value);
+		}
+	};
+
+	///
+	/// @brief 左边开，右边正无穷的区间。 (Left, +inf).
+	///
+	template <typename T>
+	class LeftClosedRightInfiniteInterval
+	{
+	private:
+		T _left{};
+
+	public:
+		///
+		/// @brief 构造函数。
+		///
+		/// @note 构造出来的区间左右端点是 T 类型的默认值。
+		///
+		constexpr LeftClosedRightInfiniteInterval() = default;
+
+		///
+		/// @brief 构造函数。
+		///
+		/// @param left 区间右端点。
+		///
+		constexpr LeftClosedRightInfiniteInterval(T const &left)
+		{
+			_left = left;
+		}
+
+		///
+		/// @brief 区间左端点。
+		///
+		/// @return T
+		///
+		constexpr T Left() const
+		{
+			return _left;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间内。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsInRange(T const &value) const
+		{
+			return value >= _left;
+		}
+
+		///
+		/// @brief 检查一个值是否在区间外。
+		///
+		/// @param value
+		/// @return
+		///
+		constexpr bool IsOutOfRange(T const &value) const
+		{
+			return !IsInRange(value);
+		}
+	};
+
 	class Interval
 	{
 	public:
