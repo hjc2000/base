949f2a454479f29c37dc17f6189880f7ce1c3ab4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-03 01:10:16 +0800

    清理main.cpp中已注释的测试代码，仅保留YearMonthDayDirectoryEntryEnumerable测试

--- exe/main.cpp
+++ exe/main.cpp
@@ -1,11 +1,6 @@
-#include "base/test/TestFraction.h"
-#include "base/test/TestInertialElement.h"
 #include "base/test/TestYearMonthDayDirectoryEntryEnumerable.h"
 
 int main()
 {
-	// base::test::TestInertialElement();
-	// base::test::TestFraction();
-	// base::test::TestInt64Fraction();
 	base::test::TestYearMonthDayDirectoryEntryEnumerable();
 }
