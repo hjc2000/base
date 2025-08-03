d79bc38f5454e57f80e67f6b5d510fc0250c29e6
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-03 12:48:30 +0800

    添加GetYearMonthDateTimeInterval函数，支持多种区间类型的整月日期时间转换

--- include/base/time/DateTime.h
+++ include/base/time/DateTime.h
@@ -1065,4 +1065,215 @@ namespace base
 		}
 	}
 
+	///
+	/// @brief 把日期时间区间的分辨率调整到整月。
+	///
+	/// @param value
+	/// @return
+	///
+	constexpr base::Interval<base::DateTime> GetYearMonthDateTimeInterval(base::Interval<base::DateTime> const &value)
+	{
+		switch (value.Type())
+		{
+		case IntervalType::Closed:
+			{
+				base::ClosedInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+				base::DateTime right = interval.Right();
+
+				base::ClosedInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::Open:
+			{
+				base::OpenInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+				base::DateTime right = interval.Right();
+
+				base::OpenInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftOpenRightClosed:
+			{
+				base::LeftOpenRightClosedInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+				base::DateTime right = interval.Right();
+
+				base::LeftOpenRightClosedInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftClosedRightOpen:
+			{
+				base::LeftClosedRightOpenInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+				base::DateTime right = interval.Right();
+
+				base::LeftClosedRightOpenInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftInfiniteRightOpen:
+			{
+				base::LeftInfiniteRightOpenInterval<base::DateTime> interval{value};
+				base::DateTime right = interval.Right();
+
+				base::LeftInfiniteRightOpenInterval<base::DateTime> ret{
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftInfiniteRightClosed:
+			{
+				base::LeftInfiniteRightClosedInterval<base::DateTime> interval{value};
+				base::DateTime right = interval.Right();
+
+				base::LeftInfiniteRightClosedInterval<base::DateTime> ret{
+					base::DateTime{
+						right.Year(),
+						right.Month(),
+						right.CurrentMonthDayCount(),
+						23,
+						59,
+						59,
+						static_cast<int64_t>(1e9) - 1,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftOpenRightInfinite:
+			{
+				base::LeftOpenRightInfiniteInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+
+				base::LeftOpenRightInfiniteInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+				};
+
+				return ret;
+			}
+		case IntervalType::LeftClosedRightInfinite:
+			{
+				base::LeftClosedRightInfiniteInterval<base::DateTime> interval{value};
+				base::DateTime left = interval.Left();
+
+				base::LeftClosedRightInfiniteInterval<base::DateTime> ret{
+					base::DateTime{
+						left.UtcHourOffset(),
+						left.Year(),
+						left.Month(),
+						1,
+						0,
+						0,
+						0,
+						0,
+					},
+				};
+
+				return ret;
+			}
+		}
+	}
+
 } // namespace base
