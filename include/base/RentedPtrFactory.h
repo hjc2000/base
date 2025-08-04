30f012f2931d8d48c0976e39b4a22e571cb3759f
hjc2000 <hjc.666@foxmail.com>
2025-08-04 10:58:43 +0800

    修复RentedPtrFactory中的static_function拼写错误，并添加详细注释说明

--- include/base/RentedPtrFactory.h
+++ include/base/RentedPtrFactory.h
@@ -1,26 +1,33 @@
 #pragma once
-#include "base/define.h"
 #include <memory>
 
 namespace base
 {
+	///
 	/// @brief 借用指针工厂
+	///
 	class RentedPtrFactory final
 	{
 	private:
 		RentedPtrFactory() = delete;
 
 	public:
+		///
 		/// @brief 创建一个借用指针。
+		///
 		/// @note 借用指针实际上是一格共享指针，这个共享指针的删除器什么也不做，不会释放内存。
+		///
 		/// @note 这么做是为了让一些例如静态变量或手动管理生命周期的变量能够被 shared_ptr
 		/// 套壳，从而兼容那些需要 shared_ptr 的 API。
 		///
 		/// @tparam T
+		///
 		/// @param o
+		///
 		/// @return
+		///
 		template <typename T>
-		static_function std::shared_ptr<T> Create(T *o)
+		static std::shared_ptr<T> Create(T *o)
 		{
 			std::shared_ptr<T> ret{
 				o,
@@ -32,4 +39,5 @@ namespace base
 			return ret;
 		}
 	};
+
 } // namespace base
