862a3a008b70519a637954e44b755840ad876938
hjc2000 <hjc.666@foxmail.com>
2025-08-04 09:55:27 +0800

    将CancellationToken的Cancel、Register和Unregister方法实现从cpp文件移至头文件

--- include/base/task/CancellationToken.h
+++ include/base/task/CancellationToken.h
@@ -64,7 +64,35 @@ namespace base
 		inline static uint64_t _id = 0;
 		static std::shared_ptr<CancellationToken> _none_cancellation_token;
 
-		void Cancel();
+		void Cancel()
+		{
+			// 只有取消过一次，即调用本函数一次后，_is_cancellation_request
+			// 才会为 true.
+			//
+			// _is_cancellation_request 为 true 表示已经取消过一次了，
+			// 这时候就不要重复取消了。
+			if (_is_cancellation_request)
+			{
+				// 先检查一次，有机会不竞争锁直接能够返回
+				return;
+			}
+
+			base::task::MutexGuard g{_lock};
+
+			if (_is_cancellation_request)
+			{
+				// 竞争锁后再次确认
+				return;
+			}
+
+			_is_cancellation_request = true;
+
+			// 执行列表中的每一个委托
+			for (auto const &pair : _delegates)
+			{
+				pair.second();
+			}
+		}
 
 	public:
 		///
@@ -103,18 +131,50 @@ namespace base
 		///
 		/// @brief 注册一个委托，当令牌取消时会被调用。
 		///
-		/// @param func @note 可以多次调用注册多个委托。
+		/// @note 可以多次调用注册多个委托。
 		///
-		/// @return std::shared_ptr<base::IIdToken> 用来取消注册委托的 token.
+		/// @param func
 		///
-		std::shared_ptr<base::IIdToken> Register(std::function<void(void)> const &func);
+		/// @return 用来取消注册委托的 token.
+		///
+		std::shared_ptr<base::IIdToken> Register(std::function<void(void)> const &func)
+		{
+			if (this == None().get())
+			{
+				throw std::runtime_error{"不要对 None 调用 Register 方法"};
+			}
+
+			base::task::MutexGuard g{_lock};
+			uint64_t current_id = _id++;
+			_delegates[current_id] = func;
+			return std::shared_ptr<IdToken>{new IdToken{this, current_id}};
+		}
 
 		///
 		/// @brief 注销通过 Register 方法注册的委托。
 		///
 		/// @param token 传入由 Register 方法返回的 token.
 		///
-		void Unregister(std::shared_ptr<base::IIdToken> const &token);
+		void Unregister(std::shared_ptr<base::IIdToken> const &token)
+		{
+			if (this == None().get())
+			{
+				return;
+			}
+
+			if (token == nullptr)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "禁止传入空指针。"};
+			}
+
+			if (token->Provider() != this)
+			{
+				throw std::invalid_argument{CODE_POS_STR + "传入了错误的 IIdToken，它不是本对象发放的。"};
+			}
+
+			base::task::MutexGuard g{_lock};
+			_delegates.erase(token->ID());
+		}
 	};
 
 } // namespace base
