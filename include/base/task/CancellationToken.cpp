2e6884408b225fe69f7b8872b08104e9adf85f16
hjc2000 <hjc.666@foxmail.com>
2025-08-04 09:56:14 +0800

    为CancellationToken.cpp添加IWYU(Include What You Use) pragma keep注释

--- include/base/task/CancellationToken.cpp
+++ include/base/task/CancellationToken.cpp
@@ -1,3 +1,3 @@
-#include "CancellationToken.h"
+#include "CancellationToken.h" // IWYU pragma: keep
 
 std::shared_ptr<base::CancellationToken> base::CancellationToken::_none_cancellation_token{new base::CancellationToken{}};
