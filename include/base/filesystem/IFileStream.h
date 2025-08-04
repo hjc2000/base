0a41e130fbe47b71e2c364eeaf6f2fa0427622aa
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-05 00:37:23 +0800

    为IFileStream接口方法添加override修饰符以明确重写关系

--- include/base/filesystem/IFileStream.h
+++ include/base/filesystem/IFileStream.h
@@ -21,49 +21,49 @@ namespace base
 		///
 		/// @return
 		///
-		virtual bool CanRead() const = 0;
+		virtual bool CanRead() const override = 0;
 
 		///
 		/// @brief 本流能否写入。
 		///
 		/// @return
 		///
-		virtual bool CanWrite() const = 0;
+		virtual bool CanWrite() const override = 0;
 
 		///
 		/// @brief 本流能否定位。
 		///
 		/// @return
 		///
-		virtual bool CanSeek() const = 0;
+		virtual bool CanSeek() const override = 0;
 
 		///
 		/// @brief 流的长度
 		///
 		/// @return
 		///
-		virtual int64_t Length() const = 0;
+		virtual int64_t Length() const override = 0;
 
 		///
 		/// @brief 设置流的长度。
 		///
 		/// @param value
 		///
-		virtual void SetLength(int64_t value) = 0;
+		virtual void SetLength(int64_t value) override = 0;
 
 		///
 		/// @brief 流当前的位置。
 		///
 		/// @return
 		///
-		virtual int64_t Position() const = 0;
+		virtual int64_t Position() const override = 0;
 
 		///
 		/// @brief 设置流当前的位置。
 		///
 		/// @param value
 		///
-		virtual void SetPosition(int64_t value) = 0;
+		virtual void SetPosition(int64_t value) override = 0;
 		/* #endregion */
 
 		/* #region 读写冲关 */
@@ -75,14 +75,14 @@ namespace base
 		///
 		/// @return
 		///
-		virtual int64_t Read(base::Span const &span) = 0;
+		virtual int64_t Read(base::Span const &span) override = 0;
 
 		///
 		/// @brief 将 span 中的数据写入本流。
 		///
 		/// @param span
 		///
-		virtual void Write(base::ReadOnlySpan const &span) = 0;
+		virtual void Write(base::ReadOnlySpan const &span) override = 0;
 
 		///
 		/// @brief 冲洗流。
@@ -90,14 +90,14 @@ namespace base
 		/// @note 对于写入的数据，作用是将其从内部缓冲区转移到底层。
 		/// @note 对于内部的可以读取但尚未读取的数据，一般不会有什么作用。Flush 没见过对可读数据生效的。
 		///
-		virtual void Flush() = 0;
+		virtual void Flush() override = 0;
 
 		///
 		/// @brief 关闭流。
 		///
 		/// @note 关闭后对流的操作将会引发异常。
 		///
-		virtual void Close() = 0;
+		virtual void Close() override = 0;
 		/* #endregion */
 	};
 
