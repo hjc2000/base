da3cd780d55dffaf985a36b843daf3c16743f63e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-05 21:53:39 +0800

    移除base::LessThanOrEqual和base::GreaterThanOrEqual辅助函数并简化比较运算符概念检查

--- include/base/sfinae/Compare.h
+++ include/base/sfinae/Compare.h
@@ -1,7 +1,5 @@
 #pragma once
 #include "base/sfinae/Equal.h" // IWYU pragma: export
-#include "base/string/define.h"
-#include <stdexcept>
 #include <type_traits>
 
 namespace base
@@ -16,52 +14,14 @@ namespace base
 		requires(std::is_same_v<decltype(a > b), bool>);
 	};
 
-	/* #region 小于等于。 */
-
 	template <typename T, typename = void>
 	concept has_less_than_or_equal_operator = requires(T a, T b) {
 		requires(std::is_same_v<decltype(a <= b), bool>);
 	};
 
-	template <typename T>
-		requires(base::has_less_than_or_equal_operator<T>)
-	inline constexpr bool LessThanOrEqual(T const &lhs, T const &rhs)
-	{
-		return lhs <= rhs;
-	}
-
-	template <typename T>
-		requires(!base::has_less_than_or_equal_operator<T>)
-	inline constexpr bool LessThanOrEqual(T const &lhs, T const &rhs)
-	{
-		throw std::runtime_error{CODE_POS_STR + "请先实现 <= 运算符。"};
-		return false;
-	}
-
-	/* #endregion */
-
-	/* #region 大于等于。 */
-
 	template <typename T, typename = void>
 	concept has_greater_than_or_equal_operator = requires(T a, T b) {
 		requires(std::is_same_v<decltype(a >= b), bool>);
 	};
-
-	template <typename T>
-		requires(base::has_greater_than_or_equal_operator<T>)
-	inline constexpr bool GreaterThanOrEqual(T const &lhs, T const &rhs)
-	{
-		return lhs >= rhs;
-	}
-
-	template <typename T>
-		requires(!base::has_greater_than_or_equal_operator<T>)
-	inline constexpr bool GreaterThanOrEqual(T const &lhs, T const &rhs)
-	{
-		throw std::runtime_error{CODE_POS_STR + "请先实现 >= 运算符。"};
-		return false;
-	}
-
-	/* #endregion */
 
 } // namespace base
