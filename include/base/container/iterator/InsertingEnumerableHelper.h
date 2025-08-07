4e4409f84fa36eac7dd9c7ce5852b941af76c3bd
hjc2000 <hjc.666@foxmail.com>
2025-08-07 17:11:16 +0800

    重构Iterator相等运算符，明确区分不同枚举器状态的比较逻辑

--- include/base/container/iterator/InsertingEnumerableHelper.h
+++ include/base/container/iterator/InsertingEnumerableHelper.h
@@ -83,26 +83,34 @@ namespace base
 
 			bool operator==(Iterator const &other) const
 			{
-				if (_enumerator != nullptr)
+				if (_enumerator == other._enumerator)
 				{
-					// 本对象不是 end.
-					if (_count <= 0)
-					{
-						return true;
-					}
+					// 两者指向同一个对象，无论如何都是相等的。
+					return true;
+				}
 
+				if (_enumerator != nullptr && other._enumerator == nullptr)
+				{
+					// 本对象不是 end, other 是 end.
 					return _enumerator->IsEnd();
 				}
 
-				if (other._enumerator != nullptr)
+				if (_enumerator == nullptr && other._enumerator != nullptr)
 				{
-					// 本对象是 end 而 other 不是。
-					if (other._count <= 0)
-					{
-						return true;
-					}
-
+					// 本对象是 end, other 不是 end.
 					return other._enumerator->IsEnd();
+				}
+
+				if (_enumerator != nullptr && other._enumerator != nullptr)
+				{
+					// 两个的 _enumerator 指向不同对象，分别独立迭代，则比较 _count 计数
+					// 来判断是不是迭代到相同的位置了。
+					return _count == other._count;
+				}
+
+				if (_enumerator == nullptr && other._enumerator == nullptr)
+				{
+					return true;
 				}
 
 				return true;
