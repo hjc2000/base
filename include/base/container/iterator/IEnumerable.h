5904647ada7be046bba6b167e049552b47c3fa7f
hjc2000 <hjc.666@foxmail.com>
2025-08-07 17:09:09 +0800

    修复Iterator相等运算符中的比较逻辑，完善不同情况下的相等判断

--- include/base/container/iterator/IEnumerable.h
+++ include/base/container/iterator/IEnumerable.h
@@ -113,14 +113,34 @@ namespace base
 
 			bool operator==(Iterator<item_type> const &other) const
 			{
-				if (_enumerator != nullptr)
+				if (_enumerator == other._enumerator)
 				{
+					// 两者指向同一个对象，无论如何都是相等的。
+					return true;
+				}
+
+				if (_enumerator != nullptr && other._enumerator == nullptr)
+				{
+					// 本对象不是 end, other 是 end.
 					return _enumerator->IsEnd();
 				}
 
-				if (other._enumerator != nullptr)
+				if (_enumerator == nullptr && other._enumerator != nullptr)
 				{
+					// 本对象是 end, other 不是 end.
 					return other._enumerator->IsEnd();
+				}
+
+				if (_enumerator != nullptr && other._enumerator != nullptr)
+				{
+					// 两个的 _enumerator 指向不同对象，分别独立迭代，则比较 _position 计数
+					// 来判断是不是迭代到相同的位置了。
+					return _position == other._position;
+				}
+
+				if (_enumerator == nullptr && other._enumerator == nullptr)
+				{
+					return true;
 				}
 
 				return true;
