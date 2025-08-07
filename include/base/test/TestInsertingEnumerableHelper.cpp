42b03f7339bfe9c2ab4765ad79a3fe4c276a3a9c
hjc2000 <hjc.666@foxmail.com>
2025-08-07 17:27:25 +0800

    在TestInsertingEnumerableHelper测试中添加List和vector的插入操作示例

--- include/base/test/TestInsertingEnumerableHelper.cpp
+++ include/base/test/TestInsertingEnumerableHelper.cpp
@@ -1,10 +1,16 @@
 #include "TestInsertingEnumerableHelper.h" // IWYU pragma: keep
 #include "base/container/iterator/InsertingEnumerableHelper.h"
+#include "base/container/List.h"
+#include <vector>
 
 #if HAS_THREAD
 
 void base::test::TestInsertingEnumerableHelper()
 {
+	base::List<int> list{0, 1, 2, 3};
+	std::vector<int> vec{6, 6, 6, 6, 6, 6};
+	base::InsertingEnumerableHelper<int> helper{list, list.Count()};
+	vec.insert(vec.begin() + 1, helper.begin(), helper.end());
 }
 
 #endif // HAS_THREAD
