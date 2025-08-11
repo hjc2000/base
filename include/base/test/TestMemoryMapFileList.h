7f326249e542f33a6fca45ba7e2fc6afbd77cad6
hjc2000 <hjc.666@foxmail.com>
2025-08-11 15:52:35 +0800

    优化TestMemoryMapFileList测试输出格式，添加数字格式化及换行控制

--- include/base/test/TestMemoryMapFileList.h
+++ include/base/test/TestMemoryMapFileList.h
@@ -1,7 +1,9 @@
 #pragma once
 #include "base/container/MemoryMapFileList.h"
+#include "base/string/String.h"
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 namespace base
 {
@@ -17,8 +19,16 @@ namespace base
 
 			for (int64_t i = 0; i < 100; i++)
 			{
-				std::cout << list.Get(i) << std::endl;
+				base::String str = std::to_string(list.Get(i));
+				str.PadLeft('0', base::StringLength{2});
+				std::cout << str << ", ";
+				if ((i + 1) % 20 == 0)
+				{
+					std::cout << std::endl;
+				}
 			}
+
+			std::cout << std::endl;
 		}
 
 	} // namespace test
