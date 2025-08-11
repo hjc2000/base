cf5456f1bc24b9a04acbb5f8e16c7ddc8b004474
hjc2000 <hjc.666@foxmail.com>
2025-08-11 15:57:10 +0800

    扩展TestMemoryMapFileList测试用例，新增Insert功能测试并优化输出格式

--- include/base/test/TestMemoryMapFileList.h
+++ include/base/test/TestMemoryMapFileList.h
@@ -11,24 +11,55 @@ namespace base
 	{
 		inline void TestMemoryMapFileList()
 		{
-			base::MemoryMapFileList<int> list{"test.map"};
-			for (int64_t i = 0; i < 100; i++)
 			{
-				list.Add(i);
-			}
+				std::cout << std::endl
+						  << "测试 Add: "
+						  << std::endl;
 
-			for (int64_t i = 0; i < 100; i++)
-			{
-				base::String str = std::to_string(list.Get(i));
-				str.PadLeft('0', base::StringLength{2});
-				std::cout << str << ", ";
-				if ((i + 1) % 20 == 0)
+				base::MemoryMapFileList<int> list{"test.map"};
+				for (int64_t i = 0; i < 100; i++)
 				{
-					std::cout << std::endl;
+					list.Add(i);
 				}
+
+				for (int64_t i = 0; i < 100; i++)
+				{
+					base::String str = std::to_string(list.Get(i));
+					str.PadLeft('0', base::StringLength{2});
+					std::cout << str << ", ";
+					if ((i + 1) % 20 == 0)
+					{
+						std::cout << std::endl;
+					}
+				}
+
+				std::cout << std::endl;
 			}
 
-			std::cout << std::endl;
+			{
+				std::cout << std::endl
+						  << "测试 Insert: "
+						  << std::endl;
+
+				base::MemoryMapFileList<int> list{"test.map"};
+				for (int64_t i = 0; i < 100; i++)
+				{
+					list.Insert(0, i);
+				}
+
+				for (int64_t i = 0; i < 100; i++)
+				{
+					base::String str = std::to_string(list.Get(i));
+					str.PadLeft('0', base::StringLength{2});
+					std::cout << str << ", ";
+					if ((i + 1) % 20 == 0)
+					{
+						std::cout << std::endl;
+					}
+				}
+
+				std::cout << std::endl;
+			}
 		}
 
 	} // namespace test
