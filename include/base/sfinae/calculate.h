a7e20c1d326241348ff4325cc4a35f7336d151cf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-12 23:07:18 +0800

    为base命名空间添加算术运算符概念定义，支持加减乘除和取余运算

---
+++ include/base/sfinae/calculate.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace base
+{
+	///
+	/// @brief 概念：有加法运算符。
+	///
+	/// @param a
+	/// @param b
+	/// @return
+	///
+	template <typename ReturnType, typename TLeft, typename TRight>
+	concept has_add_operator = requires(TLeft a, TRight b) {
+		requires(std::is_same_v<decltype(a + b), ReturnType>);
+	};
+
+	///
+	/// @brief 概念：有减法运算符。
+	///
+	/// @param a
+	/// @param b
+	/// @return
+	///
+	template <typename ReturnType, typename TLeft, typename TRight>
+	concept has_minus_operator = requires(TLeft a, TRight b) {
+		requires(std::is_same_v<decltype(a - b), ReturnType>);
+	};
+
+	///
+	/// @brief 概念：有乘法运算符。
+	///
+	/// @param a
+	/// @param b
+	/// @return
+	///
+	template <typename ReturnType, typename TLeft, typename TRight>
+	concept has_multiplication_operator = requires(TLeft a, TRight b) {
+		requires(std::is_same_v<decltype(a * b), ReturnType>);
+	};
+
+	///
+	/// @brief 概念：有除法运算符。
+	///
+	/// @param a
+	/// @param b
+	/// @return
+	///
+	template <typename ReturnType, typename TLeft, typename TRight>
+	concept has_div_operator = requires(TLeft a, TRight b) {
+		requires(std::is_same_v<decltype(a / b), ReturnType>);
+	};
+
+	///
+	/// @brief 概念：有取余运算符。
+	///
+	/// @param a
+	/// @param b
+	/// @return
+	///
+	template <typename ReturnType, typename TLeft, typename TRight>
+	concept has_mod_operator = requires(TLeft a, TRight b) {
+		requires(std::is_same_v<decltype(a % b), ReturnType>);
+	};
+
+} // namespace base
