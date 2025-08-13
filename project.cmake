7bf734152311f524fd67de82125eef8051ab9c8d
hjc2000 <hjc.666@foxmail.com>
2025-08-13 15:47:57 +0800

    重构测试程序构建逻辑，将测试主程序移至test目录并简化CMake配置

--- project.cmake
+++ project.cmake
@@ -14,12 +14,15 @@ else()
 	target_compile_definitions(${ProjectName} PUBLIC HAS_THREAD=0)
 endif()
 
+
+
+
 # 添加测试程序
 if(("${platform}" STREQUAL "msys") OR
    ("${platform}" STREQUAL "msys-clang"))
-	set(exe_name "base-test")
-	add_executable(${exe_name} ${CMAKE_CURRENT_SOURCE_DIR}/exe/main.cpp)
-	target_link_libraries(${exe_name} ${ProjectName})
-	target_install(${exe_name})
-	target_total_install(${exe_name})
+	set(test_exe_target_name "base-test")
+	add_executable(${test_exe_target_name})
+	target_import_test(${test_exe_target_name})
+
+	target_link_libraries(${test_exe_target_name} ${ProjectName})
 endif()
