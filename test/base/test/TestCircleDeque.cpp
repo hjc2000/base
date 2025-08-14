5307e5ac2882573bb9379ad573ace388f8b9677b
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-14 23:22:06 +0800

    为CircleDeque添加测试用例并验证队列操作逻辑

--- test/base/test/TestCircleDeque.cpp
+++ test/base/test/TestCircleDeque.cpp
@@ -1,7 +1,50 @@
 #include "TestCircleDeque.h" // IWYU pragma: keep
 #include "base/container/CircleDeque.h"
+#include "base/string/define.h"
+#include <iostream>
+#include <stdexcept>
 
 void base::test::TestCircleDeque()
 {
 	base::CircleDeque<int, 10> queue;
+
+	queue.PushBack(0);
+	// 0
+
+	queue.PushFront(1);
+	// 1, 0
+
+	queue.PushBack(1);
+	// 1, 0, 1
+
+	queue.PushBack(2);
+	// 1, 0, 1, 2
+
+	queue.PopFront();
+	// 0, 1, 2
+
+	queue.PopBack();
+	// 0, 1
+
+	if (queue.Count() != 2)
+	{
+		throw std::runtime_error{CODE_POS_STR + "测试不通过。"};
+	}
+
+	if (queue.Get(0) != 0)
+	{
+		throw std::runtime_error{CODE_POS_STR + "测试不通过。"};
+	}
+
+	if (queue.Get(1) != 1)
+	{
+		throw std::runtime_error{CODE_POS_STR + "测试不通过。"};
+	}
+
+	for (int num : queue)
+	{
+		std::cout << num << std::endl;
+	}
+
+	std::cout << CODE_POS_STR << "测试通过。" << std::endl;
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,10 @@
+#include "base/test/TestCircleDeque.h"
 #include "base/test/TestCounter.h"
 #include "base/test/TestIRandomAccessEnumerable.h"
 
 int main()
 {
-	base::test::TestCounter();
-	base::test::TestIRandomAccessEnumerable();
+	// base::test::TestCounter();
+	// base::test::TestIRandomAccessEnumerable();
+	base::test::TestCircleDeque();
 }
