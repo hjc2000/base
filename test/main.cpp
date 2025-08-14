be45423e3400686f0ce58f86d321e5780461e389
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-14 23:22:55 +0800

    清理测试代码，移除已注释的测试用例，仅保留CircleDeque测试

--- test/main.cpp
+++ test/main.cpp
@@ -1,10 +1,6 @@
 #include "base/test/TestCircleDeque.h"
-#include "base/test/TestCounter.h"
-#include "base/test/TestIRandomAccessEnumerable.h"
 
 int main()
 {
-	// base::test::TestCounter();
-	// base::test::TestIRandomAccessEnumerable();
 	base::test::TestCircleDeque();
 }
