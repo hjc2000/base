98e8f5f45376c1eeabb24d8f89d82b80af1560ce
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-17 22:22:47 +0800

    优化dma_copy接口设计，交换函数实现顺序并添加大小检查异常

--- include/base/stream/dma_copy.h
+++ include/base/stream/dma_copy.h
@@ -2,19 +2,37 @@
 #include "base/define.h"
 #include "base/stream/ReadOnlySpan.h"
 #include "base/stream/Span.h"
+#include "base/string/define.h"
 #include <cstdint>
+#include <stdexcept>
 
 namespace base
 {
 	///
-	/// @brief 利用 DMA 将数据从 src 内存段拷贝到 dst 内存段。
+	/// @brief 利用 DMA 将数据从 [begin, end) 内存段拷贝到 dst 指针指向的位置。
 	///
-	/// @note 实现者需要检查 src 和 dst 是否尺寸相等，不相等需要抛出异常。
+	/// @param begin
+	/// @param end
+	/// @param dst
+	///
+	void dma_copy(uint8_t const *begin, uint8_t const *end, uint8_t *dst);
+
+	///
+	/// @brief 利用 DMA 将数据从 src 内存段拷贝到 dst 内存段。
 	///
 	/// @param src
 	/// @param dst
 	///
-	void dma_copy(base::ReadOnlySpan const &src, base::Span const &dst);
+	IMPLEMENTED
+	inline void dma_copy(base::ReadOnlySpan const &src, base::Span const &dst)
+	{
+		if (src.Size() != dst.Size())
+		{
+			throw std::invalid_argument{CODE_POS_STR + "要 src 和 dst 的大小不相等。"};
+		}
+
+		dma_copy(src.Buffer(), src.Buffer() + src.Size(), dst.Buffer());
+	}
 
 	///
 	/// @brief 利用 DMA 将数据从 [begin, end) 内存段拷贝到 dst 内存段。
@@ -28,21 +46,6 @@ namespace base
 	{
 		base::ReadOnlySpan src_span{begin, end - begin};
 		dma_copy(src_span, dst);
-	}
-
-	///
-	/// @brief 利用 DMA 将数据从 [begin, end) 内存段拷贝到 dst 指针指向的位置。
-	///
-	/// @param begin
-	/// @param end
-	/// @param dst
-	///
-	IMPLEMENTED
-	inline void dma_copy(uint8_t const *begin, uint8_t const *end, uint8_t *dst)
-	{
-		base::ReadOnlySpan src_span{begin, end - begin};
-		base::Span dst_span{dst, end - begin};
-		dma_copy(src_span, dst_span);
 	}
 
 } // namespace base
