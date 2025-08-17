af450c9953953f43e50c5d326fa335a419b0260f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-18 00:53:55 +0800

    为cache.h添加带地址和大小参数的clean_d_cache函数重载

--- include/base/embedded/cache/cache.h
+++ include/base/embedded/cache/cache.h
@@ -44,6 +44,17 @@ namespace base
 		void clean_d_cache();
 
 		///
+		/// @brief 清除 D-Cache。
+		///
+		/// @note 即将原本从内存中读取到 D-Cache 中，并且已经修改过的 D-Cache 的数据写入
+		/// 内存中而不是仅仅停留在 D-Cache 中。
+		///
+		/// @param address
+		/// @param size
+		///
+		void clean_d_cache(void *address, size_t size);
+
+		///
 		/// @brief 清除并且无效化 D-Cache.
 		///
 		void clean_and_invalidate_d_cache();
