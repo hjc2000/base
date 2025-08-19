3896f92f5546657ecee6924db34b9e067afa5866
hjc2000 <hjc.666@foxmail.com>
2025-08-19 09:24:56 +0800

    优化TestMemoryDma测试模块，改进src_buffer初始化格式以增强代码可读性

--- include/base/test/TestMemoryDma.h
+++ include/base/test/TestMemoryDma.h
@@ -30,7 +30,10 @@ namespace base
 			///
 			struct Data
 			{
-				alignas(Align) uint8_t src_buffer[16] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+				alignas(Align) uint8_t src_buffer[16] =
+					{1, 2, 3, 4, 5, 6, 7,
+					 8, 9, 10, 11, 12, 13, 14, 15, 16};
+
 				alignas(Align) uint8_t dst_buffer[16]{};
 			};
 
