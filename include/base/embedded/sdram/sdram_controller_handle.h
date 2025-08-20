59b53e78389556ff66b0baff784ad0fd4916f7ab
hjc2000 <hjc.666@foxmail.com>
2025-08-20 14:19:28 +0800

    优化SDRAM控制器文档，调整GPIO初始化说明位置并补充细节

--- include/base/embedded/sdram/sdram_controller_handle.h
+++ include/base/embedded/sdram/sdram_controller_handle.h
@@ -25,18 +25,18 @@ namespace base
 		///
 		/// @note 在初始化 SDRAM 控制器时，控制器会回调此函数。在这里执行一些底层的初始化，例如 GPIO 初始化。
 		///
+		/// @note 需自行初始化 SDRAM 控制器的 GPIO 引脚。SDRAM 控制器无法负责 GPIO 引脚的初始化。
+		/// 因为：
+		/// 	@li 即使是同一个外设的同一个信号，也可以分配到不同的 GPIO 引脚上。
+		/// 	@li SDRAM 芯片各种各样，所需的地址线和数据线个数也不一样。
+		/// 这部分工作需要结合原理图完成。
+		///
 		/// @param id
 		///
 		void msp_initialize_callback(uint32_t id);
 
 		///
 		/// @brief 将 SDRAM 控制器初始化为读突发模式。写不突发。
-		///
-		/// @note 需自行初始化 SDRAM 控制器的 GPIO 引脚。SDRAM 控制器无法负责 GPIO 引脚的初始化。
-		/// 因为：
-		/// 	@li 即使是同一个外设的同一个信号，也可以分配到不同的 GPIO 引脚上。
-		/// 	@li SDRAM 芯片各种各样，所需的地址线和数据线个数也不一样。
-		/// 这部分工作需要结合原理图完成。
 		///
 		/// @param h
 		/// @param timing_provider
