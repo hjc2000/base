327f53b5c39d0601ba63d5fc63ad930ae5aaa9ec
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-20 22:01:12 +0800

    优化Slot类文档，为设备插槽添加详细注释说明

--- include/base/embedded/Slot.h
+++ include/base/embedded/Slot.h
@@ -7,23 +7,50 @@
 
 namespace base
 {
+	///
+	/// @brief 设备插槽类。
+	///
+	/// @note 初始化阶段初始化设备后插入插槽，后续不要修改了。
+	///
+	/// @note 插槽不是线程安全的，只允许一个初始化线程将设备插入插槽，并且操作过程中不允许
+	/// 其他线程读写。没错，读也不行。
+	///
 	template <typename T>
 	class Slot
 	{
 	private:
+		///
+		/// @brief 使用类型擦除，只生成一种类型的 std::vector, 减小代码段大小。
+		///
 		std::vector<std::shared_ptr<void>> _vector;
 
 	public:
+		///
+		/// @brief 插槽中的设备数量。
+		///
+		/// @return
+		///
 		int32_t Count() const
 		{
 			return static_cast<int32_t>(_vector.size());
 		}
 
+		///
+		/// @brief 添加一个设备到插槽中。
+		///
+		/// @param item
+		///
 		void Add(std::shared_ptr<T> const &item)
 		{
 			_vector.push_back(item);
 		}
 
+		///
+		/// @brief 获取插槽中指定索引的设备。
+		///
+		/// @param index
+		/// @return
+		///
 		std::shared_ptr<T> operator[](int32_t index) const
 		{
 			if (index < 0 || index > static_cast<int32_t>(_vector.size()))
@@ -34,4 +61,5 @@ namespace base
 			return std::static_pointer_cast<T>(_vector[index]);
 		}
 	};
+
 } // namespace base
