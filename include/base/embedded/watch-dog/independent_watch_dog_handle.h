fd8cd5786b18ea6106beebcaad31cb9182ee036e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-20 22:25:06 +0800

    扩展独立看门狗功能，为independent_watch_dog_handle添加打开、初始化和喂狗接口

--- include/base/embedded/watch-dog/independent_watch_dog_handle.h
+++ include/base/embedded/watch-dog/independent_watch_dog_handle.h
@@ -1,10 +1,41 @@
 #pragma once
+#include <chrono>
+#include <cstdint>
+#include <memory>
 
 namespace base
 {
 	namespace independent_watch_dog
 	{
+		///
+		/// @brief 独立看门狗句柄。
+		///
+		///
 		class independent_watch_dog_handle;
 
-	}
+		///
+		/// @brief 打开独立看门狗。
+		///
+		/// @param id
+		/// @return
+		///
+		std::shared_ptr<base::independent_watch_dog::independent_watch_dog_handle> open(uint32_t id);
+
+		///
+		/// @brief 初始化独立看门狗。
+		///
+		/// @param self
+		/// @param timeout
+		///
+		void initialize(base::independent_watch_dog::independent_watch_dog_handle &self,
+						std::chrono::milliseconds const &timeout);
+
+		///
+		/// @brief 喂狗。
+		///
+		/// @param self
+		///
+		void feed(base::independent_watch_dog::independent_watch_dog_handle &self);
+
+	} // namespace independent_watch_dog
 } // namespace base
