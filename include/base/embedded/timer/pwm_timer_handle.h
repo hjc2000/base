8d3331b2dfd18823468c3847a904bbdd25ad5961
hjc2000 <hjc.666@foxmail.com>
2025-08-25 11:58:10 +0800

    扩展嵌入式功能，完善PWM定时器模块功能，添加频率初始化及周期占空比获取接口

--- include/base/embedded/timer/pwm_timer_handle.h
+++ include/base/embedded/timer/pwm_timer_handle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/unit/Hz.h"
 #include <cstdint>
 #include <memory>
 
@@ -9,6 +10,27 @@ namespace base
 		class pwm_timer_handle;
 
 		std::shared_ptr<base::pwm_timer::pwm_timer_handle> open(uint32_t id);
+
+		void initialize(base::pwm_timer::pwm_timer_handle &self,
+						base::unit::Hz const &frequency);
+
+		///
+		/// @brief 获取一个整数，代表了 PWM 的周期。
+		///
+		/// @param self
+		/// @return
+		///
+		uint32_t cycle(base::pwm_timer::pwm_timer_handle const &self);
+
+		///
+		/// @brief 获取一个整数，代表了 PWM 的占空。
+		/// 即：
+		/// 	占空比 * cycle = duty.
+		///
+		/// @param self
+		/// @return
+		///
+		uint32_t duty(base::pwm_timer::pwm_timer_handle const &self);
 
 	} // namespace pwm_timer
 } // namespace base
