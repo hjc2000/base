7910fe3b89557481bae8da0d813111d5f26bcf41
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-25 19:46:57 +0800

    扩展嵌入式功能，完善PWM定时器模块功能，重构输出配置接口并优化注释，分离初始化与极性配置

--- include/base/embedded/timer/pwm_timer_handle.h
+++ include/base/embedded/timer/pwm_timer_handle.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "base/unit/Hz.h"
+#include <bitset>
 #include <cstdint>
 #include <memory>
 
@@ -22,14 +23,13 @@ namespace base
 		///
 		/// @note 刚开始的时候当前计数值是 0.
 		///
-		/// @note compare_value 大于当前计数值的时候输出是有效状态，输出 effective_polarity
-		/// 设置的有效极性的电平。
+		/// @note compare_value 大于当前计数值的时候输出是有效状态，输出有效极性的电平。
 		///
-		/// @note 计数值递增到等于 compare_value 的时候输出就会立刻切换成无效状态，输出与
-		/// effective_polarity 相反的的无效极性电平。
+		/// @note 计数值递增到等于 compare_value 的时候输出就会立刻切换成无效状态，
+		/// 输出无效极性电平。
 		///
 		/// @note 例如计数周期是 256, 设置 compare_value 为 256 / 8 = 128.
-		/// 设置 effective_polarity 为 Positive, 即输出有效状态是高电平。
+		/// 设置有效极性为正，即输出有效状态是高电平。
 		///
 		/// @li 刚开始计数值等于 0, 比较值大于计数值，输出是有效状态，输出高电平。
 		/// @li 计数值等于 compare_value = 128 的时候输出切换成无效状态，输出低电平。
@@ -44,49 +44,62 @@ namespace base
 		///
 		/// @param self
 		/// @param frequency PWM 的频率。
-		/// @param effective_polarity 有效极性。
 		///
 		void initialize_as_up_mode(base::pwm_timer::pwm_timer_handle &self,
-								   base::unit::Hz const &frequency,
-								   base::pwm_timer::Polarity effective_polarity);
+								   base::unit::Hz const &frequency);
 
 		///
 		/// @brief 初始化为递减计数模式。
 		///
 		/// @note 刚开始的时候当前计数值是重装载值。
 		///
-		/// @note compare_value 小于当前计数值的时候输出是无效状态，输出与 effective_polarity
-		/// 极性相反的电平。
+		/// @note compare_value 小于当前计数值的时候输出是无效状态，输出与无效电平。
 		///
-		/// @note 计数值递减到等于 compare_value 的时候，输出立刻切换为有效状态，输出 effective_polarity
-		/// 设置的有效电平。
+		/// @note 计数值递减到等于 compare_value 的时候，输出立刻切换为有效状态，输出有效电平。
 		///
 		/// @param self
 		/// @param frequency
-		/// @param effective_polarity
 		///
 		void initialize_as_down_mode(base::pwm_timer::pwm_timer_handle &self,
-									 base::unit::Hz const &frequency,
-									 base::pwm_timer::Polarity effective_polarity);
+									 base::unit::Hz const &frequency);
 
 		///
 		/// @brief 初始化为先向上计数再向下计数的模式。
 		///
 		/// @param self
 		/// @param frequency
-		/// @param effective_polarity
 		///
 		void initialize_as_up_down_mode(base::pwm_timer::pwm_timer_handle &self,
-										base::unit::Hz const &frequency,
-										base::pwm_timer::Polarity effective_polarity);
+										base::unit::Hz const &frequency);
 
 		///
 		/// @brief 定时器输出 PWM 一个周期的计数次数。
+		///
+		/// @note 初始化完定时器核心部分后，就可以知道定时器一个周期计数多少了。
 		///
 		/// @param self
 		/// @return
 		///
 		uint32_t cycle(base::pwm_timer::pwm_timer_handle const &self);
+
+		///
+		/// @brief 配置输出。
+		///
+		/// @param self
+		/// @param channels 配置要对哪一个通道生效，就把对应的位置 1.
+		/// @param effective_polarity 有效极性。
+		/// @param idle_polarity 空闲极性。
+		/// @param compare_value 比较寄存器的比较值。
+		///
+		/// @param dead_time 同一相的一对互相反相的输出，在一个输出变成无效后，经过死区时间后
+		/// 另一个输出才会变成有效。这可以避免全桥 PWM 控制的同一相 IGBT 同时导通造成短路。
+		///
+		void configure_output(base::pwm_timer::pwm_timer_handle &self,
+							  std::bitset<32> const &channels,
+							  base::pwm_timer::Polarity effective_polarity,
+							  base::pwm_timer::Polarity idle_polarity,
+							  uint32_t compare_value,
+							  uint32_t dead_time);
 
 		///
 		/// @brief 比较值。
