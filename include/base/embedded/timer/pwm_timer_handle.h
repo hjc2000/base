bc6f1989da46b02aed6fb8dd61fdf32b430aa6e0
hjc2000 <hjc.666@foxmail.com>
2025-08-25 15:30:26 +0800

    扩展嵌入式功能，完善PWM定时器模块功能，调整类声明顺序并优化枚举定义位置

--- include/base/embedded/timer/pwm_timer_handle.h
+++ include/base/embedded/timer/pwm_timer_handle.h
@@ -7,13 +7,13 @@ namespace base
 {
 	namespace pwm_timer
 	{
+		class pwm_timer_handle;
+
 		enum class Polarity
 		{
 			Positive,
 			Negative,
 		};
-
-		class pwm_timer_handle;
 
 		std::shared_ptr<base::pwm_timer::pwm_timer_handle> open(uint32_t id);
 
