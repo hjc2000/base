7f040d138b5066695b3ad983df94f67c49fac1d4
hjc2000 <hjc.666@foxmail.com>
2025-08-25 14:42:34 +0800

    扩展嵌入式功能，完善PWM定时器模块功能，添加极性配置支持并重构比较值接口，优化初始化模式命名

--- include/base/embedded/timer/pwm_timer_handle.h
+++ include/base/embedded/timer/pwm_timer_handle.h
@@ -7,15 +7,22 @@ namespace base
 {
 	namespace pwm_timer
 	{
+		enum class Polarity
+		{
+			Positive,
+			Negative,
+		};
+
 		class pwm_timer_handle;
 
 		std::shared_ptr<base::pwm_timer::pwm_timer_handle> open(uint32_t id);
 
-		void initialize(base::pwm_timer::pwm_timer_handle &self,
-						base::unit::Hz const &frequency);
+		void initialize_as_up_count_mode(base::pwm_timer::pwm_timer_handle &self,
+										 base::unit::Hz const &frequency,
+										 base::pwm_timer::Polarity polarity);
 
 		///
-		/// @brief 定时器输出 PWM 一个周期的计数值。
+		/// @brief 定时器输出 PWM 一个周期的计数次数。
 		///
 		/// @param self
 		/// @return
@@ -23,23 +30,29 @@ namespace base
 		uint32_t cycle(base::pwm_timer::pwm_timer_handle const &self);
 
 		///
-		/// @brief PWM 占空时间。
+		/// @brief 比较值。
 		///
-		/// @note 即：
-		/// 	占空比 * cycle = duty.
+		/// @note 计数值变化到等于比较值的时候，输出极性会立刻变化。
 		///
 		/// @param self
 		/// @return
 		///
-		uint32_t duty(base::pwm_timer::pwm_timer_handle const &self);
+		uint32_t compare_value(base::pwm_timer::pwm_timer_handle const &self);
 
 		///
-		/// @brief 设置 PWM 占空时间。
+		/// @brief 设置比较值。
 		///
 		/// @param self
-		/// @param duty
+		/// @param value
 		///
-		void set_duty(base::pwm_timer::pwm_timer_handle &self, uint32_t duty);
+		void set_compare_value(base::pwm_timer::pwm_timer_handle &self, uint32_t value);
+
+		///
+		/// @brief 启动定时器，开始输出 PWM 信号。
+		///
+		/// @param self
+		///
+		void start(base::pwm_timer::pwm_timer_handle &self);
 
 	} // namespace pwm_timer
 } // namespace base
