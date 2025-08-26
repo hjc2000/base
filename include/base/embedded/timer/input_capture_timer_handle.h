2c5f6b99e6ebe6f42ed0eb369f06df079121d81e
hjc2000 <hjc.666@foxmail.com>
2025-08-26 10:24:36 +0800

    扩展嵌入式功能，完善定时器模块功能，为input_capture_timer_handle添加msp_initialize_callback方法以支持输入捕获定时器的底层初始化回调

--- include/base/embedded/timer/input_capture_timer_handle.h
+++ include/base/embedded/timer/input_capture_timer_handle.h
@@ -10,5 +10,12 @@ namespace base
 
 		std::shared_ptr<base::input_capture_timer::input_capture_timer_handle> open(uint32_t id);
 
+		///
+		/// @brief 输入捕获定时器的底层初始化回调。
+		///
+		/// @param id
+		///
+		void msp_initialize_callback(uint32_t id);
+
 	} // namespace input_capture_timer
 } // namespace base
