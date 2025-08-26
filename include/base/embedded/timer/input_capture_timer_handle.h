3ce577e7d1f05d9a4fe4b1ff9291ce21dda396ee
hjc2000 <hjc.666@foxmail.com>
2025-08-26 11:53:31 +0800

    扩展嵌入式功能，完善定时器模块功能，为input_capture_timer_handle添加捕获边沿枚举和初始化配置方法以支持输入捕获定时器的完整配置

--- include/base/embedded/timer/input_capture_timer_handle.h
+++ include/base/embedded/timer/input_capture_timer_handle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <chrono>
 #include <cstdint>
 #include <memory>
 
@@ -8,7 +9,45 @@ namespace base
 	{
 		class input_capture_timer_handle;
 
+		enum CaptureEdge
+		{
+			///
+			/// @brief 上升沿捕获。
+			///
+			RisingEdge,
+
+			///
+			/// @brief 下降沿捕获。
+			///
+			FallenEdge,
+
+			///
+			/// @brief 上升沿和下降沿都捕获。
+			///
+			BothEdge,
+		};
+
 		std::shared_ptr<base::input_capture_timer::input_capture_timer_handle> open(uint32_t id);
+
+		///
+		/// @brief 初始化定时器核心。
+		///
+		/// @param self
+		/// @param period
+		///
+		void initialize(base::input_capture_timer::input_capture_timer_handle &self,
+						std::chrono::nanoseconds const &period);
+
+		///
+		/// @brief 配置捕获通道。
+		///
+		/// @param self
+		/// @param edge
+		/// @param input_prescaler 对要被捕获的输入信号进行预分频，然后再送入捕获通道。
+		///
+		void configure_channel(base::input_capture_timer::input_capture_timer_handle &self,
+							   base::input_capture_timer::CaptureEdge edge,
+							   uint32_t input_prescaler);
 
 		///
 		/// @brief 输入捕获定时器的底层初始化回调。
