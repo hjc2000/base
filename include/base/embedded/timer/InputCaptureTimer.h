f2b88e027cbe4260b3b402bbe58d36f885d747be
hjc2000 <hjc.666@foxmail.com>
2025-08-26 16:34:28 +0800

    为InputCaptureTimer类添加完整功能实现，包括初始化、配置通道、周期设置及启停控制

--- include/base/embedded/timer/InputCaptureTimer.h
+++ include/base/embedded/timer/InputCaptureTimer.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "input_capture_timer_handle.h"
+#include <cstdint>
 #include <memory>
 
 namespace base
@@ -12,6 +13,70 @@ namespace base
 			std::shared_ptr<base::input_capture_timer::input_capture_timer_handle> _handle;
 
 		public:
+			InputCaptureTimer(uint32_t id)
+			{
+				_handle = base::input_capture_timer::open(id);
+			}
+
+			void Initialize(std::chrono::nanoseconds const &period)
+			{
+				base::input_capture_timer::initialize(*_handle, period);
+			}
+
+			uint32_t CounterPeriod() const
+			{
+				return base::input_capture_timer::counter_period(*_handle);
+			}
+
+			void ConfigureChannel(uint32_t channel_id,
+								  base::input_capture_timer::CaptureEdge edge,
+								  uint32_t input_prescaler)
+			{
+				base::input_capture_timer::configure_channel(*_handle,
+															 channel_id,
+															 edge,
+															 input_prescaler);
+			}
+
+			std::chrono::nanoseconds Period() const
+			{
+				return base::input_capture_timer::period(*_handle);
+			}
+
+			void SetPeriod(std::chrono::nanoseconds const &value)
+			{
+				base::input_capture_timer::set_period(*_handle, value);
+			}
+
+			void SetPeriodElapsedCallback(std::function<void()> const &callback)
+			{
+				base::input_capture_timer::set_period_elapsed_callback(*_handle, callback);
+			}
+
+			void SetCaptureCompleteCallback(std::function<void(base::input_capture_timer::CaptureCompleteEventArgs const &)> const &callback)
+			{
+				base::input_capture_timer::set_capture_complete_callback(*_handle, callback);
+			}
+
+			void Start(uint32_t channel_id)
+			{
+				base::input_capture_timer::start(*_handle, channel_id);
+			}
+
+			void StartAllChannels()
+			{
+				base::input_capture_timer::start_all_channels(*_handle);
+			}
+
+			void Stop(uint32_t channel_id)
+			{
+				base::input_capture_timer::stop(*_handle, channel_id);
+			}
+
+			void StopAllChannels()
+			{
+				base::input_capture_timer::stop_all_channels(*_handle);
+			}
 		};
 
 	} // namespace input_capture_timer
