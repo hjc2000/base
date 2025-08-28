53cf279a9e82aa1ee990e40ff06b6ac6c4546657
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-08-28 22:28:17 +0800

    优化InputCaptureTimerPll的锁频环输出限制计算，考虑当前PID输出值

--- include/base/embedded/timer/InputCaptureTimerPll.cpp
+++ include/base/embedded/timer/InputCaptureTimerPll.cpp
@@ -61,7 +61,7 @@ void base::InputCaptureTimerPll::OnPeriodElapsed()
 {
 	_additional_capture_period += _timer.CounterPeriod();
 
-	int64_t const pll_output_limit = std::min<int64_t>(static_cast<int64_t>(_adjust_limit),
+	int64_t const pll_output_limit = std::min<int64_t>(static_cast<int64_t>(_adjust_limit - _fll_pid.CurrentOutput().Abs()),
 													   static_cast<int64_t>(_timer.CounterPeriod() / 4));
 
 	if (_fll_error > pll_output_limit)
