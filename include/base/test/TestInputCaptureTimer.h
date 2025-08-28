269db4fd22ebd93bc9727af794b7d503d8cfa52d
hjc2000 <hjc.666@foxmail.com>
2025-08-28 11:28:45 +0800

    将TestInputCaptureTimer的日志输出移至独立任务并调整定时器周期为50ms

--- include/base/test/TestInputCaptureTimer.h
+++ include/base/test/TestInputCaptureTimer.h
@@ -3,6 +3,7 @@
 #include "base/embedded/timer/InputCaptureTimer.h"
 #include "base/embedded/timer/InputCaptureTimerPll.h"
 #include "base/task/BinarySemaphore.h"
+#include "base/task/delay.h"
 #include "base/task/task.h"
 #include "base/unit/Hz.h"
 #include "base/unit/Nanosecond.h"
@@ -19,7 +20,7 @@ namespace base
 			auto task_func = [timer_id, channel_id]()
 			{
 				base::input_capture_timer::InputCaptureTimer timer{timer_id};
-				base::unit::Nanosecond period{60 * 1000};
+				base::unit::Nanosecond period{50 * 1000};
 				base::unit::Hz pwm_frequency{1 * 1000};
 				base::unit::Nanosecond pwm_period{pwm_frequency};
 				timer.Initialize(std::chrono::microseconds{period});
@@ -52,25 +53,27 @@ namespace base
 
 				timer.Start(channel_id);
 
-				uint32_t loop_times = 0;
+				base::task::run(
+					[&]()
+					{
+						while (true)
+						{
+							base::console.Write("timer.CounterPeriod() = ");
+							base::console.Write(std::to_string(timer.CounterPeriod()));
+							base::console.WriteLine();
+
+							base::console.Write("捕获值：");
+							base::console.Write(std::to_string(capture_value));
+							base::console.WriteLine();
+
+							base::task::Delay(std::chrono::milliseconds{1000});
+						}
+					});
+
 				while (true)
 				{
 					semaphore.Acquire();
 					pll.Adjust();
-
-					if (loop_times % 1000 == 0)
-					{
-						base::console.Write("timer.CounterPeriod() = ");
-						base::console.Write(std::to_string(timer.CounterPeriod()));
-						base::console.WriteLine();
-
-						base::console.Write("捕获值：");
-						base::console.Write(std::to_string(capture_value));
-						base::console.WriteLine();
-					}
-
-					loop_times++;
-					loop_times %= 1000;
 				}
 			};
 
