f5f7e35551a52e184aa239581af351b081d6c929
hjc2000 <hjc.666@foxmail.com>
2025-08-29 16:52:41 +0800

    为Stack和StdPairWrapper添加int类型模板实例化并移动Stack测试代码到test目录

--- include/base/container/Stack.cpp
+++ include/base/container/Stack.cpp
@@ -1,20 +1,3 @@
 #include "Stack.h"
-#include <iostream>
 
-#if HAS_THREAD
-
-void base::test::test_stack()
-{
-	base::Stack<int> stack;
-	for (int i = 0; i < 10; i++)
-	{
-		stack.Push(i);
-	}
-
-	for (int i = 0; i < 10; i++)
-	{
-		std::cout << stack.Pop() << std::endl;
-	}
-}
-
-#endif
+template class base::Stack<int>;
