a429390ebad23c491f2aeed803ae6e627cae5865
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-14 14:23:52 +0800

    feat(flash): 优化 flash_handle 接口并添加编程功能
    - 修改函数名以更准确地反映其功能：
      - program_size 改为 programming_size
      - erase 改为 erase_sector
    - 添加新的编程相关函数：program_sector
    - 优化函数注释，明确参数和异常的含义
    - 引入 ReadOnlySpan 以支持只读数据访问

--- include/base/embedded/flash/flash_handle.h
+++ include/base/embedded/flash/flash_handle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/stream/ReadOnlySpan.h"
 #include "base/stream/Span.h"
 #include <cstdint>
 #include <memory>
@@ -76,37 +77,65 @@ namespace base
 		/// @param self
 		/// @return
 		///
-		int64_t program_size(base::flash::flash_handle &self);
+		int64_t programming_size(base::flash::flash_handle &self);
 
 		///
 		/// @brief 擦除一个扇区。
 		///
 		/// @param self
-		/// @param sector_index
+		/// @param sector_index 扇区索引。
 		///
 		/// @exception SectorIndexOutOfRangeException 扇区索引超出范围会抛出此异常。
 		///
-		void erase(base::flash::flash_handle &self, int64_t sector_index);
+		void erase_sector(base::flash::flash_handle &self, int64_t sector_index);
 
 		///
 		/// @brief 读取一个扇区内的数据。
 		///
 		/// @param self
 		/// @param sector_index 扇区索引。
-		/// @param offset 扇区内地址偏移量。即要读取的起始位置相对于扇区起始偏移了多少字节。
-		/// @param span 读出的数据放到此 span 中。
 		///
-		/// @note 此函数禁止跨扇区读取。例如指定的起始地址离扇区结束只有 256 字节，span
-		/// 的大小就不能超过 256 字节。
+		/// @param offset 扇区内地址偏移量。即要读取的起始位置相对于扇区起始偏移了多少字节。
+		///		@note 必须对齐到最小读取粒度。
+		///
+		/// @param span 读出的数据放到此 span 中。
+		/// 	@note 此函数禁止跨扇区读取。例如指定的起始地址离扇区结束只有 256 字节，span
+		/// 	的大小就不能超过 256 字节。
 		///
 		/// @exception SectorIndexOutOfRangeException 扇区索引超出范围会抛出此异常。
 		/// @exception CrossSectorException span 如果跨扇区了，会抛出此异常。
-		/// @exception AlignmentException span 如果没有按照最小粒度对齐，会抛出此异常。
+		///
+		/// @exception AlignmentException 如果 span 的大小没有对齐到最小读取粒度，会抛出
+		/// 此异常。
 		///
 		void read_sector(base::flash::flash_handle &self,
 						 int64_t sector_index,
 						 int64_t offset,
 						 base::Span const &span);
+
+		///
+		/// @brief 编程。
+		///
+		/// @param self
+		/// @param sector_index 扇区索引。
+		///
+		/// @param offset 扇区内地址偏移量。即要编程的起始位置相对于扇区起始偏移了多少字节。
+		///		@note 必须对齐到最小编程粒度。
+		///
+		/// @param span
+		/// 	@note 此函数禁止跨扇区编程。例如指定的起始地址离扇区结束只有 256 字节，span
+		/// 	的大小就不能超过 256 字节。
+		///
+		/// @exception SectorIndexOutOfRangeException 扇区索引超出范围会抛出此异常。
+		/// @exception CrossSectorException span 如果跨扇区了，会抛出此异常。
+		///
+		/// @exception AlignmentException 如果 span 的大小没有对齐到最小编程粒度，会抛出
+		/// 此异常。
+		///
+		void program_sector(base::flash::flash_handle &self,
+							int64_t sector_index,
+							int64_t offset,
+							base::ReadOnlySpan const &span);
 
 	} // namespace flash
 } // namespace base
