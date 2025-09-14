b1cf0ce9725266a279edf728b24f66ed184cdccd
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-14 12:48:34 +0800

    feat(flash): 添加 Flash 擦除和读取扇区功能
    - 新增 erase 函数，用于擦除指定扇区
    - 新增 read_sector 函数，用于读取指定扇区内数据
    - 更新 flash_handle 接口，增加新功能

--- include/base/embedded/flash/flash_handle.h
+++ include/base/embedded/flash/flash_handle.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/stream/Span.h"
 #include <cstdint>
 #include <memory>
 
@@ -42,6 +43,30 @@ namespace base
 		/// @return
 		///
 		int64_t program_size(base::flash::flash_handle &self);
+
+		///
+		/// @brief 擦除一个扇区。
+		///
+		/// @param self
+		/// @param sector_index
+		///
+		void erase(base::flash::flash_handle &self, int64_t sector_index);
+
+		///
+		/// @brief 读取一个扇区内的数据。
+		///
+		/// @param self
+		/// @param sector_index 扇区索引。
+		/// @param offset 扇区内地址偏移量。即要读取的起始位置相对于扇区起始偏移了多少字节。
+		/// @param span 读出的数据放到此 span 中。
+		///
+		/// @note 此函数禁止跨扇区读取。例如指定的起始地址离扇区结束只有 256 字节，span
+		/// 的大小就不能超过 256 字节。
+		///
+		void read_sector(base::flash::flash_handle &self,
+						 int64_t sector_index,
+						 int64_t offset,
+						 base::Span const &span);
 
 	} // namespace flash
 } // namespace base
