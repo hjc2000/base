e105e16ccf720bed6063454d87f61c59196a843a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-14 12:13:51 +0800

    feat(flash): 添加闪存设备属性查询功能
    - 增加了 sector_size、sector_count 和 reading_size 三个函数
    - 这些函数允许查询闪存设备的扇区大小、扇区数量和读取粒度
    - 为擦除和读取操作提供了更精确的设备属性信息

--- include/base/embedded/flash/flash_handle.h
+++ include/base/embedded/flash/flash_handle.h
@@ -12,5 +12,28 @@ namespace base
 
 		void initialize(base::flash::flash_handle &self);
 
+		///
+		/// @brief 一个扇区的大小。单位：字节。
+		///
+		/// @note 擦除必须以扇区为单位，至少擦除一个扇区。
+		///
+		/// @param self
+		/// @return
+		///
+		int64_t sector_size(base::flash::flash_handle &self);
+
+		///
+		/// @brief 扇区数量。
+		///
+		/// @param self
+		/// @return
+		///
+		int64_t sector_count(base::flash::flash_handle &self);
+
+		///
+		/// @brief 读取的最小粒度。单位：字节。
+		///
+		int64_t reading_size(base::flash::flash_handle &self);
+
 	} // namespace flash
 } // namespace base
