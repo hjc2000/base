de34737146e6d9d15f51714ded34c5120caba1e2
hjc2000 <hjc.666@foxmail.com>
2025-09-22 11:20:28 +0800

    feat(usb): 完善RequestTypeOptions功能

--- include/base/embedded/usb/RequestTypeOptions.h
+++ include/base/embedded/usb/RequestTypeOptions.h
@@ -1,9 +1,14 @@
 #pragma once
 
+#include "base/bit/bit.h"
+#include <cstdint>
+
 namespace base
 {
 	namespace usb
 	{
+		/* #region 参数类 */
+
 		///
 		/// @brief 接收者类型。
 		///
@@ -33,7 +38,7 @@ namespace base
 		///
 		/// @brief 请求类型。
 		///
-		enum class RequestType
+		enum class RequestType : uint8_t
 		{
 			///
 			/// @brief 标准请求。
@@ -54,7 +59,7 @@ namespace base
 		///
 		/// @brief 数据传输方向。
 		///
-		enum class DataDirection
+		enum class DataDirection : bool
 		{
 			///
 			/// @brief 主机到设备。
@@ -67,6 +72,11 @@ namespace base
 			DeviceToHost,
 		};
 
+		/* #endregion */
+
+		///
+		/// @brief 请求类型选项。
+		///
 		class RequestTypeOptions
 		{
 		private:
@@ -84,6 +94,30 @@ namespace base
 				_recipient_type = recipient_type;
 				_request_type = request_type;
 				_data_direction = data_direction;
+			}
+
+			constexpr base::usb::RecipientType RecipientType() const
+			{
+				return _recipient_type;
+			}
+
+			constexpr base::usb::RequestType RequestType() const
+			{
+				return _request_type;
+			}
+
+			constexpr base::usb::DataDirection DataDirection() const
+			{
+				return _data_direction;
+			}
+
+			constexpr operator uint8_t() const
+			{
+				uint8_t ret = 0;
+				base::bit::WriteBits(ret, 0, 5, static_cast<uint64_t>(_recipient_type));
+				base::bit::WriteBits(ret, 5, 7, static_cast<uint64_t>(_request_type));
+				base::bit::WriteBit(ret, 7, static_cast<bool>(_data_direction));
+				return ret;
 			}
 		};
 
