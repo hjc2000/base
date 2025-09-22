c81011f7a401b702caf1f02f194eb47c49724159
hjc2000 <hjc.666@foxmail.com>
2025-09-22 10:55:59 +0800

    refactor(usb): 重构usb_fs_pcd命名空间

--- include/base/embedded/usb/UsbFsPcd.h
+++ include/base/embedded/usb/UsbFsPcd.h
@@ -6,31 +6,31 @@
 
 namespace base
 {
-	namespace usb_fs_pcd
+	namespace usb::fs_pcd
 	{
 		class UsbFsPcd
 		{
 		private:
-			std::shared_ptr<base::usb_fs_pcd::usb_fs_pcd_handle> _handle;
+			std::shared_ptr<base::usb::fs_pcd::usb_fs_pcd_handle> _handle;
 
 		public:
 			UsbFsPcd(uint32_t id)
 			{
-				_handle = base::usb_fs_pcd::open(id);
+				_handle = base::usb::fs_pcd::open(id);
 			}
 
 			void InitializeAsDevice(std::string const &clock_source_name,
 									uint32_t divider,
 									base::usb::PhyType phy_type)
 			{
-				base::usb_fs_pcd::initialize_as_device(*_handle,
-													   clock_source_name,
-													   divider,
-													   phy_type);
+				base::usb::fs_pcd::initialize_as_device(*_handle,
+														clock_source_name,
+														divider,
+														phy_type);
 			}
 		};
 
-		base::Slot<base::usb_fs_pcd::UsbFsPcd> &usb_fs_pcd_slot();
+		base::Slot<base::usb::fs_pcd::UsbFsPcd> &usb_fs_pcd_slot();
 
-	} // namespace usb_fs_pcd
+	} // namespace usb::fs_pcd
 } // namespace base
--- include/base/embedded/usb/usb_fs_pcd_handle.h
+++ include/base/embedded/usb/usb_fs_pcd_handle.h
@@ -6,26 +6,30 @@
 
 namespace base
 {
-	namespace usb_fs_pcd
+	namespace usb
 	{
-		class usb_fs_pcd_handle;
+		namespace fs_pcd
+		{
+			class usb_fs_pcd_handle;
 
-		std::shared_ptr<base::usb_fs_pcd::usb_fs_pcd_handle> open(uint32_t id);
+			std::shared_ptr<base::usb::fs_pcd::usb_fs_pcd_handle> open(uint32_t id);
 
-		void initialize_as_device(base::usb_fs_pcd::usb_fs_pcd_handle &self,
-								  std::string const &clock_source_name,
-								  uint32_t divider,
-								  base::usb::PhyType phy_type);
+			void initialize_as_device(base::usb::fs_pcd::usb_fs_pcd_handle &self,
+									  std::string const &clock_source_name,
+									  uint32_t divider,
+									  base::usb::PhyType phy_type);
 
-		///
-		/// @brief 底层初始化。
-		///
-		/// @note 例如 GPIO 引脚复用功能的初始化。这种初始化是板级的，即和具体的电路板
-		/// 相关，外设功能可以复用到不同的引脚上，这里只负责驱动外设，不处理板级的配置。
-		///
-		/// @param id
-		///
-		void msp_initialize(uint32_t id);
+			///
+			/// @brief 底层初始化。
+			///
+			/// @note 例如 GPIO 引脚复用功能的初始化。这种初始化是板级的，即和具体的电路板
+			/// 相关，外设功能可以复用到不同的引脚上，这里只负责驱动外设，不处理板级的配置。
+			///
+			/// @param id
+			///
+			void msp_initialize(uint32_t id);
 
-	} // namespace usb_fs_pcd
+		} // namespace fs_pcd
+
+	} // namespace usb
 } // namespace base
