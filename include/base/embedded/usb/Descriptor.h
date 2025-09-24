f0e5bb3232f52051c0b96fa98720da1dbb44409d
hjc2000 <hjc.666@foxmail.com>
2025-09-24 16:03:59 +0800

    Descriptor

--- include/base/embedded/usb/Descriptor.h
+++ include/base/embedded/usb/Descriptor.h
@@ -1,108 +1,17 @@
 #pragma once
+#include "base/embedded/usb/DescriptorType.h"
 #include "base/stream/ReadOnlySpan.h"
 #include "base/stream/Span.h"
 #include "base/string/define.h"
 #include <cstddef>
 #include <cstdint>
 #include <stdexcept>
+#include <string>
 
 namespace base
 {
 	namespace usb
 	{
-		enum class DescriptorType
-		{
-			///
-			/// @brief 设备描述符。
-			///
-			Device = 0x01,
-
-			///
-			/// @brief 配置描述符。
-			///
-			Configuration = 0x02,
-
-			///
-			/// @brief 字符串描述符。
-			///
-			String = 0x03,
-
-			///
-			/// @brief 接口描述符。
-			///
-			Interface = 0x04,
-
-			///
-			/// @brief 端点描述符。
-			///
-			Endpoint = 0x05,
-
-			///
-			/// @brief 设备限定描述符。
-			///
-			DeviceQualifier = 0x06,
-
-			///
-			/// @brief 其他速率配置描述符。
-			///
-			OtherSpeedConfiguration = 0x07,
-
-			///
-			/// @brief 接口功率描述符。
-			///
-			InterfacePower = 0x08,
-
-			///
-			/// @brief OTG 描述符。
-			///
-			OTG = 0x09,
-
-			///
-			/// @brief 调试描述符。
-			///
-			Debug = 0x0A,
-
-			///
-			/// @brief 接口关联描述符。
-			///
-			InterfaceAssociation = 0x0B,
-
-			///
-			/// @brief BOS 描述符。
-			///
-			BOS = 0x0F,
-
-			///
-			/// @brief 设备能力描述符。
-			///
-			DeviceCapability = 0x10,
-
-			///
-			/// @brief HID描述符。
-			///
-			HID = 0x21,
-
-			///
-			/// @brief HID 报告描述符。
-			///
-			HIDReport = 0x22,
-
-			///
-			/// @brief Hub 描述符。
-			///
-			Hub = 0x29,
-
-			///
-			/// @brief 超级速度 USB 端点伴随描述符。
-			///
-			SuperSpeedEndpointCompanion = 0x30,
-
-			///
-			/// @brief 超级速度 Plus 等时端点伴随描述符。
-			///
-			SuperSpeedPlusIsochronousEndpointCompanion = 0x31
-		};
-
 		///
 		/// @brief USB 描述符。
 		///
@@ -131,10 +40,7 @@ namespace base
 			///
 			alignas(size_t) uint8_t _data[255 - 1 - 1];
 
-		public:
-			constexpr Descriptor() = default;
-
-			Descriptor(base::usb::DescriptorType type, base::ReadOnlySpan const &span)
+			void Initialize(base::usb::DescriptorType type, base::ReadOnlySpan const &span)
 			{
 				if (span.Size() > sizeof(_data))
 				{
@@ -146,12 +52,42 @@ namespace base
 				DataSpan().CopyFrom(span);
 			}
 
-			constexpr uint8_t Length() const
+		public:
+			Descriptor() = default;
+
+			Descriptor(base::usb::DescriptorType type, base::ReadOnlySpan const &span)
+			{
+				Initialize(type, span);
+			}
+
+			///
+			/// @brief 创建字符串描述符。
+			///
+			/// @param unicode_string 字符串。这其中的字符编码必须是 UTF16-LE.
+			///
+			Descriptor(std::string const &unicode_string)
+			{
+				if (unicode_string.size() % 2 != 0)
+				{
+					// 字符编码必须是 UTF16-LE。
+					// 字符串长度为奇数说明肯定不是 UTF16-LE 编码的。
+					throw std::invalid_argument{CODE_POS_STR + "必须是 UTF16-LE 编码的字符串。"};
+				}
+
+				base::ReadOnlySpan str_span{
+					reinterpret_cast<uint8_t const *>(unicode_string.data()),
+					unicode_string.size(),
+				};
+
+				Initialize(base::usb::DescriptorType::String, str_span);
+			}
+
+			uint8_t Length() const
 			{
 				return _length;
 			}
 
-			constexpr base::usb::DescriptorType Type() const
+			base::usb::DescriptorType Type() const
 			{
 				return _type;
 			}
