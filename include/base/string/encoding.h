a0442411f4d257482a7f061ae7f673aff008d4e4
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-09-25 01:09:19 +0800

    to_utf16le_string

--- include/base/string/encoding.h
+++ include/base/string/encoding.h
@@ -1,4 +1,9 @@
 #pragma once
+#include "base/bit/bit_converte.h"
+#include <bit>
+#include <cstddef>
+#include <cstdint>
+#include <string>
 
 namespace base
 {
@@ -6,7 +11,27 @@ namespace base
 	{
 		namespace encoding
 		{
+			constexpr std::u16string to_utf16le_string(std::u16string const &str)
+			{
+				if (std::endian::native == std::endian::little)
+				{
+					return str;
+				}
 
-		}
+				std::u16string ret{str};
+				for (size_t i = 0; i < ret.size(); i++)
+				{
+					uint8_t high = ret[i] >> 8;
+					uint8_t low = ret[i] & 0xFF;
+
+					// 颠倒字节序。
+					uint16_t value = base::bit_converte::ToUInt16(low, high);
+					ret[i] = value;
+				}
+
+				return ret;
+			}
+
+		} // namespace encoding
 	} // namespace string
 } // namespace base
