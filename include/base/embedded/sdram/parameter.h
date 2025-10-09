e2cc84a0d284cb8cbb2a65c5cc2183f136df546e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-10-09 22:34:25 +0800

    ``` refactor(sdram): 重构 SDRAM 相关命名空间和类定义
    将 `base::sdram` 命名空间下的类和函数定义进行重构，统一使用 `base::sdram` 作为命名空间，
    简化代码结构并提升可读性。同时调整了头文件中的类成员布局与缩进格式，使整体风格一致。
    ```

--- include/base/embedded/sdram/parameter.h
+++ include/base/embedded/sdram/parameter.h
@@ -1,113 +1,110 @@
 #pragma once
 
-namespace base
+namespace base::sdram
 {
-	namespace sdram
+	///
+	/// @brief SDRAM 中 BANK 的数量。
+	///
+	///
+	class BankCount
 	{
-		///
-		/// @brief SDRAM 中 BANK 的数量。
-		///
-		///
-		class BankCount
+	private:
+		int _value = 0;
+
+	public:
+		constexpr explicit BankCount(int value)
+			: _value(value)
 		{
-		private:
-			int _value = 0;
+		}
 
-		public:
-			constexpr explicit BankCount(int value)
-				: _value(value)
-			{
-			}
-
-			constexpr int Value() const
-			{
-				return _value;
-			}
-		};
-
-		///
-		/// @brief 行地址的位数。
-		///
-		///
-		class RowBitCount
+		constexpr int Value() const
 		{
-		private:
-			int _value = 0;
+			return _value;
+		}
+	};
 
-		public:
-			constexpr explicit RowBitCount(int value)
-				: _value(value)
-			{
-			}
+	///
+	/// @brief 行地址的位数。
+	///
+	///
+	class RowBitCount
+	{
+	private:
+		int _value = 0;
 
-			constexpr int Value() const
-			{
-				return _value;
-			}
-		};
-
-		///
-		/// @brief 列地址的位数。
-		///
-		///
-		class ColumnBitCount
+	public:
+		constexpr explicit RowBitCount(int value)
+			: _value(value)
 		{
-		private:
-			int _value = 0;
+		}
 
-		public:
-			constexpr explicit ColumnBitCount(int value)
-				: _value(value)
-			{
-			}
-
-			constexpr int Value() const
-			{
-				return _value;
-			}
-		};
-
-		///
-		/// @brief 数据宽度，或者说数据总线位数。
-		///
-		///
-		class DataWidth
+		constexpr int Value() const
 		{
-		private:
-			int _value = 0;
+			return _value;
+		}
+	};
 
-		public:
-			constexpr explicit DataWidth(int value)
-				: _value(value)
-			{
-			}
+	///
+	/// @brief 列地址的位数。
+	///
+	///
+	class ColumnBitCount
+	{
+	private:
+		int _value = 0;
 
-			constexpr int Value() const
-			{
-				return _value;
-			}
-		};
-
-		///
-		/// @brief 读突发长度。
-		///
-		///
-		class ReadBurstLength
+	public:
+		constexpr explicit ColumnBitCount(int value)
+			: _value(value)
 		{
-		private:
-			int _value = 0;
+		}
 
-		public:
-			constexpr explicit ReadBurstLength(int value)
-				: _value(value)
-			{
-			}
+		constexpr int Value() const
+		{
+			return _value;
+		}
+	};
 
-			constexpr int Value() const
-			{
-				return _value;
-			}
-		};
+	///
+	/// @brief 数据宽度，或者说数据总线位数。
+	///
+	///
+	class DataWidth
+	{
+	private:
+		int _value = 0;
 
-	} // namespace sdram
-} // namespace base
+	public:
+		constexpr explicit DataWidth(int value)
+			: _value(value)
+		{
+		}
+
+		constexpr int Value() const
+		{
+			return _value;
+		}
+	};
+
+	///
+	/// @brief 读突发长度。
+	///
+	///
+	class ReadBurstLength
+	{
+	private:
+		int _value = 0;
+
+	public:
+		constexpr explicit ReadBurstLength(int value)
+			: _value(value)
+		{
+		}
+
+		constexpr int Value() const
+		{
+			return _value;
+		}
+	};
+
+} // namespace base::sdram
