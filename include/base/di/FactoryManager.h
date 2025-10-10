66d4024b3d6d47500a10cfb9a148b30afa7fae7e
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-10-10 22:50:12 +0800

    注释

--- include/base/di/FactoryManager.h
+++ include/base/di/FactoryManager.h
@@ -5,7 +5,9 @@ namespace base
 {
 	///
 	/// @brief 工厂管理者
+	///
 	/// @tparam FactoryType 工厂类
+	///
 	/// @note 工厂类 FactoryType 应该是接口，而不是可实例化的非抽象类，这样工厂管理者才有
 	/// 存在的意义。
 	///
@@ -21,12 +23,13 @@ namespace base
 	public:
 		///
 		/// @brief 获取工厂实例。
+		///
 		/// @note 设置了自定义工厂后就会返回自定义工厂，否则返回默认工厂。
 		///
 		/// @return 返回工厂类对象的共享指针。
-		/// @note 返回共享指针而不是引用是因为如果返回的是自定义工厂类的话，自定义工厂类
-		/// 是随时可能被替换的，如果返回的是引用，自定义工厂类被替换时智能指针会把旧的工厂
-		/// 类对象给析构了，并释放内存，这就会引发内存错误。
+		/// 	@note 返回共享指针而不是引用是因为如果返回的是自定义工厂类的话，自定义工厂类
+		/// 	是随时可能被替换的，如果返回的是引用，自定义工厂类被替换时智能指针会把旧的工厂
+		/// 	类对象给析构了，并释放内存，这就会引发内存错误。
 		///
 		std::shared_ptr<FactoryType> Factory()
 		{
@@ -50,6 +53,7 @@ namespace base
 
 		///
 		/// @brief 设置自定义工厂
+		///
 		/// @param o
 		///
 		void SetCustomFactory(std::shared_ptr<FactoryType> o)
@@ -59,9 +63,12 @@ namespace base
 
 		///
 		/// @brief 默认工厂
+		///
 		/// @note 默认的工厂。如果不存在默认，可以实现为返回空指针。
+		///
 		/// @return
 		///
 		virtual std::shared_ptr<FactoryType> DefaultFactory() = 0;
 	};
+
 } // namespace base
