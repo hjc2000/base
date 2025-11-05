148979fe8c31fd038f30f18d17de5a986ab0ce04
hjc2000 <hjc.666@foxmail.com>
2025-11-05 14:57:23 +0800

    feat(segment-led): 添加数字3到数字9的符号代码定义
    新增了静态常量函数 Number_3() 到 Number_9()，用于生成对应数字的七段数码管显示编码。
    同时删除了不再使用的 convert.h 头文件及其相关实现，因为其功能已被新的符号代码取代。
    默认符号值已从 Number_0() 更改为 Number_6()，以反映新的初始化状态。

--- include/base/embedded/segment-led/SymbolCode.h
+++ include/base/embedded/segment-led/SymbolCode.h
@@ -76,6 +76,84 @@ namespace base::segment_led
 			return SymbolCode{value};
 		}
 
+		static constexpr base::segment_led::SymbolCode Number_3()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(5);
+			value |= base::bit::Bit(6);
+			value |= base::bit::Bit(4);
+			value |= base::bit::Bit(3);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_4()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(1);
+			value |= base::bit::Bit(5);
+			value |= base::bit::Bit(6);
+			value |= base::bit::Bit(4);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_5()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(1);
+			value |= base::bit::Bit(6);
+			value |= base::bit::Bit(4);
+			value |= base::bit::Bit(3);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_6()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(1);
+			value |= base::bit::Bit(2);
+			value |= base::bit::Bit(3);
+			value |= base::bit::Bit(4);
+			value |= base::bit::Bit(6);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_7()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(5);
+			value |= base::bit::Bit(4);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_8()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(1);
+			value |= base::bit::Bit(2);
+			value |= base::bit::Bit(3);
+			value |= base::bit::Bit(4);
+			value |= base::bit::Bit(5);
+			value |= base::bit::Bit(6);
+			return SymbolCode{value};
+		}
+
+		static constexpr base::segment_led::SymbolCode Number_9()
+		{
+			uint8_t value = 0;
+			value |= base::bit::Bit(0);
+			value |= base::bit::Bit(1);
+			value |= base::bit::Bit(3);
+			value |= base::bit::Bit(4);
+			value |= base::bit::Bit(5);
+			value |= base::bit::Bit(6);
+			return SymbolCode{value};
+		}
+
 		/// @note 数码管各段 LED 的索引如下：
 		///
 		///				   0
@@ -89,6 +167,6 @@ namespace base::segment_led
 		///			       3       ．
 	};
 
-	constexpr uint8_t _value{base::segment_led::SymbolCode::Number_0()};
+	constexpr uint8_t _value{base::segment_led::SymbolCode::Number_6()};
 
 } // namespace base::segment_led
--- include/base/embedded/segment-led/convert.cpp
+++
@@ -1,1 +0,0 @@
-#include "convert.h" // IWYU pragma: keep
