0185fb009e45d8e0de32b73922d737d89fa93fe1
hjc2000 <hjc.666@foxmail.com>
2025-12-24 10:21:33 +0800

    ``` feat(unit): 添加mWh单位的运算符重载支持
    为mWh.h头文件添加了mW与Hour的乘法运算符重载，以及mWh与其他单位的除法运算符重载，
    支持mWh = mW * Hour的单位换算操作，并包含了必要的头文件依赖。
    ```

--- include/base/unit/mWh.h
+++ include/base/unit/mWh.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "base/math/Fraction.h"
+#include "base/unit/Hour.h"
+#include "base/unit/mW.h"
 #include "IUnit.h"
 #include <type_traits>
 
@@ -48,3 +50,48 @@ namespace base::unit
 	};
 
 } // namespace base::unit
+
+/* #region mWh = mW * Hour 运算符重载 */
+
+template <typename TLeft, typename TRight>
+	requires(std::is_convertible_v<TLeft, base::unit::mW> &&
+			 std::is_convertible_v<TRight, base::unit::Hour>)
+inline base::unit::mWh operator*(TLeft const &left, TRight const &right)
+{
+	base::Fraction mw = base::unit::mW{left}.Value();
+	base::Fraction hour = base::unit::Hour{right}.Value();
+	base::unit::mWh ret{mw * hour};
+	return ret;
+}
+
+template <typename TLeft, typename TRight>
+	requires(std::is_convertible_v<TLeft, base::unit::Hour> &&
+			 std::is_convertible_v<TRight, base::unit::mW>)
+inline base::unit::mWh operator*(TLeft const &left, TRight const &right)
+{
+	return right * left;
+}
+
+template <typename TLeft, typename TRight>
+	requires(std::is_convertible_v<TLeft, base::unit::mWh> &&
+			 std::is_convertible_v<TRight, base::unit::mW>)
+base::unit::Hour operator/(TLeft const &left, TRight const &right)
+{
+	base::Fraction mwh = base::unit::mWh{left}.Value();
+	base::Fraction mw = base::unit::mW{right}.Value();
+	base::unit::Hour ret{mwh / mw};
+	return ret;
+}
+
+template <typename TLeft, typename TRight>
+	requires(std::is_convertible_v<TLeft, base::unit::mWh> &&
+			 std::is_convertible_v<TRight, base::unit::Hour>)
+base::unit::mW operator/(TLeft const &left, TRight const &right)
+{
+	base::Fraction mwh = base::unit::mWh{left}.Value();
+	base::Fraction hour = base::unit::Hour{right}.Value();
+	base::unit::mW ret{mwh / hour};
+	return ret;
+}
+
+/* #endregion */
