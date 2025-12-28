79daac7dd95389faaee46b4468c930f3dc299eff
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2025-12-28 21:01:14 +0800

    fix_bilibili_uwp_video

---
+++ include/base/filesystem/fix_bilibili_uwp_video.cpp
@@ -0,0 +1,5 @@
+#include "fix_bilibili_uwp_video.h" // IWYU pragma: keep
+
+void base::fix_bilibili_uwp_video(base::Path const &video_path)
+{
+}
---
+++ include/base/filesystem/fix_bilibili_uwp_video.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "base/filesystem/Path.h"
+
+namespace base
+{
+	void fix_bilibili_uwp_video(base::Path const &video_path);
+
+} // namespace base
