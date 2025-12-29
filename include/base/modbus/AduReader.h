b7c005f3047a4ccf7a1f2aa5f685500be8d730e6
hjc2000 <hjc.666@foxmail.com>
2025-12-29 11:10:25 +0800

    namespace base::modbus

--- include/base/modbus/AduReader.h
+++ include/base/modbus/AduReader.h
@@ -9,94 +9,91 @@
 #include <cstdint>
 #include <stdexcept>
 
-namespace base
+namespace base::modbus
 {
-	namespace modbus
+	class AduReader
 	{
-		class AduReader
+	private:
+		base::ReadOnlySpan _span{};
+
+		///
+		/// @brief CRC 校验数据的内存段。
+		///
+		/// @return
+		///
+		base::ReadOnlySpan CrcSpan() const
 		{
-		private:
-			base::ReadOnlySpan _span{};
+			return _span[base::Range{_span.Size() - 2, _span.Size()}];
+		}
 
-			///
-			/// @brief CRC 校验数据的内存段。
-			///
-			/// @return
-			///
-			base::ReadOnlySpan CrcSpan() const
+		///
+		/// @brief CRC 校验值。
+		///
+		/// @return
+		///
+		uint16_t Crc() const
+		{
+			return base::big_endian_remote_converter.FromBytes<uint16_t>(CrcSpan());
+		}
+
+	public:
+		///
+		/// @brief
+		///
+		/// @param span 传进来的内存段必须以站号开始，以 CRC16 结尾，不得在末尾含有
+		/// 无效字节。传进来的整个内存段都是有效数据。
+		///
+		AduReader(base::ReadOnlySpan const &span)
+		{
+			if (span.Size() < 4)
 			{
-				return _span[base::Range{_span.Size() - 2, _span.Size()}];
+				throw std::invalid_argument{CODE_POS_STR + "传入的 span 过小，不可能装下 modbus 帧，无法读取。"};
 			}
 
-			///
-			/// @brief CRC 校验值。
-			///
-			/// @return
-			///
-			uint16_t Crc() const
-			{
-				return base::big_endian_remote_converter.FromBytes<uint16_t>(CrcSpan());
-			}
+			_span = span;
+		}
 
-		public:
-			///
-			/// @brief
-			///
-			/// @param span 传进来的内存段必须以站号开始，以 CRC16 结尾，不得在末尾含有
-			/// 无效字节。传进来的整个内存段都是有效数据。
-			///
-			AduReader(base::ReadOnlySpan const &span)
-			{
-				if (span.Size() < 4)
-				{
-					throw std::invalid_argument{CODE_POS_STR + "传入的 span 过小，不可能装下 modbus 帧，无法读取。"};
-				}
+		///
+		/// @brief 站号。
+		///
+		/// @return
+		///
+		uint8_t StationNumber() const
+		{
+			return _span[0];
+		}
 
-				_span = span;
-			}
+		///
+		/// @brief 功能码。
+		///
+		/// @return
+		///
+		base::modbus::FunctionCode FunctionCode() const
+		{
+			return base::modbus::FunctionCode{_span[1]};
+		}
 
-			///
-			/// @brief 站号。
-			///
-			/// @return
-			///
-			uint8_t StationNumber() const
-			{
-				return _span[0];
-			}
+		///
+		/// @brief 数据内存段。
+		///
+		/// @return
+		///
+		base::ReadOnlySpan DataSpan() const
+		{
+			return _span[base::Range{2, _span.Size() - 2}];
+		}
 
-			///
-			/// @brief 功能码。
-			///
-			/// @return
-			///
-			base::modbus::FunctionCode FunctionCode() const
-			{
-				return base::modbus::FunctionCode{_span[1]};
-			}
+		///
+		/// @brief 进行 CRC 校验。
+		///
+		/// @return true 表示 CRC 校验通过，false 表示 CRC 校验不通过。
+		///
+		bool CheckCrc() const
+		{
+			base::modbus::ModbusCrc16 crc{};
+			crc.Add(_span[base::Range{0, _span.Size() - 2}]);
+			return crc.RegisterValue() == Crc();
+		}
+	};
 
-			///
-			/// @brief 数据内存段。
-			///
-			/// @return
-			///
-			base::ReadOnlySpan DataSpan() const
-			{
-				return _span[base::Range{2, _span.Size() - 2}];
-			}
-
-			///
-			/// @brief 进行 CRC 校验。
-			///
-			/// @return true 表示 CRC 校验通过，false 表示 CRC 校验不通过。
-			///
-			bool CheckCrc() const
-			{
-				base::modbus::ModbusCrc16 crc{};
-				crc.Add(_span[base::Range{0, _span.Size() - 2}]);
-				return crc.RegisterValue() == Crc();
-			}
-		};
-
-	} // namespace modbus
-} // namespace base
+} // namespace base::modbus
--- include/base/modbus/ReadingRecordsResponseReader.h
+++ include/base/modbus/ReadingRecordsResponseReader.h
@@ -7,63 +7,60 @@
 #include <cstdint>
 #include <stdexcept>
 
-namespace base
+namespace base::modbus
 {
-	namespace modbus
+	///
+	/// @brief 读多个记录的响应帧读者。
+	///
+	///
+	class ReadingRecordsResponseReader
 	{
-		///
-		/// @brief 读多个记录的响应帧读者。
-		///
-		///
-		class ReadingRecordsResponseReader
+	private:
+		base::modbus::AduReader _adu_reader;
+
+	public:
+		ReadingRecordsResponseReader(base::ReadOnlySpan const &span)
+			: _adu_reader(span)
 		{
-		private:
-			base::modbus::AduReader _adu_reader;
-
-		public:
-			ReadingRecordsResponseReader(base::ReadOnlySpan const &span)
-				: _adu_reader(span)
+			base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
+			if (function_code != base::modbus::function_code::ReadRecords())
 			{
-				base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
-				if (function_code != base::modbus::function_code::ReadRecords())
-				{
-					throw std::runtime_error{CODE_POS_STR + "错误的功能码。"};
-				}
+				throw std::runtime_error{CODE_POS_STR + "错误的功能码。"};
 			}
+		}
 
-			///
-			/// @brief 站号。
-			///
-			/// @return
-			///
-			uint8_t StationNumber() const
-			{
-				return _adu_reader.StationNumber();
-			}
+		///
+		/// @brief 站号。
+		///
+		/// @return
+		///
+		uint8_t StationNumber() const
+		{
+			return _adu_reader.StationNumber();
+		}
 
-			///
-			/// @brief 数据字节数。
-			///
-			/// @return
-			///
-			uint8_t DataByteCount() const
-			{
-				return _adu_reader.DataSpan()[0];
-			}
+		///
+		/// @brief 数据字节数。
+		///
+		/// @return
+		///
+		uint8_t DataByteCount() const
+		{
+			return _adu_reader.DataSpan()[0];
+		}
 
-			///
-			/// @brief 响应中发回来的数据。字节数为 DataByteCount().
-			///
-			/// @note 本读者类无法知道这里面的数据是什么含义和格式，所以只能是返回内存段。
-			///
-			/// @return
-			///
-			base::ReadOnlySpan DataSpan() const
-			{
-				base::ReadOnlySpan adu_data_span = _adu_reader.DataSpan();
-				return adu_data_span[base::Range{1, adu_data_span.Size()}];
-			}
-		};
+		///
+		/// @brief 响应中发回来的数据。字节数为 DataByteCount().
+		///
+		/// @note 本读者类无法知道这里面的数据是什么含义和格式，所以只能是返回内存段。
+		///
+		/// @return
+		///
+		base::ReadOnlySpan DataSpan() const
+		{
+			base::ReadOnlySpan adu_data_span = _adu_reader.DataSpan();
+			return adu_data_span[base::Range{1, adu_data_span.Size()}];
+		}
+	};
 
-	} // namespace modbus
-} // namespace base
+} // namespace base::modbus
