19975e8fc48b187a3aa2f449633273d870930094
hjc2000 <hjc.666@foxmail.com>
2025-12-29 10:14:47 +0800

    注释

--- private_src/profinet/dcp/DcpHeaderReader.h
+++ private_src/profinet/dcp/DcpHeaderReader.h
@@ -8,7 +8,9 @@
 
 namespace base::profinet
 {
+	///
 	/// @brief DCP 头部读者。
+	///
 	class DcpHeaderReader
 	{
 	private:
@@ -41,26 +43,32 @@ namespace base::profinet
 			return base::big_endian_remote_converter.FromBytes<uint32_t>(span);
 		}
 
+		///
 		/// @brief 响应延迟。
 		/// @return
+		///
 		uint16_t ResponseDelay() const
 		{
 			base::ReadOnlySpan span = _span.Slice(base::Range{6, 8});
 			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
 		}
 
+		///
 		/// @brief Blocks 的有效数据的长度，包括填充字节。
 		/// @note 填充是为了 2 字节对齐，每一个 Block 都必须 2 字节对齐，如果没有对齐，
 		/// 尾部需要填充 1 字节。
 		/// @return
+		///
 		uint16_t DataLength() const
 		{
 			base::ReadOnlySpan span = _span.Slice(base::Range{8, 10});
 			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
 		}
 
+		///
 		/// @brief 头部固定为 10 字节。
 		/// @return
+		///
 		static constexpr int32_t HeaderSize()
 		{
 			return 10;
--- private_src/profinet/dcp/DcpHeaderWriter.h
+++ private_src/profinet/dcp/DcpHeaderWriter.h
@@ -3,47 +3,53 @@
 #include "base/net/profinet/dcp/DcpServiceTypeEnum.h"
 #include "base/stream/Span.h"
 
-namespace base
+namespace base::profinet
 {
-	namespace profinet
+	///
+	/// @brief DCP 头部作者。
+	///
+	class DcpHeaderWriter
 	{
-		/// @brief DCP 头部作者。
-		class DcpHeaderWriter
+	private:
+		base::Span _span;
+
+	public:
+		DcpHeaderWriter(base::Span const &span);
+
+		base::profinet::DcpServiceIdEnum ServiceId() const;
+		void SetServiceId(base::profinet::DcpServiceIdEnum value);
+
+		base::profinet::DcpServiceTypeEnum ServiceType() const;
+		void SetServiceType(base::profinet::DcpServiceTypeEnum value);
+
+		uint32_t Xid() const;
+		void SetXid(uint32_t value);
+
+		///
+		/// @brief 响应延迟。
+		/// @return
+		///
+		uint16_t ResponseDelay() const;
+
+		void SetResponseDelay(uint16_t value);
+
+		///
+		/// @brief Blocks 的有效数据的长度，包括填充字节。
+		/// @note 填充是为了 2 字节对齐，每一个 Block 都必须 2 字节对齐，如果没有对齐，
+		/// 尾部需要填充 1 字节。
+		/// @return
+		///
+		uint16_t DataLength() const;
+
+		void SetDataLength(uint16_t value);
+
+		///
+		/// @brief 头部固定为 10 字节。
+		/// @return
+		///
+		static constexpr int32_t HeaderSize()
 		{
-		private:
-			base::Span _span;
-
-		public:
-			DcpHeaderWriter(base::Span const &span);
-
-			base::profinet::DcpServiceIdEnum ServiceId() const;
-			void SetServiceId(base::profinet::DcpServiceIdEnum value);
-
-			base::profinet::DcpServiceTypeEnum ServiceType() const;
-			void SetServiceType(base::profinet::DcpServiceTypeEnum value);
-
-			uint32_t Xid() const;
-			void SetXid(uint32_t value);
-
-			/// @brief 响应延迟。
-			/// @return
-			uint16_t ResponseDelay() const;
-			void SetResponseDelay(uint16_t value);
-
-			/// @brief Blocks 的有效数据的长度，包括填充字节。
-			/// @note 填充是为了 2 字节对齐，每一个 Block 都必须 2 字节对齐，如果没有对齐，
-			/// 尾部需要填充 1 字节。
-			/// @return
-			uint16_t DataLength() const;
-
-			void SetDataLength(uint16_t value);
-
-			/// @brief 头部固定为 10 字节。
-			/// @return
-			static constexpr int32_t HeaderSize()
-			{
-				return 10;
-			}
-		};
-	} // namespace profinet
-} // namespace base
+			return 10;
+		}
+	};
+} // namespace base::profinet
