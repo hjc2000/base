64d170d146c7fc5e542bec429683a4a9176bf3e5
hjc2000 <hjc.666@foxmail.com>
2025-12-29 13:10:32 +0800

    组织命名空间

--- include/base/net/ethernet/LengthOrTypeEnum.h
+++ include/base/net/ethernet/LengthOrTypeEnum.h
@@ -4,62 +4,62 @@
 #include <iostream>
 #include <string>
 
-namespace base
+namespace base::ethernet
 {
-	namespace ethernet
+	///
+	/// @brief 提供以太网中一些上层协议的类型长度字段。
+	///
+	///
+	enum class LengthOrTypeEnum : uint16_t
 	{
 		///
-		/// @brief 提供以太网中一些上层协议的类型长度字段。
+		/// @brief IP 协议的类型长度字段。
 		///
 		///
-		enum class LengthOrTypeEnum : uint16_t
-		{
-			///
-			/// @brief IP 协议的类型长度字段。
-			///
-			///
-			IP = 0x0800,
+		IP = 0x0800,
 
-			///
-			/// @brief ARP 协议的类型长度字段。
-			///
-			///
-			ARP = 0x0806,
+		///
+		/// @brief ARP 协议的类型长度字段。
+		///
+		///
+		ARP = 0x0806,
 
-			///
-			/// @brief VLAN 标签的类型长度字段。
-			///
-			/// @note VLAN 标签也叫 802.1Q 标签。
-			///
-			VlanTag = 0x8100,
+		///
+		/// @brief VLAN 标签的类型长度字段。
+		///
+		/// @note VLAN 标签也叫 802.1Q 标签。
+		///
+		VlanTag = 0x8100,
 
-			///
-			/// @brief PROFINET 帧。
-			///
-			///
-			Profinet = 0x8892,
+		///
+		/// @brief PROFINET 帧。
+		///
+		///
+		Profinet = 0x8892,
 
-			///
-			/// @brief MRP.
-			///
-			///
-			MRP = 0x88E3,
+		///
+		/// @brief MRP.
+		///
+		///
+		MRP = 0x88E3,
 
-			///
-			/// @brief EtherCAT.
-			///
-			///
-			EtherCAT = 0x88A4,
+		///
+		/// @brief EtherCAT.
+		///
+		///
+		EtherCAT = 0x88A4,
 
-			///
-			/// @brief LLDP.
-			///
-			///
-			LLDP = 0x88CC,
-		};
+		///
+		/// @brief LLDP.
+		///
+		///
+		LLDP = 0x88CC,
+	};
 
-	} // namespace ethernet
+} // namespace base::ethernet
 
+namespace base
+{
 	inline std::string to_string(base::ethernet::LengthOrTypeEnum value)
 	{
 		std::string ret;
