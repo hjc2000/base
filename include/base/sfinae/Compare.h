1b06f125538266252edfb65ddaee17dc6f93d343
hjc2000 <hjc.666@foxmail.com>
2025-12-29 13:49:03 +0800

    注释

--- include/base/sfinae/Compare.h
+++ include/base/sfinae/Compare.h
@@ -6,10 +6,6 @@ namespace base
 	///
 	/// @brief 概念：具有相等运算符。
 	///
-	/// @param a
-	/// @param b
-	/// @return
-	///
 	template <typename TLeft, typename TRight>
 	concept has_equal_operator = requires(TLeft a, TRight b) {
 		requires(std::is_same_v<decltype(a == b), bool>);
@@ -17,10 +13,6 @@ namespace base
 
 	///
 	/// @brief 概念：具有小于运算符。
-	///
-	/// @param a
-	/// @param b
-	/// @return
 	///
 	template <typename TLeft, typename TRight>
 	concept has_less_than_operator = requires(TLeft a, TRight b) {
@@ -30,10 +22,6 @@ namespace base
 	///
 	/// @brief 概念：具有大于运算符。
 	///
-	/// @param a
-	/// @param b
-	/// @return
-	///
 	template <typename TLeft, typename TRight>
 	concept has_greater_than_operator = requires(TLeft a, TRight b) {
 		requires(std::is_same_v<decltype(a > b), bool>);
@@ -42,10 +30,6 @@ namespace base
 	///
 	/// @brief 概念：具有小于等于运算符。
 	///
-	/// @param a
-	/// @param b
-	/// @return
-	///
 	template <typename TLeft, typename TRight>
 	concept has_less_than_or_equal_operator = requires(TLeft a, TRight b) {
 		requires(std::is_same_v<decltype(a <= b), bool>);
@@ -53,10 +37,6 @@ namespace base
 
 	///
 	/// @brief 概念：具有大于等于运算符。
-	///
-	/// @param a
-	/// @param b
-	/// @return
 	///
 	template <typename TLeft, typename TRight>
 	concept has_greater_than_or_equal_operator = requires(TLeft a, TRight b) {
