c4b77571fc65ea7cca77408c09cdb8bb347b8a29
hjc2000 <hjc.666@foxmail.com>
2025-12-29 13:31:43 +0800

    内联

--- include/base/net/IPAddress.h
+++ include/base/net/IPAddress.h
@@ -7,6 +7,7 @@
 #include "base/string/String.h"
 #include "base/string/ToHexString.h"
 #include <bit>
+#include <iostream>
 #include <stdexcept>
 
 namespace base
@@ -387,14 +388,39 @@ namespace base
 		}
 	};
 
-#if HAS_THREAD
+	inline void TestIpAddress()
+	{
+		{
+			base::IPAddress ip{
+				std::endian::big,
+				{
+					192,
+					168,
+					1,
+					1,
+				},
+			};
 
-	///
-	/// @brief 测试 IPAddressType
-	///
-	///
-	void TestIpAddress();
+			std::cout << ip << std::endl;
+		}
 
-#endif
+		{
+			base::IPAddress ip{
+				std::endian::big,
+				{
+					0x20, 0x01, // 2001
+					0x0d, 0xb8, // 0db8
+					0x85, 0xa3, // 85a3
+					0x00, 0x00, // 0000
+					0x00, 0x00, // 0000
+					0x8a, 0x2e, // 8a2e
+					0x03, 0x70, // 0370
+					0x73, 0x34  // 7334
+				},
+			};
+
+			std::cout << ip << std::endl;
+		}
+	}
 
 } // namespace base
