314a77f999cf32b3187816cd55d7623077d9b02a
hjc2000 <hjc.666@foxmail.com>
2025-12-29 14:04:46 +0800

    注释

--- include/base/math/DirectionDetecter.h
+++ include/base/math/DirectionDetecter.h
@@ -3,16 +3,21 @@
 
 namespace base
 {
+	///
 	/// @brief 上升阈值
+	///
 	/// @note 本次输入值比锚点增加多少就认为当前方向是上升。
+	///
 	class DirectionDetecter_RisingThreshold
 	{
 	private:
 		double _value = 0;
 
 	public:
+		///
 		/// @brief 上升阈值
 		/// @param value
+		///
 		explicit DirectionDetecter_RisingThreshold(double value)
 		{
 			if (value <= 0)
@@ -29,16 +34,22 @@ namespace base
 		}
 	};
 
+	///
 	/// @brief 下降阈值
+	///
 	/// @note 本次输入值比锚点减少多少就认为当前方向是下降。
+	///
 	class DirectionDetecter_FallenThreshold
 	{
 	private:
 		double _value = 0;
 
 	public:
+		///
 		/// @brief 下降阈值
+		///
 		/// @param value
+		///
 		explicit DirectionDetecter_FallenThreshold(double value)
 		{
 			if (value >= 0)
@@ -55,15 +66,20 @@ namespace base
 		}
 	};
 
+	///
 	/// @brief 初始锚点
+	///
 	class DirectionDetecter_InitialAnchor
 	{
 	private:
 		double _value = 0;
 
 	public:
+		///
 		/// @brief 初始锚点
+		///
 		/// @param value
+		///
 		explicit DirectionDetecter_InitialAnchor(double value)
 		{
 			_value = value;
@@ -75,27 +91,39 @@ namespace base
 		}
 	};
 
+	///
 	/// @brief 当前方向
+	///
 	enum class DirectionDetecter_Direction
 	{
 		Rising,
 		Falling,
 	};
 
+	///
 	/// @brief 方向的变化
+	///
 	enum class DirectionDetecter_DirectionChange
 	{
+		///
 		/// @brief 方向没有变化
+		///
 		None,
 
+		///
 		/// @brief 从上升方向变成了下降方向
+		///
 		FromRisingToFalling,
 
+		///
 		/// @brief 从下降方向变成了上升方向
+		///
 		FromFallingToRising,
 	};
 
+	///
 	/// @brief 方向检测器。
+	///
 	class DirectionDetecter
 	{
 	private:
@@ -108,45 +136,63 @@ namespace base
 		DirectionDetecter_Direction _current_direction = DirectionDetecter_Direction::Falling;
 		DirectionDetecter_Direction _last_direction = DirectionDetecter_Direction::Falling;
 
+		///
 		/// @brief 方向切换瞬间的输入值
+		///
 		double _turning_point = 0;
 		DirectionDetecter_DirectionChange _direction_change = DirectionDetecter_DirectionChange::None;
 
+		///
 		/// @brief 更新 _direction_change 字段。
+		///
 		void UpdateDirectionChangeField();
 
 	public:
+		///
 		/// @brief 构造方向检测器
+		///
 		/// @param rising_threshold 上升阈值。当前输入值比锚点值大多少才会认为当前方向是上升。
 		/// @param fallen_threshold 下降阈值。当前输入值比锚点值小多少才会认为当前方向是下降。
 		/// @param initial_direction 初始方向。
 		/// @param initial_anchor 初始锚点。
+		///
 		DirectionDetecter(base::DirectionDetecter_RisingThreshold const &rising_threshold,
 						  base::DirectionDetecter_FallenThreshold const &fallen_threshold,
 						  DirectionDetecter_Direction initial_direction,
 						  DirectionDetecter_InitialAnchor const &initial_anchor);
 
+		///
 		/// @brief 输入一个值，会与历史比较以检测方向的变化。
+		///
 		/// @param value
+		///
 		void Input(double value);
 
+		///
 		/// @brief 当前位置。
+		///
 		/// @note 没调用过 Input 则是初始位置。调用过 Input 则是最近一次调用 Input 时传入的值。
+		///
 		/// @return
+		///
 		double CurrentPosition() const
 		{
 			return _current_position;
 		}
 
+		///
 		/// @brief 锚点位置。
 		/// @return
+		///
 		double AnchorPoint() const
 		{
 			return _anchor_point;
 		}
 
+		///
 		/// @brief 最近一次转折点。即最近一次方向切换的点。
 		/// @return
+		///
 		double TurningPoint() const
 		{
 			return _turning_point;
@@ -162,8 +208,10 @@ namespace base
 			return _last_direction;
 		}
 
+		///
 		/// @brief 检查最近一次输入后方向的变化情况。
 		/// @return
+		///
 		DirectionDetecter_DirectionChange DirectionChange() const
 		{
 			return _direction_change;
