0308bc66ebd6b1724afd9ed62a79e865de5bac82
hjc2000 <hjc.666@foxmail.com>
2025-12-29 13:29:32 +0800

    内联

--- include/base/net/profinet/RtcPdu.h
+++ include/base/net/profinet/RtcPdu.h
@@ -3,27 +3,48 @@
 #include "base/net/profinet/C_SDU.h"
 #include "base/stream/Span.h"
 
-namespace base
+namespace base::profinet
 {
-	namespace profinet
+	///
+	/// @brief 实时循环帧的协议数据单元。
+	///
+	class RtcPdu
 	{
-		/// @brief 实时循环帧的协议数据单元。
-		class RtcPdu
+	private:
+		base::Span _span;
+
+	public:
+		RtcPdu() = default;
+
+		RtcPdu(base::Span const &span)
 		{
-		private:
-			base::Span _span;
+			_span = span;
+		}
 
-		public:
-			RtcPdu() = default;
-			RtcPdu(base::Span const &span);
+		base::Span const &Span() const
+		{
+			return _span;
+		}
 
-			base::Span const &Span() const;
+		base::profinet::C_SDU C_SDU() const
+		{
+			return base::profinet::C_SDU();
+		}
 
-			base::profinet::C_SDU C_SDU() const;
-			void Set_C_SDU(base::profinet::C_SDU const &value);
+		void Set_C_SDU(base::profinet::C_SDU const &value)
+		{
+			C_SDU().Span().CopyFrom(value.Span());
+		}
 
-			base::profinet::ApduStatus ApduStatus() const;
-			void SetApduStatus(base::profinet::ApduStatus const &value);
-		};
-	} // namespace profinet
-} // namespace base
+		base::profinet::ApduStatus ApduStatus() const
+		{
+			return base::profinet::ApduStatus();
+		}
+
+		void SetApduStatus(base::profinet::ApduStatus const &value)
+		{
+			ApduStatus().Span().CopyFrom(value.Span());
+		}
+	};
+
+} // namespace base::profinet
