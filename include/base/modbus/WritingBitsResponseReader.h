5651db29f2bd3dd11de735e0171dd366a3c9473f
hjc2000 <hjc.666@foxmail.com>
2025-12-29 11:13:23 +0800

    namespace base::modbus

--- include/base/modbus/WritingBitsResponseReader.h
+++ include/base/modbus/WritingBitsResponseReader.h
@@ -4,58 +4,55 @@
 #include "base/stream/ReadOnlySpan.h"
 #include <cstdint>
 
-namespace base
+namespace base::modbus
 {
-	namespace modbus
+	class WritingBitsResponseReader
 	{
-		class WritingBitsResponseReader
+	private:
+		base::modbus::AduReader _adu_reader;
+
+	public:
+		WritingBitsResponseReader(base::ReadOnlySpan const &span)
+			: _adu_reader(span)
 		{
-		private:
-			base::modbus::AduReader _adu_reader;
-
-		public:
-			WritingBitsResponseReader(base::ReadOnlySpan const &span)
-				: _adu_reader(span)
+			base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
+			if (function_code != base::modbus::function_code::WriteBits())
 			{
-				base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
-				if (function_code != base::modbus::function_code::WriteBits())
-				{
-					throw std::runtime_error{CODE_POS_STR + "功能码错误。"};
-				}
+				throw std::runtime_error{CODE_POS_STR + "功能码错误。"};
 			}
+		}
 
-			///
-			/// @brief 站号。
-			///
-			/// @return
-			///
-			uint8_t StationNumber() const
-			{
-				return _adu_reader.StationNumber();
-			}
+		///
+		/// @brief 站号。
+		///
+		/// @return
+		///
+		uint8_t StationNumber() const
+		{
+			return _adu_reader.StationNumber();
+		}
 
-			///
-			/// @brief 要写入的位数据的起始地址。
-			///
-			/// @return
-			///
-			uint16_t StartAddress() const
-			{
-				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{0, 2}];
-				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
-			}
+		///
+		/// @brief 要写入的位数据的起始地址。
+		///
+		/// @return
+		///
+		uint16_t StartAddress() const
+		{
+			base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{0, 2}];
+			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+		}
 
-			///
-			/// @brief 要写入的位数据的个数。
-			///
-			/// @return
-			///
-			uint16_t BitCount() const
-			{
-				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{2, 4}];
-				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
-			}
-		};
+		///
+		/// @brief 要写入的位数据的个数。
+		///
+		/// @return
+		///
+		uint16_t BitCount() const
+		{
+			base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{2, 4}];
+			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+		}
+	};
 
-	} // namespace modbus
-} // namespace base
+} // namespace base::modbus
--- include/base/modbus/WritingRecordsResponseReader.h
+++ include/base/modbus/WritingRecordsResponseReader.h
@@ -8,62 +8,59 @@
 #include <cstdint>
 #include <stdexcept>
 
-namespace base
+namespace base::modbus
 {
-	namespace modbus
+	///
+	/// @brief 写多个记录的响应帧读者。
+	///
+	///
+	class WritingRecordsResponseReader
 	{
-		///
-		/// @brief 写多个记录的响应帧读者。
-		///
-		///
-		class WritingRecordsResponseReader
+	private:
+		base::modbus::AduReader _adu_reader;
+
+	public:
+		WritingRecordsResponseReader(base::ReadOnlySpan const &span)
+			: _adu_reader(span)
 		{
-		private:
-			base::modbus::AduReader _adu_reader;
-
-		public:
-			WritingRecordsResponseReader(base::ReadOnlySpan const &span)
-				: _adu_reader(span)
+			base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
+			if (function_code != base::modbus::function_code::WriteRecords())
 			{
-				base::modbus::FunctionCode function_code = _adu_reader.FunctionCode();
-				if (function_code != base::modbus::function_code::WriteRecords())
-				{
-					throw std::runtime_error{CODE_POS_STR + "错误的功能码。"};
-				}
+				throw std::runtime_error{CODE_POS_STR + "错误的功能码。"};
 			}
+		}
 
-			///
-			/// @brief 站号。
-			///
-			/// @return
-			///
-			uint8_t StationNumber() const
-			{
-				return _adu_reader.StationNumber();
-			}
+		///
+		/// @brief 站号。
+		///
+		/// @return
+		///
+		uint8_t StationNumber() const
+		{
+			return _adu_reader.StationNumber();
+		}
 
-			///
-			/// @brief 起始地址。
-			///
-			/// @return
-			///
-			uint16_t StartAddress() const
-			{
-				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{0, 2}];
-				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
-			}
+		///
+		/// @brief 起始地址。
+		///
+		/// @return
+		///
+		uint16_t StartAddress() const
+		{
+			base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{0, 2}];
+			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+		}
 
-			///
-			/// @brief 记录数。
-			///
-			/// @return
-			///
-			uint16_t RecordCount() const
-			{
-				base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{2, 4}];
-				return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
-			}
-		};
+		///
+		/// @brief 记录数。
+		///
+		/// @return
+		///
+		uint16_t RecordCount() const
+		{
+			base::ReadOnlySpan span = _adu_reader.DataSpan()[base::Range{2, 4}];
+			return base::big_endian_remote_converter.FromBytes<uint16_t>(span);
+		}
+	};
 
-	} // namespace modbus
-} // namespace base
+} // namespace base::modbus
