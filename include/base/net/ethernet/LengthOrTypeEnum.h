4efcb58fcdd05c11bfcfeb7a8e9a63108f99688d
hjc2000 <hjc.666@foxmail.com>
2025-12-29 13:09:08 +0800

    内联

--- include/base/net/ethernet/LengthOrTypeEnum.h
+++ include/base/net/ethernet/LengthOrTypeEnum.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "base/string/ToHexString.h"
 #include <cstdint>
 #include <iostream>
 #include <string>
@@ -56,15 +57,60 @@ namespace base
 			///
 			LLDP = 0x88CC,
 		};
+
 	} // namespace ethernet
 
-	///
-	/// @brief 将 base::ethernet::LengthOrTypeEnum 转换为字符串。
-	///
-	/// @param value
-	/// @return std::string
-	///
-	std::string to_string(base::ethernet::LengthOrTypeEnum value);
+	inline std::string to_string(base::ethernet::LengthOrTypeEnum value)
+	{
+		std::string ret;
+		switch (value)
+		{
+		case base::ethernet::LengthOrTypeEnum::IP:
+			{
+				ret = "IP";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::ARP:
+			{
+				ret = "ARP";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::VlanTag:
+			{
+				ret = "VlanTag";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::Profinet:
+			{
+				ret = "Profinet";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::MRP:
+			{
+				ret = "MRP";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::EtherCAT:
+			{
+				ret = "EtherCAT";
+				break;
+			}
+		case base::ethernet::LengthOrTypeEnum::LLDP:
+			{
+				ret = "LLDP";
+				break;
+			}
+		default:
+			{
+				ret = "未知";
+				break;
+			}
+		}
+
+		ret += ": " + base::ToHexString(static_cast<uint16_t>(value));
+		ret += " (十进制: " + std::to_string(static_cast<uint16_t>(value)) + ")";
+		return ret;
+	}
 
 } // namespace base
 
@@ -73,6 +119,11 @@ namespace base
 ///
 /// @param os
 /// @param length_type
-/// @return std::ostream&
 ///
-std::ostream &operator<<(std::ostream &os, base::ethernet::LengthOrTypeEnum length_type);
+/// @return
+///
+inline std::ostream &operator<<(std::ostream &os, base::ethernet::LengthOrTypeEnum length_type)
+{
+	os << base::to_string(length_type);
+	return os;
+}
