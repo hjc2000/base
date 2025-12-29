66bd6da89c83a21ffefda97b3e86be70fe833c4b
hjc2000 <hjc.666@foxmail.com>
2025-12-29 11:17:06 +0800

    注释

--- include/base/net/profinet/dcp/DcpHeaderReader.h
+++ include/base/net/profinet/dcp/DcpHeaderReader.h
@@ -55,8 +55,10 @@ namespace base::profinet
 
 		///
 		/// @brief Blocks 的有效数据的长度，包括填充字节。
+		///
 		/// @note 填充是为了 2 字节对齐，每一个 Block 都必须 2 字节对齐，如果没有对齐，
 		/// 尾部需要填充 1 字节。
+		///
 		/// @return
 		///
 		uint16_t DataLength() const
@@ -67,6 +69,7 @@ namespace base::profinet
 
 		///
 		/// @brief 头部固定为 10 字节。
+		///
 		/// @return
 		///
 		static constexpr int32_t HeaderSize()
