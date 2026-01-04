8f1012cd445b0efb25e0420b56e370e1a77ce109
hjc2000 <hjc.666@foxmail.com>
2026-01-04 10:39:05 +0800

    ``` refactor(time): 调整命名空间结构
    将命名空间从 base::time 重新组织为嵌套的 base::time 命名空间，
    移除了多余的命名空间嵌套层级，保持代码结构更清晰。
    ```

--- include/base/time/time.h
+++ include/base/time/time.h
@@ -2,43 +2,40 @@
 #include "base/time/TimePointSinceEpoch.h"
 #include "DateTime.h"
 
-namespace base
+namespace base::time
 {
-	namespace time
+	///
+	/// @brief 获取当前时刻。
+	///
+	/// @return
+	///
+	base::TimePointSinceEpoch Now();
+
+	///
+	/// @brief 获取当前的日期时间。
+	///
+	/// @return
+	///
+	inline base::DateTime NowDateTime()
 	{
-		///
-		/// @brief 获取当前时刻。
-		///
-		/// @return
-		///
-		base::TimePointSinceEpoch Now();
+		return base::DateTime{Now()};
+	}
 
-		///
-		/// @brief 获取当前的日期时间。
-		///
-		/// @return
-		///
-		inline base::DateTime NowDateTime()
-		{
-			return base::DateTime{Now()};
-		}
+	///
+	/// @brief 获取稳定时钟的当前时刻。
+	///
+	/// @return
+	///
+	base::TimePointSinceEpoch SteadyClockNow();
 
-		///
-		/// @brief 获取稳定时钟的当前时刻。
-		///
-		/// @return
-		///
-		base::TimePointSinceEpoch SteadyClockNow();
+	///
+	/// @brief 设置系统的时间。
+	///
+	/// @note 需要管理员权限。
+	/// @note 操作系统自带时区功能，所以设置时间时不要手动处理时区带来的时间偏移。
+	///
+	/// @param value
+	///
+	void SetRealTime(base::TimePointSinceEpoch const &value);
 
-		///
-		/// @brief 设置系统的时间。
-		///
-		/// @note 需要管理员权限。
-		/// @note 操作系统自带时区功能，所以设置时间时不要手动处理时区带来的时间偏移。
-		///
-		/// @param value
-		///
-		void SetRealTime(base::TimePointSinceEpoch const &value);
-
-	} // namespace time
-} // namespace base
+} // namespace base::time
