f8ff91e4d8afb152a3f6d99eee9fa5a5a4044a83
hjc2000 <hjc.666@foxmail.com>
2026-01-26 09:48:36 +0800

    重命名

--- test/base/test/TestDateTime.h
+++ test/base/test/TestDateTime.h
@@ -11,18 +11,18 @@ namespace base::test
 	{
 		base::TimePointSinceEpoch now_time_point = base::time::Now();
 
-		base::DateTime now{
+		base::DateTime now_date_time{
 			base::UtcHourOffset{8},
 			now_time_point,
 		};
 
-		base::DateTimeStringBuilder dsb = now.LocalDateTimeStringBuilder();
-		dsb.SetYearMonthDaySeparator('/');
+		base::DateTimeStringBuilder date_time_string_builder = now_date_time.LocalDateTimeStringBuilder();
+		date_time_string_builder.SetYearMonthDaySeparator('/');
 
-		std::cout << dsb << std::endl;
-		std::cout << base::to_string(now_time_point) << std::endl;
 		std::cout << static_cast<std::chrono::nanoseconds>(now_time_point) << std::endl;
-		std::cout << static_cast<std::chrono::nanoseconds>(now.TimePointSinceEpoch()) << std::endl;
+		std::cout << static_cast<std::chrono::nanoseconds>(now_date_time.TimePointSinceEpoch()) << std::endl;
+		std::cout << date_time_string_builder << std::endl;
+		std::cout << base::to_string(now_time_point) << std::endl;
 	}
 
 } // namespace base::test
