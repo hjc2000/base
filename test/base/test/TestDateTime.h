a87538b43f1c4b33c8f3c3a9ea3fd36a5766793e
hjc2000 <hjc.666@foxmail.com>
2026-01-26 09:31:28 +0800

    TestDateTime内联

--- test/base/test/TestDateTime.h
+++ test/base/test/TestDateTime.h
@@ -1,7 +1,28 @@
 #pragma once
+#include "base/time/convert.h"
+#include "base/time/time.h"
+#include "base/time/TimePointSinceEpoch.h"
+#include <chrono>
+#include <iostream>
 
 namespace base::test
 {
-	void TestDateTime();
+	inline void TestDateTime()
+	{
+		base::TimePointSinceEpoch now_time_point = base::time::Now();
+
+		base::DateTime now{
+			base::UtcHourOffset{8},
+			now_time_point,
+		};
+
+		base::DateTimeStringBuilder dsb = now.LocalDateTimeStringBuilder();
+		dsb.SetYearMonthDaySeparator('/');
+
+		std::cout << dsb << std::endl;
+		std::cout << base::to_string(now_time_point) << std::endl;
+		std::cout << static_cast<std::chrono::nanoseconds>(now_time_point) << std::endl;
+		std::cout << static_cast<std::chrono::nanoseconds>(now.TimePointSinceEpoch()) << std::endl;
+	}
 
 } // namespace base::test
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,9 @@
+#include "base/test/TestDateTime.h"
 #include <cstdint>  // IWYU pragma: keep
 #include <iostream> // IWYU pragma: keep
 
 int main()
 {
-
+	base::test::TestDateTime();
 	return 0;
 }
