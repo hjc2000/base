d19bb26ecdec1dce84d2ea9766479c5280a79345
hjc2000 <hjc.666@foxmail.com>
2026-01-26 09:50:02 +0800

    注释

--- include/base/time/time.cpp
+++ include/base/time/time.cpp
@@ -1,4 +1,4 @@
-#include "time.h"
+#include "time.h" // IWYU pragma: keep
 
 #if HAS_THREAD
 
@@ -20,6 +20,7 @@ void base::time::SetRealTime(base::TimePointSinceEpoch const &value)
 
 	// 设置系统时间
 	int result = clock_settime(CLOCK_REALTIME, &ts);
+
 	if (result != 0)
 	{
 		throw std::runtime_error{"设置系统时间失败，请检查是否是管理员权限。"};
