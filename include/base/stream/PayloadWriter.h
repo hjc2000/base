8754e5d4a5ab534ef59fbcefe8433b2f79b5a6e2
hjc2000 <hjc.666@foxmail.com>
2026-01-29 11:08:50 +0800

    ``` feat(base): 增强PayloadWriter功能
    - 添加_position成员变量用于跟踪写入位置
    - 实现Position()方法获取当前写入位置
    - 实现ResetPosition()方法重置写入位置
    - 添加WritePayload方法支持字节流写入
    - 添加模板WritePayload方法支持类型值写入并自动进行字节序转换
    - 引入AutoBitConverter用于处理不同字节序的转换
    - 添加必要的头文件包含<cstdint>
    ```

--- include/base/stream/PayloadWriter.h
+++ include/base/stream/PayloadWriter.h
@@ -1,5 +1,7 @@
 #pragma once
+#include "base/bit/AutoBitConverter.h"
 #include "base/stream/Span.h"
+#include <cstdint>
 
 namespace base
 {
@@ -7,11 +9,62 @@ namespace base
 	{
 	private:
 		base::Span _span{};
+		int64_t _position = 0;
 
 	public:
 		PayloadWriter(base::Span const &span)
 			: _span{span}
 		{
+		}
+
+		int64_t Position() const
+		{
+			return _position;
+		}
+
+		void ResetPosition()
+		{
+			_position = 0;
+		}
+
+		///
+		/// @brief 写入载荷。
+		///
+		/// @param span
+		///
+		void WritePayload(base::ReadOnlySpan const &span)
+		{
+			base::Range range{
+				_position,
+				_position + span.Size(),
+			};
+
+			base::Span span_to_write = _span[range];
+			span_to_write.CopyFrom(span);
+
+			_position += span_to_write.Size();
+		}
+
+		///
+		/// @brief 写入载荷。
+		///
+		/// @param value
+		/// @param remote_endian
+		///
+		template <typename ValueType>
+		void WritePayload(ValueType value, std::endian remote_endian)
+		{
+			base::Range range{
+				_position,
+				_position + static_cast<int64_t>(sizeof(ValueType)),
+			};
+
+			base::Span span_to_write = _span[range];
+
+			base::AutoBitConverter conveter{remote_endian};
+			conveter.GetBytes(value, span_to_write);
+
+			_position += span_to_write.Size();
 		}
 	};
 
