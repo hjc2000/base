5acad4209c9424f116c769c8e11911c3490327bf
hjc2000 <hjc.666@foxmail.com>
2026-02-06 13:49:17 +0800

    MasterSlaveFlipFlop

--- include/base/MasterSlaveFlipFlop.h
+++ include/base/MasterSlaveFlipFlop.h
@@ -3,14 +3,14 @@
 namespace base
 {
 	///
-	/// @brief 同步触发器。
+	/// @brief 主从触发器。
 	///
 	/// 输入端可以一直更新，不会影响到输出端的数据。调用 UpdateOutput 将
 	/// 输入端的数据更新到输出端。直到再次更新之前，调用 GetOutput 得到的一直是上次
 	/// 更新的结果。
 	///
 	template <typename T>
-	class Trigger final
+	class MasterSlaveFlipFlop final
 	{
 	private:
 		T _output{};
