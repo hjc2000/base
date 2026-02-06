2d10f64165eeb2f0929e6d46519f1f601119c90b
hjc2000 <hjc.666@foxmail.com>
2026-02-06 13:42:03 +0800

    注释

--- include/base/Trigger.h
+++ include/base/Trigger.h
@@ -2,11 +2,13 @@
 
 namespace base
 {
-	/// @brief 输入端可以一直更新，不会影响到输出端的数据。调用 UpdateOutput 将
+	///
+	/// @brief 同步触发器。
+	///
+	/// 输入端可以一直更新，不会影响到输出端的数据。调用 UpdateOutput 将
 	/// 输入端的数据更新到输出端。直到再次更新之前，调用 GetOutput 得到的一直是上次
 	/// 更新的结果。
 	///
-	/// @tparam T
 	template <typename T>
 	class Trigger final
 	{
@@ -14,7 +16,9 @@ namespace base
 		T _output{};
 		T _input{};
 
+		///
 		/// @brief 从来没有更新过输出
+		///
 		bool _have_not_update_output = true;
 
 	public:
@@ -39,4 +43,5 @@ namespace base
 			return _have_not_update_output;
 		}
 	};
+
 } // namespace base
