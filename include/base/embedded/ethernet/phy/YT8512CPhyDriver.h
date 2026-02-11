905d4773d0723285e3c17747b9cfe65d43d4627c
hjc2000 <hjc.666@foxmail.com>
2026-02-11 11:07:06 +0800

    ``` refactor(ethernet): 重构YT8512CPhyDriver类结构
    将YT8512CPhyDriver的实现从cpp文件移至头文件中，采用内联实
    现方式。同时调整命名空间结构，从嵌套命名空间改为直接使用
    base::ethernet命名空间。添加了必要的注释文档，并修正了代码
    组织结构以提高可维护性。
    ```

--- include/base/embedded/ethernet/phy/YT8512CPhyDriver.h
+++ include/base/embedded/ethernet/phy/YT8512CPhyDriver.h
@@ -2,30 +2,69 @@
 #include "base/embedded/ethernet/EthernetController.h"
 #include "base/embedded/ethernet/parameter.h"
 #include "base/embedded/ethernet/phy/IPhyDriver.h"
+#include "base/string/define.h"
+#include "base/unit/Mbps.h"
 
-namespace base
+namespace base::ethernet
 {
-	namespace ethernet
+	class YT8512CPhyDriver :
+		public base::ethernet::IPhyDriver
 	{
-		class YT8512CPhyDriver :
-			public base::ethernet::IPhyDriver
+	private:
+		base::ethernet::EthernetController _ethernet_controller;
+
+	public:
+		YT8512CPhyDriver(base::ethernet::EthernetController const &controller)
+			: _ethernet_controller(controller)
 		{
-		private:
-			base::ethernet::EthernetController _ethernet_controller;
+		}
 
-		public:
-			YT8512CPhyDriver(base::ethernet::EthernetController const &controller);
+		virtual base::ethernet::EthernetController &EthernetController() override
+		{
+			return _ethernet_controller;
+		}
 
-			virtual base::ethernet::EthernetController &EthernetController() override;
+		/// @brief 获取此网口的双工模式。
+		/// @return
+		virtual base::ethernet::DuplexMode DuplexMode() override
+		{
+			uint32_t register_value = ReadRegister(0x11);
+			uint32_t const mask = 0b1 << 13;
+			uint32_t duplex_register = (register_value & mask) >> 13;
 
-			/// @brief 获取此网口的双工模式。
-			/// @return
-			virtual base::ethernet::DuplexMode DuplexMode() override;
+			if (duplex_register)
+			{
+				return base::ethernet::DuplexMode::FullDuplex;
+			}
 
-			/// @brief 获取此网口的速度。
-			/// @return
-			virtual base::unit::Mbps Speed() override;
-		};
+			return base::ethernet::DuplexMode::HalfDuplex;
+		}
 
-	} // namespace ethernet
-} // namespace base
+		/// @brief 获取此网口的速度。
+		/// @return
+		virtual base::unit::Mbps Speed() override
+		{
+			uint32_t register_value = ReadRegister(0x11);
+			uint32_t const mask = 0b11 << 14;
+			uint32_t duplex_register = (register_value & mask) >> 14;
+
+			if (duplex_register == 0b00)
+			{
+				return base::unit::Mbps{10};
+			}
+			else if (duplex_register == 0b01)
+			{
+				return base::unit::Mbps{100};
+			}
+			else if (duplex_register == 0b01)
+			{
+				return base::unit::Mbps{1000};
+			}
+			else
+			{
+				throw std::runtime_error{CODE_POS_STR + "读取连接速率寄存器时读取到保留值。"};
+			}
+		}
+	};
+
+} // namespace base::ethernet
