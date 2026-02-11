62396898fb394254d6fe357b189ebef69a8d26b7
hjc2000 <hjc.666@foxmail.com>
2026-02-11 11:33:10 +0800

    ``` refactor(task): 调整delay.h中的命名空间结构
    将嵌套的命名空间 base::task 改为扁平化的 base::task，
    移除了多余的命名空间声明层级，使代码结构更加清晰简洁。
    ```

--- include/base/task/delay.h
+++ include/base/task/delay.h
@@ -4,61 +4,58 @@
 #include "base/unit/Second.h"
 #include <chrono>
 
-namespace base
+namespace base::task
 {
-	namespace task
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	void Delay(std::chrono::seconds const &time);
+
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	void Delay(std::chrono::milliseconds const &time);
+
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	void Delay(std::chrono::microseconds const &time);
+
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	void Delay(std::chrono::nanoseconds const &time);
+
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	IMPLEMENTED
+	inline void Delay(base::unit::Nanosecond const &time)
 	{
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		void Delay(std::chrono::seconds const &time);
+		std::chrono::nanoseconds ns = static_cast<std::chrono::nanoseconds>(time);
+		Delay(ns);
+	}
 
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		void Delay(std::chrono::milliseconds const &time);
+	///
+	/// @brief 延时。
+	///
+	/// @param time
+	///
+	IMPLEMENTED
+	inline void Delay(base::unit::Second const &time)
+	{
+		std::chrono::nanoseconds ns = static_cast<std::chrono::nanoseconds>(time);
+		Delay(ns);
+	}
 
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		void Delay(std::chrono::microseconds const &time);
-
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		void Delay(std::chrono::nanoseconds const &time);
-
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		IMPLEMENTED
-		inline void Delay(base::unit::Nanosecond const &time)
-		{
-			std::chrono::nanoseconds ns = static_cast<std::chrono::nanoseconds>(time);
-			Delay(ns);
-		}
-
-		///
-		/// @brief 延时。
-		///
-		/// @param time
-		///
-		IMPLEMENTED
-		inline void Delay(base::unit::Second const &time)
-		{
-			std::chrono::nanoseconds ns = static_cast<std::chrono::nanoseconds>(time);
-			Delay(ns);
-		}
-
-	} // namespace task
-} // namespace base
+} // namespace base::task
