c7b9f67d294818793f6f734ebbcb9178244745cc
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 02:20:25 +0800

    ``` refactor(interrupt): 重构中断模块为命名空间并改进类设计
    - 将base命名空间改为base::interrupt命名空间
    - 移除AS_STATIC_CLASS宏，将interrupt类重命名为InterruptContext
    - 将静态方法转换为实例方法，提高封装性
    - 添加友元函数支持递归中断控制
    - 改进递归中断逻辑实现
    - 更新全局中断保护类的实现方式
    ```

--- include/base/embedded/interrupt/interrupt.h
+++ include/base/embedded/interrupt/interrupt.h
@@ -1,13 +1,16 @@
 #pragma once
-#include "base/define.h"
 #include <cstdint>
 
-namespace base
+namespace base::interrupt
 {
-	class interrupt
+	void disable_global_interrupt_recursive() noexcept;
+	void enable_global_interrupt_recursive() noexcept;
+
+	class InterruptContext
 	{
 	private:
-		AS_STATIC_CLASS(interrupt);
+		friend void disable_global_interrupt_recursive() noexcept;
+		friend void enable_global_interrupt_recursive() noexcept;
 
 		inline static int32_t volatile _global_interrupt_disable_times = 0;
 
@@ -15,83 +18,84 @@ namespace base
 		/// @brief 禁用全局中断。
 		///
 		///
-		static void disable_global_interrupt() noexcept;
+		void disable_global_interrupt() noexcept;
 
 		///
 		/// @brief 使能全局中断。
 		///
 		///
-		static void enable_global_interrupt() noexcept;
-
-	public:
-		///
-		/// @brief 禁用指定的中断。
-		///
-		/// @param irq 中断号。
-		///
-		static void disable_interrupt(int32_t irq) noexcept;
-
-		///
-		/// @brief 使能指定的中断。不指定优先级，保持默认优先级。
-		///
-		/// @param irq 中断号。
-		///
-		static void enable_interrupt(int32_t irq) noexcept;
-
-		///
-		/// @brief 使能指定的中断并指定优先级。
-		///
-		/// @param irq 中断号。
-		/// @param priority 优先级。
-		///
-		static void enable_interrupt(int32_t irq, int32_t priority) noexcept;
-
-		/* #region 递归禁用，使能全局中断 */
-
-		///
-		/// @brief 递归地禁用全局中断。
-		///
-		///
-		static void disable_global_interrupt_recursive() noexcept
-		{
-			disable_global_interrupt();
-			_global_interrupt_disable_times = _global_interrupt_disable_times + 1;
-		}
-
-		///
-		/// @brief 递归地使能全局中断。
-		///
-		///
-		static void enable_global_interrupt_recursive() noexcept
-		{
-			disable_global_interrupt();
-			_global_interrupt_disable_times = _global_interrupt_disable_times - 1;
-
-			if (_global_interrupt_disable_times <= 0)
-			{
-				_global_interrupt_disable_times = 0;
-				enable_global_interrupt();
-			}
-		}
-
-		///
-		/// @brief 构造时禁用全局中断，析构时使能全局中断。
-		///
-		class GlobalInterruptionGuard
-		{
-		public:
-			GlobalInterruptionGuard()
-			{
-				disable_global_interrupt_recursive();
-			}
-
-			~GlobalInterruptionGuard()
-			{
-				enable_global_interrupt_recursive();
-			}
-		};
-
-		/* #endregion */
+		void enable_global_interrupt() noexcept;
 	};
 
-} // namespace base
+	base::interrupt::InterruptContext &context();
+
+	///
+	/// @brief 禁用指定的中断。
+	///
+	/// @param irq 中断号。
+	///
+	void disable_interrupt(int32_t irq) noexcept;
+
+	///
+	/// @brief 使能指定的中断。不指定优先级，保持默认优先级。
+	///
+	/// @param irq 中断号。
+	///
+	void enable_interrupt(int32_t irq) noexcept;
+
+	///
+	/// @brief 使能指定的中断并指定优先级。
+	///
+	/// @param irq 中断号。
+	/// @param priority 优先级。
+	///
+	void enable_interrupt(int32_t irq, int32_t priority) noexcept;
+
+	/* #region 递归禁用，使能全局中断 */
+
+	///
+	/// @brief 递归地禁用全局中断。
+	///
+	///
+	inline void disable_global_interrupt_recursive() noexcept
+	{
+		context().disable_global_interrupt();
+		context()._global_interrupt_disable_times = context()._global_interrupt_disable_times + 1;
+	}
+
+	///
+	/// @brief 递归地使能全局中断。
+	///
+	///
+	inline void enable_global_interrupt_recursive() noexcept
+	{
+		context().disable_global_interrupt();
+		context()._global_interrupt_disable_times = context()._global_interrupt_disable_times - 1;
+
+		if (context()._global_interrupt_disable_times <= 0)
+		{
+			context()._global_interrupt_disable_times = 0;
+			context().enable_global_interrupt();
+		}
+	}
+
+	///
+	/// @brief 构造时禁用全局中断，析构时使能全局中断。
+	///
+	class GlobalInterruptionGuard
+	{
+	public:
+		GlobalInterruptionGuard()
+		{
+			disable_global_interrupt_recursive();
+		}
+
+		~GlobalInterruptionGuard()
+		{
+			enable_global_interrupt_recursive();
+		}
+	};
+
+	/* #endregion */
+
+} // namespace base::interrupt
