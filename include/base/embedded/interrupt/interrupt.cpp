32e70167c00d44f7a757481b371754810a7c8faf
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 01:16:43 +0800

    ``` feat(interrupt): 添加递归中断控制功能
    - 实现了递归禁用和使能全局中断的功能函数
    - 添加了GlobalInterruptionGuard RAII类用于自动管理中断状态
    - 在无线程环境下提供中断嵌套保护机制
    - 增加了全局中断禁用计数器来跟踪递归调用次数
    ```

--- include/base/embedded/interrupt/interrupt.cpp
+++ include/base/embedded/interrupt/interrupt.cpp
@@ -1,1 +1,28 @@
 #include "interrupt.h" // IWYU pragma: keep
+
+#if !HAS_THREAD
+
+namespace
+{
+	int32_t volatile _global_interrupt_disable_times = 0;
+}
+
+void base::interrupt::disable_global_interrupt_recursive() noexcept
+{
+	disable_global_interrupt();
+	_global_interrupt_disable_times = _global_interrupt_disable_times + 1;
+}
+
+void base::interrupt::enable_global_interrupt_recursive() noexcept
+{
+	disable_global_interrupt();
+	_global_interrupt_disable_times = _global_interrupt_disable_times - 1;
+
+	if (_global_interrupt_disable_times <= 0)
+	{
+		_global_interrupt_disable_times = 0;
+		enable_global_interrupt();
+	}
+}
+
+#endif // HAS_THREAD
