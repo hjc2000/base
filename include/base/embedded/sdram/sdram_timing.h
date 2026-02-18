921196a7316261891a90dd76a94441d61a4e2b85
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 15:51:57 +0800

    内联

--- include/base/embedded/sdram/sdram_timing.h
+++ include/base/embedded/sdram/sdram_timing.h
@@ -352,7 +352,26 @@ namespace base::sdram
 		/// @brief 序列化为 json
 		/// @return
 		///
-		virtual base::Json ToJson() const override;
+		virtual base::Json ToJson() const override
+		{
+			base::Json root{
+				{"row_count", row_count()},
+				{"clock_frequency", clock_frequency().ToString()},
+				{"clock_cycle", clock_cycle().ToString()},
+				{"t_rsc_clock_cycle_count", t_rsc_clock_cycle_count()},
+				{"t_xsr_clock_cycle_count", t_xsr_clock_cycle_count()},
+				{"t_ras_clock_cycle_count", t_ras_clock_cycle_count()},
+				{"t_rc_clock_cycle_count", t_rc_clock_cycle_count()},
+				{"t_wr_clock_cycle_count", t_wr_clock_cycle_count()},
+				{"t_rp_clock_cycle_count", t_rp_clock_cycle_count()},
+				{"t_rcd_clock_cycle_count", t_rcd_clock_cycle_count()},
+				{"t_ref_clock_cycle_count", t_ref_clock_cycle_count()},
+				{"auto_refresh_command_clock_cycle_count", auto_refresh_command_clock_cycle_count()},
+				{"cas_latency", cas_latency()},
+			};
+
+			return root;
+		}
 	};
 
 } // namespace base::sdram
