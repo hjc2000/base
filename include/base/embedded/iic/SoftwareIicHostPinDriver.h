f49d39a37b93974e2ec8d46675d20d0ba6b95d42
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 15:19:17 +0800

    ``` refactor(iic): 重构SoftwareIicHostPinDriver头文件结构
    - 将命名空间从 base::iic 调整为 base::iic
    - 添加了详细的Doxygen注释文档
    - 完善了模板类和特化类的虚函数实现
    - 优化了GPIO引脚初始化逻辑
    - 统一了代码格式和结构
    ```

--- include/base/embedded/iic/SoftwareIicHostPinDriver.h
+++ include/base/embedded/iic/SoftwareIicHostPinDriver.h
@@ -1,160 +1,156 @@
 #pragma once
-
 #include "base/embedded/gpio/gpio_parameter.h"
 #include "base/embedded/gpio/GpioPin.h"
 
-namespace base
+namespace base::iic
 {
-	namespace iic
+	/* #region ISoftwareIicHostPinDriver */
+
+	class ISoftwareIicHostPinDriver
 	{
-		/* #region ISoftwareIicHostPinDriver */
+	public:
+		///
+		/// @brief 写 SCL 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSCL(bool value) = 0;
 
-		class ISoftwareIicHostPinDriver
+		///
+		/// @brief 更改 SDA 引脚的 IO 方向。
+		///
+		/// @warning 改变方向和读写是分开的。不要自作聪明，在 WriteSDA 的实现中将 SDA
+		/// 引脚方向切换为输出，在 ReadSDA 的实现中将 SDA 引脚的方向切换为输入。
+		///
+		/// @param value
+		///
+		virtual void ChangeSDADirection(base::gpio::Direction value) = 0;
+
+		///
+		/// @brief 写 SDA 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSDA(bool value) = 0;
+
+		///
+		/// @brief 读 SDA 引脚的值。
+		///
+		/// @return
+		///
+		virtual bool ReadSDA() const = 0;
+	};
+
+	template <typename GpioType>
+	class SoftwareIicHostPinDriver :
+		public base::iic::ISoftwareIicHostPinDriver
+	{
+	public:
+		///
+		/// @brief 写 SCL 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSCL(bool value) override = 0;
+
+		///
+		/// @brief 更改 SDA 引脚的 IO 方向。
+		///
+		/// @param value
+		///
+		virtual void ChangeSDADirection(base::gpio::Direction value) override = 0;
+
+		///
+		/// @brief 写 SDA 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSDA(bool value) override = 0;
+
+		///
+		/// @brief 读 SDA 引脚的值。
+		///
+		/// @return
+		///
+		virtual bool ReadSDA() const override = 0;
+	};
+
+	/* #endregion */
+
+	/* #region 针对 base::gpio::GpioPin 特化 */
+
+	template <>
+	class SoftwareIicHostPinDriver<base::gpio::GpioPin> :
+		public base::iic::ISoftwareIicHostPinDriver
+	{
+	private:
+		base::gpio::GpioPin _scl_pin;
+		base::gpio::GpioPin _sda_pin;
+
+	public:
+		SoftwareIicHostPinDriver(base::gpio::GpioPin const &scl_pin,
+								 base::gpio::GpioPin const &sda_pin)
+			: _scl_pin(scl_pin),
+			  _sda_pin(sda_pin)
 		{
-		public:
-			///
-			/// @brief 写 SCL 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSCL(bool value) = 0;
+			_scl_pin.InitializeAsOutputMode();
+			_sda_pin.InitializeAsOutputMode();
 
-			///
-			/// @brief 更改 SDA 引脚的 IO 方向。
-			///
-			/// @warning 改变方向和读写是分开的。不要自作聪明，在 WriteSDA 的实现中将 SDA
-			/// 引脚方向切换为输出，在 ReadSDA 的实现中将 SDA 引脚的方向切换为输入。
-			///
-			/// @param value
-			///
-			virtual void ChangeSDADirection(base::gpio::Direction value) = 0;
+			_scl_pin.WritePin(true);
+			_sda_pin.WritePin(true);
+		}
 
-			///
-			/// @brief 写 SDA 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSDA(bool value) = 0;
-
-			///
-			/// @brief 读 SDA 引脚的值。
-			///
-			/// @return
-			///
-			virtual bool ReadSDA() const = 0;
-		};
-
-		template <typename GpioType>
-		class SoftwareIicHostPinDriver :
-			public base::iic::ISoftwareIicHostPinDriver
+		///
+		/// @brief 写 SCL 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSCL(bool value) final override
 		{
-		public:
-			///
-			/// @brief 写 SCL 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSCL(bool value) override = 0;
+			_scl_pin.WritePin(value);
+		}
 
-			///
-			/// @brief 更改 SDA 引脚的 IO 方向。
-			///
-			/// @param value
-			///
-			virtual void ChangeSDADirection(base::gpio::Direction value) override = 0;
-
-			///
-			/// @brief 写 SDA 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSDA(bool value) override = 0;
-
-			///
-			/// @brief 读 SDA 引脚的值。
-			///
-			/// @return
-			///
-			virtual bool ReadSDA() const override = 0;
-		};
-
-		/* #endregion */
-
-		/* #region 针对 base::gpio::GpioPin 特化 */
-
-		template <>
-		class SoftwareIicHostPinDriver<base::gpio::GpioPin> :
-			public base::iic::ISoftwareIicHostPinDriver
+		///
+		/// @brief 更改 SDA 引脚的 IO 方向。
+		///
+		/// @warning 改变方向和读写是分开的。不要自作聪明，在 WriteSDA 的实现中将 SDA
+		/// 引脚方向切换为输出，在 ReadSDA 的实现中将 SDA 引脚的方向切换为输入。
+		///
+		/// @param value
+		///
+		virtual void ChangeSDADirection(base::gpio::Direction value) final override
 		{
-		private:
-			base::gpio::GpioPin _scl_pin;
-			base::gpio::GpioPin _sda_pin;
-
-		public:
-			SoftwareIicHostPinDriver(base::gpio::GpioPin const &scl_pin,
-									 base::gpio::GpioPin const &sda_pin)
-				: _scl_pin(scl_pin),
-				  _sda_pin(sda_pin)
+			if (value == base::gpio::Direction::Input)
 			{
-				_scl_pin.InitializeAsOutputMode();
+				_sda_pin.InitializeAsInputMode();
+			}
+			else
+			{
 				_sda_pin.InitializeAsOutputMode();
-
-				_scl_pin.WritePin(true);
-				_sda_pin.WritePin(true);
 			}
+		}
 
-			///
-			/// @brief 写 SCL 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSCL(bool value) final override
-			{
-				_scl_pin.WritePin(value);
-			}
+		///
+		/// @brief 写 SDA 引脚的值。
+		///
+		/// @param value
+		///
+		virtual void WriteSDA(bool value) final override
+		{
+			_sda_pin.WritePin(value);
+		}
 
-			///
-			/// @brief 更改 SDA 引脚的 IO 方向。
-			///
-			/// @warning 改变方向和读写是分开的。不要自作聪明，在 WriteSDA 的实现中将 SDA
-			/// 引脚方向切换为输出，在 ReadSDA 的实现中将 SDA 引脚的方向切换为输入。
-			///
-			/// @param value
-			///
-			virtual void ChangeSDADirection(base::gpio::Direction value) final override
-			{
-				if (value == base::gpio::Direction::Input)
-				{
-					_sda_pin.InitializeAsInputMode();
-				}
-				else
-				{
-					_sda_pin.InitializeAsOutputMode();
-				}
-			}
+		///
+		/// @brief 读 SDA 引脚的值。
+		///
+		/// @return
+		///
+		virtual bool ReadSDA() const final override
+		{
+			return _sda_pin.ReadPin();
+		}
+	};
 
-			///
-			/// @brief 写 SDA 引脚的值。
-			///
-			/// @param value
-			///
-			virtual void WriteSDA(bool value) final override
-			{
-				_sda_pin.WritePin(value);
-			}
+	/* #endregion */
 
-			///
-			/// @brief 读 SDA 引脚的值。
-			///
-			/// @return
-			///
-			virtual bool ReadSDA() const final override
-			{
-				return _sda_pin.ReadPin();
-			}
-		};
-
-		/* #endregion */
-
-	} // namespace iic
-} // namespace base
+} // namespace base::iic
