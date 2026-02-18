a132a6d5c1ea2b086fa6a766c235117eb7b0e12f
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-02-18 15:54:16 +0800

    ``` refactor(embedded): 重构SDRAM时序提供者接口
    将ISDRAMTimingProvider类从base::sdram嵌套命名空间移至base::sdram
    命名空间下，改善代码结构和可读性。同时优化了类的文档注释格式，
    使代码更符合项目编码规范。
    ```

--- include/base/embedded/sdram/ISDRAMTimingProvider.h
+++ include/base/embedded/sdram/ISDRAMTimingProvider.h
@@ -2,34 +2,31 @@
 #include "base/unit/MHz.h"
 #include "sdram_timing.h"
 
-namespace base
+namespace base::sdram
 {
-	namespace sdram
+	///
+	/// @brief SDRAM 时序提供者。
+	///
+	///
+	class ISDRAMTimingProvider
 	{
-		///
-		/// @brief SDRAM 时序提供者。
-		///
-		///
-		class ISDRAMTimingProvider
-		{
-		public:
-			virtual ~ISDRAMTimingProvider() = default;
+	public:
+		virtual ~ISDRAMTimingProvider() = default;
 
-			///
-			/// @brief 允许的最大时钟频率。
-			///
-			/// @return
-			///
-			virtual base::unit::MHz MaxClockFrequency() const = 0;
+		///
+		/// @brief 允许的最大时钟频率。
+		///
+		/// @return
+		///
+		virtual base::unit::MHz MaxClockFrequency() const = 0;
 
-			///
-			/// @brief 获取指定 CLK 频率下的时序。
-			///
-			/// @param clk_frequency
-			/// @return
-			///
-			virtual base::sdram::sdram_timing GetTiming(base::unit::MHz const &clk_frequency) const = 0;
-		};
+		///
+		/// @brief 获取指定 CLK 频率下的时序。
+		///
+		/// @param clk_frequency
+		/// @return
+		///
+		virtual base::sdram::sdram_timing GetTiming(base::unit::MHz const &clk_frequency) const = 0;
+	};
 
-	} // namespace sdram
-} // namespace base
+} // namespace base::sdram
