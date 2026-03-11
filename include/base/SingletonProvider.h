8d049dc5598d74e33d048d418a9c5310f6183b0a
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-03-12 01:18:30 +0800

    注释

--- include/base/SingletonProvider.h
+++ include/base/SingletonProvider.h
@@ -5,6 +5,19 @@
 
 namespace base
 {
+	///
+	/// @brief 单例提供者。
+	///
+	/// @note 本类需要被实例化为一个全局对象，然后获取单例时从这个本类
+	/// 的全局对象中获取。
+	///
+	/// @note 直接将需要作为单例的类型定义为全局对象，会有构造顺序的问题，
+	/// 本类避免了这个问题，可以放心将本类定义为全局对象，这样就可以获得想要
+	/// 的类型的单例了。
+	///
+	/// @note 可以参考本项目中 StdOutStreamProvider.h 中的 StdOutStreamProvider 类
+	/// 的做法。
+	///
 	template <typename T>
 	class SingletonProvider
 	{
@@ -29,6 +42,7 @@ namespace base
 			if (!_initialized)
 			{
 				base::task::TaskSchedulerSuspendGuard g{};
+
 				if (!_initialized)
 				{
 					new (_instance_buffer) T{};
