f3633dca7f914af57caa756363a668014c22a2d2
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2026-03-13 01:20:39 +0800

    ``` refactor(heap): 将堆管理实现从cpp文件迁移到头文件中
    将heap.cpp中的所有函数实现移至heap.h头文件中作为内联函数，
    同时重构了堆队列的管理和访问方式，使用新的命名空间base::detail::heap
    来封装内部实现细节，并提供了统一的访问接口。
    ```

--- include/base/embedded/heap/heap.h
+++ include/base/embedded/heap/heap.h
@@ -1,8 +1,37 @@
 #pragma once
+#include "base/container/CircleDeque.h"
+#include "base/embedded/heap/Heap4.h"
 #include "base/embedded/heap/IHeap.h"
+#include "base/GlobalObjectProvider.h"
+#include "base/RentedPtrFactory.h"
 #include "base/stream/Span.h"
+#include "base/task/task.h"
 #include <cstddef>
+#include <cstdint>
 #include <memory>
+
+namespace base::detail::heap
+{
+	using HeapQueue = base::CircleDeque<std::shared_ptr<base::heap::IHeap>, 10>;
+
+	class HeapQueueProvider
+	{
+	private:
+		inline static base::GlobalObjectProvider<base::detail::heap::HeapQueue> _provider{};
+
+	public:
+		static base::detail::heap::HeapQueue &Instance()
+		{
+			return _provider.Instance();
+		}
+	};
+
+	inline base::detail::heap::HeapQueue &heap_queue()
+	{
+		return base::detail::heap::HeapQueueProvider::Instance();
+	}
+
+} // namespace base::detail::heap
 
 namespace base::heap
 {
@@ -19,7 +48,17 @@ namespace base::heap
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
 	/// @param heap
 	///
-	void PushBack(std::shared_ptr<base::heap::IHeap> const &heap);
+	inline void PushBack(std::shared_ptr<base::heap::IHeap> const &heap)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+
+		if (base::detail::heap::heap_queue().Count() == 0)
+		{
+			base::detail::heap::heap_queue().PushBack(base::RentedPtrFactory::Create(&base::heap::Heap()));
+		}
+
+		base::detail::heap::heap_queue().PushBack(heap);
+	}
 
 	///
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
@@ -27,13 +66,22 @@ namespace base::heap
 	///
 	/// @param size
 	///
-	void PushBack(uint8_t *buffer, size_t size);
+	inline void PushBack(uint8_t *buffer, size_t size)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+		std::shared_ptr<base::heap::Heap4> heap{new base::heap::Heap4{buffer, size}};
+		base::heap::PushBack(heap);
+	}
 
 	///
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
 	/// @param span
 	///
-	void PushBack(base::Span const &span);
+	inline void PushBack(base::Span const &span)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+		PushBack(span.Buffer(), span.Size());
+	}
 
 	/* #endregion */
 
@@ -43,7 +91,17 @@ namespace base::heap
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
 	/// @param heap
 	///
-	void PushFront(std::shared_ptr<base::heap::IHeap> const &heap);
+	inline void PushFront(std::shared_ptr<base::heap::IHeap> const &heap)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+
+		if (base::detail::heap::heap_queue().Count() == 0)
+		{
+			base::detail::heap::heap_queue().PushFront(base::RentedPtrFactory::Create(&base::heap::Heap()));
+		}
+
+		base::detail::heap::heap_queue().PushFront(heap);
+	}
 
 	///
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
@@ -51,18 +109,65 @@ namespace base::heap
 	///
 	/// @param size
 	///
-	void PushFront(uint8_t *buffer, size_t size);
+	inline void PushFront(uint8_t *buffer, size_t size)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+		std::shared_ptr<base::heap::Heap4> heap{new base::heap::Heap4{buffer, size}};
+		base::heap::PushFront(heap);
+	}
 
 	///
 	/// @brief 添加一个堆到堆列表。一旦添加，堆就会投入使用，被用于动态内存分配。
 	/// @param span
 	///
-	void PushFront(base::Span const &span);
+	inline void PushFront(base::Span const &span)
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+		PushFront(span.Buffer(), span.Size());
+	}
 
 	/* #endregion */
 
-	void *Malloc(size_t size) noexcept;
+	inline void *Malloc(size_t size) noexcept
+	{
+		base::task::TaskSchedulerSuspendGuard g;
 
-	void Free(void *ptr) noexcept;
+		if (base::detail::heap::heap_queue().Count() == 0)
+		{
+			void *p = base::heap::Heap().Malloc(size);
+			return p;
+		}
+
+		for (int64_t i = 0; i < base::detail::heap::heap_queue().Count(); i++)
+		{
+			void *ptr = base::detail::heap::heap_queue()[i]->Malloc(size);
+			if (ptr != nullptr)
+			{
+				return ptr;
+			}
+		}
+
+		return nullptr;
+	}
+
+	inline void Free(void *ptr) noexcept
+	{
+		base::task::TaskSchedulerSuspendGuard g;
+
+		if (base::detail::heap::heap_queue().Count() == 0)
+		{
+			base::heap::Heap().Free(ptr);
+			return;
+		}
+
+		for (int64_t i = 0; i < base::detail::heap::heap_queue().Count(); i++)
+		{
+			if (ptr >= base::detail::heap::heap_queue()[i]->begin() && ptr < base::detail::heap::heap_queue()[i]->end())
+			{
+				base::detail::heap::heap_queue()[i]->Free(ptr);
+				return;
+			}
+		}
+	}
 
 } // namespace base::heap
